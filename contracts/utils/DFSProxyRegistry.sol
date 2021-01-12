65fd0d1cbc5728a15fa754e99b08e1e68fcc7eb2
Nenad Palinkasevic <nesa993@gmail.com>
2021-01-12 18:09:12 +0100

    DFSRegistry proxy update

--- contracts/utils/DFSProxyRegistry.sol
+++ contracts/utils/DFSProxyRegistry.sol
@@ -11,6 +11,7 @@ contract DFSProxyRegistry is AdminAuth {
     IProxyRegistry public mcdRegistry = IProxyRegistry(0x4678f0a6958e4D2Bc4F1BAF7Bc52E8F3564f3fE4);
 
     mapping(address => address) public changedOwners;
+    mapping(address => address[]) public additionalProxies;
 
     /// @notice Changes the proxy that is returned for the user
     /// @dev Used when the user changed DSProxy ownership himself
@@ -31,5 +32,15 @@ contract DFSProxyRegistry is AdminAuth {
         }
 
         return proxyAddr;
+    }
+
+    function addAdditionalProxy(address _user, address _proxy) public onlyOwner {
+        if (IDSProxy(_proxy).owner() == _user) {
+            additionalProxies[_user].push(_proxy);
+        }
+    }
+			
+    function getAllProxies(address _user) public view returns (address, address[] memory) {
+        return (getMcdProxy(_user), additionalProxies[_user]);
     }
 }
