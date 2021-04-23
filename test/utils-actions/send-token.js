9a8daa14b190ac7e2c5259c1963402ea0af92a93
kjaredic <kjaredic@protonmail.com>
2021-04-23 11:10:55 +0200

    Refactor ugly code

--- test/utils-actions/send-token.js
+++ test/utils-actions/send-token.js
@@ -1,6 +1,5 @@
 const { expect } = require('chai');
 const hre = require('hardhat');
-
 const dfs = require('@defisaver/sdk');
 
 const {
@@ -11,62 +10,47 @@ const {
     WETH_ADDRESS,
 } = require('../utils');
 
-// TODO: test when amount == uint.max
-
 describe('Send-Token', function () {
     this.timeout(80000);
 
-    let senderAcc; let recieverAcc; let proxy;
+    let senderAcc; let proxy;
 
     before(async () => {
         await redeploy('WrapEth');
-        await redeploy('DFSSell');
-        await redeploy('TaskExecutor');
 
-        // eslint-disable-next-line prefer-destructuring
         senderAcc = (await hre.ethers.getSigners())[0];
-        recieverAcc = (await hre.ethers.getSigners())[1];
         proxy = await getProxy(senderAcc.address);
     });
     it('... should send tokens direct action', async () => {
         const wrapEthAddr = await getAddrFromRegistry('WrapEth');
-
-        const amount = hre.ethers.constants.WeiPerEther;
-
-        const wrapEthAction = new dfs.actions.basic.WrapEthAction(amount.mul(4));
-
+        const wrapEthAction = new dfs.actions.basic.WrapEthAction(hre.ethers.utils.parseUnits('4', 18));
         const functionData = wrapEthAction.encodeForDsProxyCall()[1];
 
         await proxy['execute(address,bytes)'](wrapEthAddr, functionData, {
-            value: amount.mul(4),
+            value: hre.ethers.utils.parseUnits('4', 18),
             gasLimit: 3000000,
         });
 
         const sendTokenAddr = await getAddrFromRegistry('SendToken');
-
         const sendTokenAction = new dfs.actions.basic.SendTokenAction(
-            WETH_ADDRESS, recieverAcc.address, amount.mul(3),
+            WETH_ADDRESS, senderAcc.address, hre.ethers.utils.parseUnits('3', 18),
         );
-
         const sendTokenData = sendTokenAction.encodeForDsProxyCall()[1];
 
         await proxy['execute(address,bytes)'](sendTokenAddr, sendTokenData);
 
-        expect(await balanceOf(WETH_ADDRESS, recieverAcc.address)).to.be.eq(amount.mul(3));
+        expect(await balanceOf(WETH_ADDRESS, senderAcc.address)).to.be.eq(hre.ethers.utils.parseUnits('3', 18));
     });
 
     it('... should send tokens direct action uint256.max', async () => {
-        const amount = hre.ethers.constants.WeiPerEther;
         const sendTokenAddr = await getAddrFromRegistry('SendToken');
-
         const sendTokenAction = new dfs.actions.basic.SendTokenAction(
-            WETH_ADDRESS, recieverAcc.address, hre.ethers.constants.MaxUint256,
+            WETH_ADDRESS, senderAcc.address, hre.ethers.constants.MaxUint256,
         );
-
         const sendTokenData = sendTokenAction.encodeForDsProxyCall()[1];
 
         await proxy['execute(address,bytes)'](sendTokenAddr, sendTokenData);
 
-        expect(await balanceOf(WETH_ADDRESS, recieverAcc.address)).to.be.eq(amount.mul(4));
+        expect(await balanceOf(WETH_ADDRESS, senderAcc.address)).to.be.eq(hre.ethers.utils.parseUnits('4', 18));
     });
 });
