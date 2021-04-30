c581a2142679d7a7ee286e55b681775b25bc159c
majkic99 <60391069+majkic99@users.noreply.github.com>
2021-04-30 15:57:49 +0200

    feat: add uniswapv3 collect action

---
+++ contracts/actions/uniswap/v3/UniCollectV3.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.7.6;
+pragma experimental ABIEncoderV2;
+
+import "../../../DS/DSMath.sol";
+import "../../ActionBase.sol";
+import "../../../utils/TokenUtils.sol";
+import "../../../interfaces/uniswap/v3/IUniswapV3NonfungiblePositionManager.sol";
+
+/// @title Collects tokensOwed from a position represented by tokenId
+contract UniCollectV3 is ActionBase, DSMath{
+    using TokenUtils for address;
+    //TODO CHANGE ADDRESS
+    IUniswapV3NonfungiblePositionManager public constant positionManager =
+        IUniswapV3NonfungiblePositionManager(0x0);
+
+    /// @param tokenId - The ID of the token for which liquidity is being decreased
+    /// @param recipient - accounts to receive the tokens
+    /// @param amount0Max - The maximum amount of token0 to collect
+    /// @param amount1Max - The maximum amount of token1 to collect
+    struct Params{
+        uint256 tokenId;
+        address recipient;
+        uint128 amount0Max;
+        uint128 amount1Max;
+    }
+
+    /// @inheritdoc ActionBase
+    function executeAction(
+        bytes[] memory _callData,
+        bytes[] memory _subData,
+        uint8[] memory _paramMapping,
+        bytes32[] memory _returnValues
+    ) public payable virtual override returns (bytes32) {
+        Params memory uniData = parseInputs(_callData);
+        
+        uniData.recipient = _parseParamAddr(uniData.recipient, _paramMapping[0], _subData, _returnValues);
+        uniData.amount0Max = _parseParamAddr(uniData.amount0Max, _paramMapping[1], _subData, _returnValues);
+        uniData.amount1Max = _parseParamAddr(uniData.amount1Max, _paramMapping[2], _subData, _returnValues);
+
+        (uint256 amount0, uint256 amount1) = _uniCollect(uniData);
+        return bytes32(); //TODO what to return
+    }
+
+    /// @inheritdoc ActionBase
+    function executeActionDirect(bytes[] memory _callData) public payable override {
+        Params memory uniData = parseInputs(_callData);
+        
+        _uniCollect(uniData);
+    }
+
+    /// @inheritdoc ActionBase
+    function actionType() public pure virtual override returns (uint8) {
+        return uint8(ActionType.STANDARD_ACTION);
+    }
+
+    //////////////////////////// ACTION LOGIC ////////////////////////////
+    
+    /// @dev collects from tokensOwed on position, sends to recipient, up to amountMax
+    /// @return amount sent to the recipient
+    function _uniCollect(Params memory _uniData)
+        internal
+        returns (
+            uint256 amount0,
+            uint256 amount1
+        )
+    {
+        IUniswapV3NonfungiblePositionManager.CollectParams memory collectParams = 
+            IUniswapV3NonfungiblePositionManager.CollectParams({
+                tokenId: _uniData.tokenId,
+                recipient: _uniData.recipient,
+                amount0Max: _uniData.amount0Max,
+                amount1Max: _uniData.amount1Max
+            })
+        (amount0, amount1) = positionManager.collect(collectParams);
+    }
+        
+    function parseInputs(bytes[] memory _callData)
+        internal
+        pure
+        returns (
+            Params memory uniData
+        )
+    {
+        uniData = abi.decode(_callData[0], (Params));
+    }
+}
