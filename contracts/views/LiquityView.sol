535517ec401c3a155bcc52a35bdb96d8bcb7d008
kjaredic <kjaredic@protonmail.com>
2021-05-31 18:01:00 +0200

    feat: add useful functions to liquity view contract

--- contracts/views/LiquityView.sol
+++ contracts/views/LiquityView.sol
@@ -10,25 +10,20 @@ contract LiquityView is LiquityHelper {
     using TokenUtils for address;
     using SafeMath for uint256;
 
-    enum LiquityActionId {
-        Open,
-        Borrow,
-        Payback,
-        Supply,
-        Withdraw
-    }
+    enum LiquityActionId {Open, Borrow, Payback, Supply, Withdraw}
 
     function isRecoveryMode() public view returns (bool) {
         uint256 price = PriceFeed.lastGoodPrice();
         return TroveManager.checkRecoveryMode(price);
     }
-    
+
     function computeNICR(uint256 _coll, uint256 _debt) public pure returns (uint256) {
         if (_debt > 0) {
             return _coll.mul(1e20).div(_debt);
         }
         // Return the maximal value for uint256 if the Trove has a debt of 0. Represents "infinite" CR.
-        else { // if (_debt == 0)
+        else {
+            // if (_debt == 0)
             return 2**256 - 1;
         }
     }
@@ -41,18 +36,18 @@ contract LiquityView is LiquityHelper {
         LiquityActionId _action,
         address _from,
         uint256 _collAmount,
-        uint256 _LUSDAmount
+        uint256 _lusdAmount
     ) external view returns (uint256 NICR) {
         //  LiquityOpen
         if (_action == LiquityActionId.Open) {
             if (!isRecoveryMode())
-                _LUSDAmount = _LUSDAmount.add(TroveManager.getBorrowingFeeWithDecay(_LUSDAmount));
-            _LUSDAmount = BorrowerOperations.getCompositeDebt(_LUSDAmount);
+                _lusdAmount = _lusdAmount.add(TroveManager.getBorrowingFeeWithDecay(_lusdAmount));
+            _lusdAmount = BorrowerOperations.getCompositeDebt(_lusdAmount);
 
             if (_collAmount == type(uint256).max)
                 _collAmount = TokenUtils.WETH_ADDR.getBalance(_from);
-            
-            return computeNICR(_collAmount, _LUSDAmount);
+
+            return computeNICR(_collAmount, _lusdAmount);
         }
 
         (uint256 debt, uint256 coll, , ) = TroveManager.getEntireDebtAndColl(_troveOwner);
@@ -60,41 +55,81 @@ contract LiquityView is LiquityHelper {
         //  LiquityBorrow
         if (_action == LiquityActionId.Borrow) {
             if (!isRecoveryMode())
-                _LUSDAmount = _LUSDAmount.add(TroveManager.getBorrowingFeeWithDecay(_LUSDAmount));
-            return computeNICR(coll, debt.add(_LUSDAmount));
+                _lusdAmount = _lusdAmount.add(TroveManager.getBorrowingFeeWithDecay(_lusdAmount));
+            return computeNICR(coll, debt.add(_lusdAmount));
         }
-        
+
         //  LiquityPayback
         if (_action == LiquityActionId.Payback) {
-            if (_LUSDAmount == type(uint256).max)
-                return computeNICR(coll, 0);
-            return computeNICR(coll, debt.sub(_LUSDAmount));
+            if (_lusdAmount == type(uint256).max) return computeNICR(coll, 0);
+            return computeNICR(coll, debt.sub(_lusdAmount));
         }
 
         //  LiquitySupply
         if (_action == LiquityActionId.Supply) {
             if (_collAmount == type(uint256).max)
                 _collAmount = TokenUtils.WETH_ADDR.getBalance(_from);
-            
+
             return computeNICR(coll.add(_collAmount), debt);
         }
-        
+
         //  LiquityWithdraw
         if (_action == LiquityActionId.Withdraw) {
-            if (_LUSDAmount == type(uint256).max)
-                return computeNICR(0, debt);
+            if (_lusdAmount == type(uint256).max) return computeNICR(0, debt);
             return computeNICR(coll.sub(_collAmount), debt);
         }
-            
     }
 
-    function getApproxHint(uint _CR, uint _numTrials, uint _inputRandomSeed) external
+    function getApproxHint(
+        uint256 _CR,
+        uint256 _numTrials,
+        uint256 _inputRandomSeed
+    )
+        external
         view
-        returns (address hintAddress, uint diff, uint latestRandomSeed) {
-            return HintHelpers.getApproxHint(_CR, _numTrials, _inputRandomSeed);
+        returns (
+            address hintAddress,
+            uint256 diff,
+            uint256 latestRandomSeed
+        )
+    {
+        return HintHelpers.getApproxHint(_CR, _numTrials, _inputRandomSeed);
     }
 
-    function findInsertPosition(uint256 _ICR, address _prevId, address _nextId) external view returns (address upperHint, address lowerHint) {
+    function findInsertPosition(
+        uint256 _ICR,
+        address _prevId,
+        address _nextId
+    ) external view returns (address upperHint, address lowerHint) {
         return SortedTroves.findInsertPosition(_ICR, _prevId, _nextId);
+    }
+
+    function getTroveInfo(address _troveOwner)
+        external
+        view
+        returns (
+            uint256 collAmount,
+            uint256 debtAmount,
+            uint256 collPrice,
+            uint256 TCRatio,
+            bool recoveryMode
+        )
+    {
+        collAmount = TroveManager.getTroveColl(_troveOwner);
+        debtAmount = TroveManager.getTroveDebt(_troveOwner);
+        collPrice = PriceFeed.lastGoodPrice();
+        TCRatio = TroveManager.getTCR(collPrice);
+        recoveryMode = TroveManager.checkRecoveryMode(collPrice);
+    }
+
+    function getApproxHint(
+        uint256 _collAmount,
+        uint256 _debtAmount,
+        uint256 _numTrials,
+        uint256 _inputRandomSeed
+    ) external view returns (address upperHint, address lowerHint) {
+        uint256 NICR = _collAmount.mul(1e20).div(_debtAmount);
+        (address hintAddress, , ) = HintHelpers.getApproxHint(NICR, _numTrials, _inputRandomSeed);
+        (upperHint, lowerHint) = SortedTroves.findInsertPosition(NICR, hintAddress, hintAddress);
     }
 }
