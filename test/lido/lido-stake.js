81881767c41465da1f9c8971a419ff5a5eed571c
majkic99 <60391069+majkic99@users.noreply.github.com>
2021-06-04 17:25:16 +0200

    test: add expect in tests

--- test/lido/lido-stake.js
+++ test/lido/lido-stake.js
@@ -29,9 +29,24 @@ describe('Lido WETH staking', function () {
         const amount = hre.ethers.utils.parseUnits('10', 18);
         await depositToWeth(amount);
         await approve(WETH_ADDRESS, proxy.address);
+
+        const stEthBalanceBefore = await balanceOf('0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84', senderAcc.address);
+        const wethBalanceBefore = await balanceOf(WETH_ADDRESS, senderAcc.address);
         await lidoStake(amount, senderAcc.address, senderAcc.address, proxy);
-        console.log(amount.toString());
-        console.log((await balanceOf('0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84', senderAcc.address)).toString());
-        console.log((await balanceOf('0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84', proxy.address)).toString());
+        const stEthBalanceAfter = await balanceOf('0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84', senderAcc.address);
+        const wethBalanceAfter = await balanceOf(WETH_ADDRESS, senderAcc.address);
+
+        expect(wethBalanceAfter).to.be.eq(wethBalanceBefore.sub(amount));
+
+        const wethChange = wethBalanceBefore.sub(wethBalanceAfter);
+        const stEthChange = stEthBalanceAfter.sub(stEthBalanceBefore);
+        // first time this is called the difference is 2 because of stEth internal math
+        if (wethChange.sub(stEthChange).eq(2)) {
+            expect((stEthBalanceAfter.sub(stEthBalanceBefore)))
+                .to.be.eq(wethBalanceBefore.sub(wethBalanceAfter).sub(2));
+        } else {
+            expect((stEthBalanceAfter.sub(stEthBalanceBefore)))
+                .to.be.eq(wethBalanceBefore.sub(wethBalanceAfter));
+        }
     }).timeout(50000);
 });
