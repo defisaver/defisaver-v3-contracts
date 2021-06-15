e6c59b1e250b73b81536b2ac3effd64798b9256e
majkic99 <vrnebojsamajkic@gmail.com>
2021-06-15 12:06:27 +0200

    refactor: add LiquityReedem 0.8.4 version

--- hardhat.config.js
+++ hardhat.config.js
@@ -39,9 +39,6 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: '0.7.6',
-            },
-            {
                 version: '0.8.4',
             },
         ],
