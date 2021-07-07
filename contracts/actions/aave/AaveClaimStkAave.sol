d0fd9e8168a6cf2e9789279421b3111448938194
kjaredic <kjaredic@protonmail.com>
2021-07-07 14:58:27 +0200

    refactor: update action contract

--- contracts/actions/aave/AaveClaimStkAave.sol
+++ contracts/actions/aave/AaveClaimStkAave.sol
@@ -6,12 +6,11 @@ pragma experimental ABIEncoderV2;
 import "../ActionBase.sol";
 import "./helpers/AaveHelper.sol";
 
-contract AaveClaimRewards is ActionBase, AaveHelper {
+contract AaveClaimStkAave is ActionBase, AaveHelper {
 
     struct Params {
         address[] assets;
         uint256 amount;     // Amount of rewards to claim
-        address onBehalf;   // Address to check and claim rewards
         address to;         // Address that will be receiving the rewards
     }
 
@@ -25,10 +24,9 @@ contract AaveClaimRewards is ActionBase, AaveHelper {
         Params memory params = parseInputs(_callData);
 
         params.amount = _parseParamUint(params.amount, _paramMapping[0], _subData, _returnValues);
-        params.onBehalf = _parseParamAddr(params.onBehalf, _paramMapping[1], _subData, _returnValues);
-        params.to = _parseParamAddr(params.to, _paramMapping[2], _subData, _returnValues);
+        params.to = _parseParamAddr(params.to, _paramMapping[1], _subData, _returnValues);
 
-        uint256 claimedAmount = _aaveClaimRewards(params);
+        uint256 claimedAmount = _aaveClaimStkAave(params);
 
         return bytes32(claimedAmount);
     }
@@ -37,7 +35,7 @@ contract AaveClaimRewards is ActionBase, AaveHelper {
     function executeActionDirect(bytes[] memory _callData) public payable override {
         Params memory params = parseInputs(_callData);
 
-        _aaveClaimRewards(params);
+        _aaveClaimStkAave(params);
     }
 
     /// @inheritdoc ActionBase
@@ -47,16 +45,24 @@ contract AaveClaimRewards is ActionBase, AaveHelper {
 
     //////////////////////////// ACTION LOGIC ////////////////////////////
 
-    /// @notice Claims rewards on behalf of user, on all the assets of the lending pool, accumulating the pending rewards
-    function _aaveClaimRewards(Params memory _params) internal returns (uint256 claimedAmount) {
+    /// @notice Claims stkAave rewards on the assets of the lending pool
+    function _aaveClaimStkAave(Params memory _params) internal returns (uint256 claimedAmount) {
         // amount 0 is safe
         // amount > unclaimedRewards is safe
-        // if onBehalf != msg.sender the caller must be whitelisted via "allowClaimOnBehalf" function by the RewardsAdmin role manager
-        claimedAmount = AaveIncentivesController.claimRewardsOnBehalf(
+        claimedAmount = AaveIncentivesController.claimRewards(
             _params.assets,
             _params.amount,
-            _params.onBehalf,
             _params.to
+        );
+
+        logger.Log(
+            address(this),
+            msg.sender,
+            "AaveClaimStkAave",
+            abi.encode(
+                _params,
+                claimedAmount
+            )
         );
     }
 
