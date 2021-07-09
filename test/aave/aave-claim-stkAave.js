4e21c65677c642c788e2c2f9024502848ebcdc60
kjaredic <kjaredic@protonmail.com>
2021-07-09 14:46:04 +0200

    feat: add edge cases to claim stkAave test

--- test/aave/aave-claim-stkAave.js
+++ test/aave/aave-claim-stkAave.js
@@ -33,11 +33,13 @@ describe('Aave-Supply', function () {
     const supplyAmount = fetchAmountinUSDPrice(tokenSymbol, '10000');
 
     let senderAcc; let proxy; let proxyAddr; let dataProvider;
-    let aTokenInfo;
+    let aTokenInfo; let AaveView;
+    let accruedRewards;
 
     before(async () => {
         await redeploy('AaveSupply');
         await redeploy('AaveClaimStkAave');
+        AaveView = await redeploy('AaveView');
 
         senderAcc = (await hre.ethers.getSigners())[0];
         proxy = await getProxy(senderAcc.address);
@@ -50,15 +52,36 @@ describe('Aave-Supply', function () {
     it(`... should supply ${supplyAmount} ${tokenSymbol} to Aave`, async () => {
         // eslint-disable-next-line max-len
         await supplyAave(proxy, AAVE_MARKET, hre.ethers.utils.parseUnits(supplyAmount, 18), WETH_ADDRESS, senderAcc.address);
+        // eslint-disable-next-line max-len
+        expect(await balanceOf(aTokenInfo.aTokenAddress, proxyAddr)).to.eq(hre.ethers.utils.parseUnits(supplyAmount, 18));
     });
 
-    it('... should claim accrued rewards', async () => {
+    it('... should accrue rewards over time', async () => {
         const secondsInMonth = 2592000;
         await timeTravel(secondsInMonth);
 
-        const balanceBefore = await balanceOf(stkAaveAddr, proxyAddr);
         // eslint-disable-next-line max-len
-        await claimStkAave(proxy, [aTokenInfo.aTokenAddress], hre.ethers.constants.MaxUint256, proxyAddr);
-        expect(await balanceOf(stkAaveAddr, proxyAddr)).to.be.gt(balanceBefore);
+        await supplyAave(proxy, AAVE_MARKET, hre.ethers.constants.One, WETH_ADDRESS, senderAcc.address);
+        // this is done so the getter function below returns accurate balance
+
+        accruedRewards = await AaveView['getUserUnclaimedRewards(address)'](proxyAddr);
+        expect(accruedRewards).to.be.gt(hre.ethers.constants.Zero);
+    });
+
+    it('... should not revert when claiming 0 rewards', async () => {
+        // eslint-disable-next-line max-len
+        await expect(claimStkAave(proxy, [aTokenInfo.aTokenAddress], hre.ethers.constants.Zero, proxyAddr)).to.not.be.reverted;
+    });
+
+    it('... should claim half of all accrued rewards', async () => {
+        // eslint-disable-next-line max-len
+        await claimStkAave(proxy, [aTokenInfo.aTokenAddress], accruedRewards.div('2'), proxyAddr);
+        expect(await balanceOf(stkAaveAddr, proxyAddr)).to.be.eq(accruedRewards.div('2'));
+    });
+
+    it('... should claim all accrued rewards when amount > unclaimed rewards', async () => {
+        // eslint-disable-next-line max-len
+        await claimStkAave(proxy, [aTokenInfo.aTokenAddress], accruedRewards.add('1'), proxyAddr);
+        expect(await balanceOf(stkAaveAddr, proxyAddr)).to.be.eq(accruedRewards);
     });
 });
