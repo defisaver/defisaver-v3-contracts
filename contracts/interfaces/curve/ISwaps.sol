0afb7d140ad6b7f3907948d9ea493753a950ec5e
kjaredic <kjaredic@protonmail.com>
2021-08-24 16:49:39 +0200

    feat: add curve interface contracts

---
+++ contracts/interfaces/curve/ISwaps.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.7.6;
+pragma experimental ABIEncoderV2;
+
+interface ISwaps {
+
+    ///@notice Perform an exchange using the pool that offers the best rate
+    ///@dev Prior to calling this function, the caller must approve
+    ///        this contract to transfer `_amount` coins from `_from`
+    ///        Does NOT check rates in factory-deployed pools
+    ///@param _from Address of coin being sent
+    ///@param _to Address of coin being received
+    ///@param _amount Quantity of `_from` being sent
+    ///@param _expected Minimum quantity of `_from` received
+    ///        in order for the transaction to succeed
+    ///@param _receiver Address to transfer the received tokens to
+    ///@return uint256 Amount received
+    function exchange_with_best_rate(
+        address _from,
+        address _to,
+        uint256 _amount,
+        uint256 _expected,
+        address _receiver
+    ) external payable returns (uint256);
+
+
+    ///@notice Perform an exchange using a specific pool
+    ///@dev Prior to calling this function, the caller must approve
+    ///        this contract to transfer `_amount` coins from `_from`
+    ///        Works for both regular and factory-deployed pools
+    ///@param _pool Address of the pool to use for the swap
+    ///@param _from Address of coin being sent
+    ///@param _to Address of coin being received
+    ///@param _amount Quantity of `_from` being sent
+    ///@param _expected Minimum quantity of `_from` received
+    ///        in order for the transaction to succeed
+    ///@param _receiver Address to transfer the received tokens to
+    ///@return uint256 Amount received
+    function exchange(
+        address _pool,
+        address _from,
+        address _to,
+        uint256 _amount,
+        uint256 _expected,
+        address _receiver
+    ) external payable returns (uint256);
+
+
+
+    ///@notice Find the pool offering the best rate for a given swap.
+    ///@dev Checks rates for regular and factory pools
+    ///@param _from Address of coin being sent
+    ///@param _to Address of coin being received
+    ///@param _amount Quantity of `_from` being sent
+    ///@param _exclude_pools A list of up to 8 addresses which shouldn't be returned
+    ///@return Pool address, amount received
+    function get_best_rate(
+        address _from,
+        address _to,
+        uint256 _amount,
+        address[8] memory _exclude_pools
+    ) external view returns (address, uint256);
+
+
+    ///@notice Get the current number of coins received in an exchange
+    ///@dev Works for both regular and factory-deployed pools
+    ///@param _pool Pool address
+    ///@param _from Address of coin to be sent
+    ///@param _to Address of coin to be received
+    ///@param _amount Quantity of `_from` to be sent
+    ///@return Quantity of `_to` to be received
+    function get_exchange_amount(
+        address _pool,
+        address _from,
+        address _to,
+        uint256 _amount
+    ) external view returns (uint256);
+
+
+    ///@notice Get the current number of coins required to receive the given amount in an exchange
+    ///@param _pool Pool address
+    ///@param _from Address of coin to be sent
+    ///@param _to Address of coin to be received
+    ///@param _amount Quantity of `_to` to be received
+    ///@return Quantity of `_from` to be sent
+    function get_input_amount(
+        address _pool,
+        address _from,
+        address _to,
+        uint256 _amount
+    ) external view returns (uint256);
+
+
+    ///@notice Get the current number of coins required to receive the given amount in an exchange
+    ///@param _pool Pool address
+    ///@param _from Address of coin to be sent
+    ///@param _to Address of coin to be received
+    ///@param _amounts Quantity of `_to` to be received
+    ///@return Quantity of `_from` to be sent
+    function get_exchange_amounts(
+        address _pool,
+        address _from,
+        address _to,
+        uint256[] memory _amounts
+    ) external view returns (uint256[] memory);
+
+
+    ///@notice Set calculator contract
+    ///@dev Used to calculate `get_dy` for a pool
+    ///@param _pool Pool address
+    ///@return `CurveCalc` address
+    function get_calculator(address _pool) external view returns (address);
+}
