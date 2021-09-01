cb4d58cacef3680742af258ddc933e26de69e2bd
majkic99 <vrnebojsamajkic@gmail.com>
2021-09-01 14:09:07 +0200

    refactor: update deployments files

--- contracts/actions/aave/helpers/MainnetAaveAddresses.sol
+++ contracts/actions/aave/helpers/MainnetAaveAddresses.sol
@@ -2,7 +2,6 @@
 
 pragma solidity =0.7.6;
 
-/// @title Utility functions and data used in AaveV2 actions
 contract MainnetAaveAddresses {
     address constant public STAKED_CONTROLLER_ADDR = 0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5;
 }
--- contracts/actions/compound/helpers/MainnetCompAddresses.sol
+++ contracts/actions/compound/helpers/MainnetCompAddresses.sol
@@ -2,7 +2,6 @@
 
 pragma solidity =0.7.6;
 
-/// @title Utility functions and data used in Compound actions
 contract MainnetCompAddresses {
     address public constant C_ETH_ADDR = 0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5;
     address public constant COMPTROLLER_ADDR = 0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B;
