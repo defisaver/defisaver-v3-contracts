fa78417b249d3a116202828beb9186b57f344d56
Nenad Palinkasevic <nesa993@gmail.com>
2021-09-18 18:06:04 +0200

    docs: remove comments that break sol merge

--- contracts/interfaces/aaveV2/IPriceOracleGetterAave.sol
+++ contracts/interfaces/aaveV2/IPriceOracleGetterAave.sol
@@ -3,9 +3,6 @@
 
 pragma solidity =0.7.6;
 
-/************
-@title IPriceOracleGetterAave interface
-@notice Interface for the Aave price oracle.*/
 abstract contract IPriceOracleGetterAave {
     function getAssetPrice(address _asset) external virtual view returns (uint256);
     function getAssetsPrices(address[] calldata _assets) external virtual view returns(uint256[] memory);
