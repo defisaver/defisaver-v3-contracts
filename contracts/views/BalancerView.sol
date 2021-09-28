2551cef94ee4606bdcd032f79f9d957b04b02ca2
kjaredic <kjaredic@protonmail.com>
2021-09-28 15:31:26 +0200

    feat: add Balancer view contract

---
+++ contracts/views/BalancerView.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.7.6;
+
+import "../actions/balancer/helpers/BalancerV2Helper.sol";
+import "../interfaces/balancer/IPool.sol";
+
+contract BalancerView is BalancerV2Helper {
+    function getPoolTokens(address _pool) external view returns (
+        address[] memory tokens,
+        uint256[] memory balances
+    ) {
+        bytes32 poolId = IPool(_pool).getPoolId();
+        (tokens, balances, ) = vault.getPoolTokens(poolId); 
+    }
+}
