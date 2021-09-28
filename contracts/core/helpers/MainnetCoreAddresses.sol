e73f6407fe57145b24c7e5b692e57b6f0f215d9f
majkic99 <vrnebojsamajkic@gmail.com>
2021-09-28 11:52:35 +0200

    feat: add placeholder addr for proxy auth

--- contracts/core/helpers/MainnetCoreAddresses.sol
+++ contracts/core/helpers/MainnetCoreAddresses.sol
@@ -5,5 +5,5 @@ pragma solidity =0.7.6;
 contract MainnetCoreAddresses {
     address internal constant DEFI_SAVER_LOGGER_ADDR = 0x5c55B921f590a89C1Ebe84dF170E655a82b62126;
     address internal constant REGISTRY_ADDR = 0xD6049E1F5F3EfF1F921f5532aF1A1632bA23929C;
-    address internal constant PROXY_AUTH_ADDR = 0x3Aa5ebB10DC797CAC828524e59A333d0A371443c;
+    address internal constant PROXY_AUTH_ADDR = 0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF;
 }
