db665a7aee92827bda21151947028b63d4337849
kjaredic <kjaredic@protonmail.com>
2021-11-08 14:37:15 +0100

    feat: separate mainnet addresses from contract logic

--- contracts/actions/liquity/helpers/LiquityRatioHelper.sol
+++ contracts/actions/liquity/helpers/LiquityRatioHelper.sol
@@ -5,19 +5,20 @@ pragma solidity =0.8.4;
 import "../../../DS/DSMath.sol";
 import "../../../interfaces/liquity/ITroveManager.sol";
 import "../../../interfaces/liquity/IPriceFeed.sol";
+import "./MainnetLiquityAddresses.sol";
 
 /// @title Helper methods for Liquity ratio calc.
-contract LiquityRatioHelper is DSMath {
-
-    ITroveManager constant public TroveManager = ITroveManager(0xA39739EF8b0231DbFA0DcdA07d7e29faAbCf4bb2);
-    IPriceFeed constant public PriceFeed = IPriceFeed(0x4c517D4e2C851CA76d7eC94B805269Df0f2201De);
+contract LiquityRatioHelper is DSMath, MainnetLiquityAddresses {
 
     /// @notice Gets Trove CR
     /// @param _troveOwner Address of the trove owner
     function getRatio(address _troveOwner) public view returns (uint256 ratio, bool isActive) {
-        uint256 troveStatus = TroveManager.getTroveStatus(_troveOwner);
-        isActive = troveStatus == 1;
-        if (isActive == false) return (ratio, isActive);
+        ITroveManager TroveManager = ITroveManager(TROVE_MANAGER_ADDRESS);
+        IPriceFeed PriceFeed = IPriceFeed(PRICE_FEED_ADDRESS);
+
+        isActive = TroveManager.getTroveStatus(_troveOwner) == 1;
+
+        if (!isActive) return (ratio, isActive);
 
         uint256 debtAmount = TroveManager.getTroveDebt(_troveOwner);
         if (debtAmount == 0) return (ratio, isActive);
---
+++ contracts/actions/liquity/helpers/MainnetLiquityAddresses.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.4;
+
+contract MainnetLiquityAddresses {
+    address internal TROVE_MANAGER_ADDRESS = 0xA39739EF8b0231DbFA0DcdA07d7e29faAbCf4bb2;
+    address internal PRICE_FEED_ADDRESS = 0x4c517D4e2C851CA76d7eC94B805269Df0f2201De;
+}
