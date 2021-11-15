719877bc4478c0f9603335f0d8f1d1f3dd408ab9
majkic99 <vrnebojsamajkic@gmail.com>
2021-11-15 09:12:12 +0100

    fix: fix wrong name in title docs

--- contracts/actions/guni/helpers/GUniHelper.sol
+++ contracts/actions/guni/helpers/GUniHelper.sol
@@ -6,7 +6,7 @@ import "./MainnetGUniAddresses.sol";
 import "../../../interfaces/guni/IGUniRouter02.sol";
 
 
-/// @title Utility functions and data used in AaveV2 actions
+/// @title Utility functions and data used in GUni actions
 contract GUniHelper is MainnetGUniAddresses{
     IGUniRouter02 public constant gUniRouter = IGUniRouter02(G_UNI_ROUTER_02_ADDRESS);
 }
--- contracts/actions/guni/helpers/MainnetGUniAddresses.sol
+++ contracts/actions/guni/helpers/MainnetGUniAddresses.sol
@@ -2,7 +2,7 @@
 
 pragma solidity =0.7.6;
 
-/// @title Utility functions and data used in AaveV2 actions
+/// @title Utility functions and data used in GUni actions
 contract MainnetGUniAddresses {
     address public constant G_UNI_ROUTER_02_ADDRESS = 0x14E6D67F824C3a7b4329d3228807f8654294e4bd;
 }
