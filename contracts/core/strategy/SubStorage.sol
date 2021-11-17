d93479dd1659c88fc8425bb9eaf3d304760aafd9
Nenad Palinkasevic <nesa993@gmail.com>
2021-11-17 09:30:19 +0100

    refactor: update events in SubStorage and remove unneeded function

--- contracts/core/strategy/SubStorage.sol
+++ contracts/core/strategy/SubStorage.sol
@@ -13,11 +13,10 @@ contract SubStorage is StrategyModel, AdminAuth {
     error SenderNotSubOwnerError(address, uint256);
     error UserPositionsEmpty();
 
-    event Subscribe(uint256, StrategySub);
-    event UpdateData(uint256, StrategySub);
-    event ActivateSub(uint256);
-    event DeactivateSub(uint256);
-    event RemoveSub(uint256);
+    event Subscribe(uint256 indexed, address indexed, bytes32 indexed, StrategySub);
+    event UpdateData(uint256 indexed, bytes32 indexed, StrategySub);
+    event ActivateSub(uint256 indexed);
+    event DeactivateSub(uint256 indexed);
 
     modifier onlySubOwner(uint256 _subId) {
         if (address(strategiesSubs[_subId].userProxy) == address(0)) {
@@ -37,17 +36,20 @@ contract SubStorage is StrategyModel, AdminAuth {
     function subscribeToStrategy(
         StrategySub memory _sub
     ) public returns (uint256) {
+
+        bytes32 subStorageHash = keccak256(abi.encode(_sub));
+
         strategiesSubs.push(
             StoredSubData(
                 bytes20(msg.sender),
                 true,
-                keccak256(abi.encode(_sub))
+                subStorageHash
             )            
         );
 
         uint256 currentId = strategiesSubs.length - 1;
 
-        emit Subscribe(currentId, _sub);
+        emit Subscribe(currentId, msg.sender, subStorageHash, _sub);
 
         return currentId;
     }
@@ -60,9 +62,12 @@ contract SubStorage is StrategyModel, AdminAuth {
         StrategySub calldata _sub
     ) public onlySubOwner(_subId) {
         StoredSubData storage storedSubData = strategiesSubs[_subId];
-        storedSubData.strategySubHash = keccak256(abi.encode(_sub));
 
-        emit UpdateData(_subId, _sub);
+        bytes32 subStorageHash = keccak256(abi.encode(_sub));
+
+        storedSubData.strategySubHash = subStorageHash;
+
+        emit UpdateData(_subId, subStorageHash, _sub);
     }
 
     function activateSub(
@@ -83,19 +88,6 @@ contract SubStorage is StrategyModel, AdminAuth {
         sub.isEnabled = false;
 
         emit DeactivateSub(_subId);
-    }
-
-    /// @notice Unsubscribe an existing subscription
-    /// @dev Only callable by proxy who created the subscription
-    /// @param _subId Subscription id
-    function removeSub(uint256 _subId) public onlySubOwner(_subId) {
-        uint lastSub = strategiesSubs.length - 1;
-
-        strategiesSubs[_subId] = strategiesSubs[lastSub]; // last sub put in place of the deleted one
-        strategiesSubs.pop(); // delete last subscription, because it moved
-
-        emit RemoveSub(_subId);
-
     }
 
     ///////////////////// VIEW ONLY FUNCTIONS ////////////////////////////
