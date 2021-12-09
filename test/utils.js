9b4789e91c28ead7cca0e3214a25c139cd90e1b0
majkic99 <vrnebojsamajkic@gmail.com>
2021-12-09 13:42:14 +0100

    feat: add helper functions for setting erc20 balance

--- test/utils.js
+++ test/utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 const hre = require('hardhat');
 const fs = require('fs');
 
@@ -112,6 +113,107 @@ const coinGeckoHelper = {
     LUSD: 'liquity-usd',
     LQTY: 'liquity',
     TORN: 'tornado-cash',
+};
+
+async function findBalancesSlot(tokenAddress) {
+    const encode = (types, values) => hre.ethers.utils.defaultAbiCoder.encode(types, values);
+    const account = hre.ethers.constants.AddressZero;
+    const probeA = encode(['uint'], [1]);
+    const probeB = encode(['uint'], [2]);
+    const token = await hre.ethers.getContractAt(
+        'IERC20',
+        tokenAddress,
+    );
+    for (let i = 0; i < 100; i++) {
+        {
+            let probedSlot = hre.ethers.utils.keccak256(
+                encode(['address', 'uint'], [account, i]),
+            );
+            // remove padding for JSON RPC
+            while (probedSlot.startsWith('0x0')) { probedSlot = `0x${probedSlot.slice(3)}`; }
+            const prev = await hre.ethers.provider.send(
+                'eth_getStorageAt',
+                [tokenAddress, probedSlot, 'latest'],
+            );
+            // make sure the probe will change the slot value
+            const probe = prev === probeA ? probeB : probeA;
+
+            await hre.ethers.provider.send('hardhat_setStorageAt', [
+                tokenAddress,
+                probedSlot,
+                probe,
+            ]);
+
+            const balance = await token.balanceOf(account);
+            // reset to previous value
+            await hre.ethers.provider.send('hardhat_setStorageAt', [
+                tokenAddress,
+                probedSlot,
+                prev,
+            ]);
+            if (balance.eq(hre.ethers.BigNumber.from(probe))) { return { isVyper: false, num: i }; }
+        }
+        {
+            let probedSlot = hre.ethers.utils.keccak256(
+                encode(['uint', 'address'], [i, account]),
+            );
+            // remove padding for JSON RPC
+            while (probedSlot.startsWith('0x0')) { probedSlot = `0x${probedSlot.slice(3)}`; }
+            const prev = await hre.ethers.provider.send(
+                'eth_getStorageAt',
+                [tokenAddress, probedSlot, 'latest'],
+            );
+            // make sure the probe will change the slot value
+            const probe = prev === probeA ? probeB : probeA;
+
+            await hre.ethers.provider.send('hardhat_setStorageAt', [
+                tokenAddress,
+                probedSlot,
+                probe,
+            ]);
+
+            const balance = await token.balanceOf(account);
+            // reset to previous value
+            await hre.ethers.provider.send('hardhat_setStorageAt', [
+                tokenAddress,
+                probedSlot,
+                prev,
+            ]);
+            if (balance.eq(hre.ethers.BigNumber.from(probe))) { return { isVyper: true, num: i }; }
+        }
+    }
+    console.log('Balance slot not found');
+    return 0;
+}
+
+const toBytes32 = (bn) => hre.ethers.utils.hexlify(hre.ethers.utils.zeroPad(bn.toHexString(), 32));
+
+const setStorageAt = async (address, index, value) => {
+    await hre.ethers.provider.send('hardhat_setStorageAt', [address, index, value]);
+    await hre.ethers.provider.send('evm_mine', []); // Just mines to the next block
+};
+
+const setBalance = async (tokenAddr, userAddr, value) => {
+    const slotInfo = await findBalancesSlot(tokenAddr);
+    console.log(`SLOT FOUND ${slotInfo.num}`);
+    let index;
+    if (slotInfo.isVyper) {
+        index = hre.ethers.utils.solidityKeccak256(
+            ['uint256', 'uint256'],
+            [slotInfo.num, userAddr], // key, slot
+        );
+    } else {
+        index = hre.ethers.utils.solidityKeccak256(
+            ['uint256', 'uint256'],
+            [userAddr, slotInfo.num], // key, slot
+        );
+    }
+
+    await setStorageAt(
+        tokenAddr,
+        index.toString(),
+        toBytes32(value).toString(),
+    );
 };
 
 const fetchAmountinUSDPrice = (tokenSign, amountUSD) => {
@@ -442,4 +544,5 @@ module.exports = {
     AWBTC_ADDR,
     WBTC_ADDR,
     WSTETH_ADDRESS,
+    setBalance,
 };
