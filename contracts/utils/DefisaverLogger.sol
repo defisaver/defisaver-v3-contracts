86afe570b833fa8eb3b4df0f9d59c9bb8dfa3c1d
kjaredic <kjaredic@protonmail.com>
2022-01-24 19:21:58 +0100

    refactor: remove unneeded code

--- contracts/utils/DefisaverLogger.sol
+++ contracts/utils/DefisaverLogger.sol
@@ -3,24 +3,6 @@
 pragma solidity =0.8.10;
 
 contract DefisaverLogger {
-    event LogEvent(
-        address indexed contractAddress,
-        address indexed caller,
-        string indexed logName,
-        bytes data
-    );
-
-    // solhint-disable-next-line func-name-mixedcase
-    function Log(
-        address _contract,
-        address _caller,
-        string memory _logName,
-        bytes memory _data
-    ) public {
-        emit LogEvent(_contract, _caller, _logName, _data);
-    }
-
-    // The code above would become unused
     event RecipeEvent(
         address indexed caller,
         string indexed logName
