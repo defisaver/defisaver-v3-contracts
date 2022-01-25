731e202da5fba10965c673b9ca83c9209bb4845a
majkic99 <vrnebojsamajkic@gmail.com>
2022-01-25 13:59:24 +0100

    test: add more tests for uint.max

--- test/utils-actions/pull-token-with-permit.js
+++ test/utils-actions/pull-token-with-permit.js
@@ -74,10 +74,74 @@ describe('Pull-Token-Permit', function () {
         await proxy['execute(address,bytes)'](pullTokenAddr, pullTokenData);
         const proxyBalanceAfter = await balanceOf(ADAI_ADDR, proxy.address);
 
+        const approvedAmount = await aDaiContract.allowance(senderAcc.address, proxy.address);
+        console.log(`approved amount: ${approvedAmount.toString()}`);
+
         console.log(proxyBalanceBefore.toString());
         console.log(proxyBalanceAfter.toString());
 
-        expect(proxyBalanceAfter).to.be.gt(proxyBalanceBefore);
+        expect(proxyBalanceAfter.sub(proxyBalanceBefore)).to.be.gte(amount);
+    });
+
+    it('... should pull full balance of aDAI with permit - direct action', async () => {
+        const amount = hre.ethers.utils.parseUnits('1000', 18);
+        // this will work for years, current timestamp = 1743034755
+        const deadline = '91743034755';
+
+        const uintMax = hre.ethers.constants.MaxUint256;
+
+        const domain = {
+            name: 'Aave interest bearing DAI',
+            version: '1',
+            chainId: 1,
+            verifyingContract: '0x028171bCA77440897B824Ca71D1c56caC55b68A3',
+        };
+        const types = {
+            Permit: [
+                { name: 'owner', type: 'address' },
+                { name: 'spender', type: 'address' },
+                { name: 'value', type: 'uint256' },
+                { name: 'nonce', type: 'uint256' },
+                { name: 'deadline', type: 'uint256' },
+            ],
+        };
+        const aDaiContract = await hre.ethers.getContractAt('IERC20WithPermit', ADAI_ADDR);
+        // eslint-disable-next-line no-underscore-dangle
+        const nonce = await aDaiContract._nonces(senderAcc.address);
+        const value = {
+            owner: senderAcc.address,
+            spender: proxy.address,
+            value: uintMax,
+            nonce,
+            deadline,
+        };
+        // eslint-disable-next-line no-underscore-dangle
+        const rawSignature = await senderAcc._signer._signTypedData(domain, types, value);
+        console.log(rawSignature.toString());
+        const signatureSplitted = hre.ethers.utils.splitSignature(rawSignature);
+        console.log(signatureSplitted);
+        const r = signatureSplitted.r;
+        const s = signatureSplitted.s;
+        const v = signatureSplitted.v;
+        await setBalance(ADAI_ADDR, senderAcc.address, amount);
+
+        const pullTokenAddr = await getAddrFromRegistry('PermitPullToken');
+
+        const proxyBalanceBefore = await balanceOf(ADAI_ADDR, proxy.address);
+        const pullTokenAction = new dfs.actions.basic.PermitPullTokenAction(
+            ADAI_ADDR, senderAcc.address, uintMax, deadline, v, r, s,
+        );
+        const pullTokenData = pullTokenAction.encodeForDsProxyCall()[1];
+
+        await proxy['execute(address,bytes)'](pullTokenAddr, pullTokenData);
+        const proxyBalanceAfter = await balanceOf(ADAI_ADDR, proxy.address);
+        const approvedAmount = await aDaiContract.allowance(senderAcc.address, proxy.address);
+        console.log(`approved amount: ${approvedAmount.toString()}`);
+
+        console.log(proxyBalanceBefore.toString());
+        console.log(proxyBalanceAfter.toString());
+
+        expect(proxyBalanceAfter.sub(proxyBalanceBefore)).to.be.gte(amount);
     });
     it('... should pull 1000 DAI with permit - direct action', async () => {
         const amount = hre.ethers.utils.parseUnits('1000', 18);
@@ -128,9 +192,70 @@ describe('Pull-Token-Permit', function () {
         await proxy['execute(address,bytes)'](pullTokenAddr, pullTokenData);
         const proxyBalanceAfter = await balanceOf(DAI_ADDR, proxy.address);
 
+        const approvedAmount = await daiContract.allowance(senderAcc.address, proxy.address);
+        console.log(`approved amount: ${approvedAmount.toString()}`);
+
         console.log(proxyBalanceBefore.toString());
         console.log(proxyBalanceAfter.toString());
 
-        expect(proxyBalanceAfter).to.be.gt(proxyBalanceBefore);
+        expect(proxyBalanceAfter.sub(proxyBalanceBefore)).to.be.eq(amount);
+    });
+    it('... should pull full balance of DAI with permit - direct action', async () => {
+        const amount = hre.ethers.utils.parseUnits('2000', 18);
+        const deadline = 0;
+        const uintMax = hre.ethers.constants.MaxUint256;
+
+        const domain = {
+            name: 'Dai Stablecoin',
+            version: '1',
+            chainId: 1,
+            verifyingContract: DAI_ADDR,
+        };
+        const types = {
+            Permit: [
+                { name: 'holder', type: 'address' },
+                { name: 'spender', type: 'address' },
+                { name: 'nonce', type: 'uint256' },
+                { name: 'expiry', type: 'uint256' },
+                { name: 'allowed', type: 'bool' },
+            ],
+        };
+        const daiContract = await hre.ethers.getContractAt('IERC20WithPermit', DAI_ADDR);
+        const nonce = await daiContract.nonces(senderAcc.address);
+        const value = {
+            holder: senderAcc.address,
+            spender: proxy.address,
+            nonce,
+            expiry: deadline,
+            allowed: true,
+        };
+        // eslint-disable-next-line no-underscore-dangle
+        const rawSignature = await senderAcc._signer._signTypedData(domain, types, value);
+        console.log(rawSignature.toString());
+        const signatureSplitted = hre.ethers.utils.splitSignature(rawSignature);
+        console.log(signatureSplitted);
+        const r = signatureSplitted.r;
+        const s = signatureSplitted.s;
+        const v = signatureSplitted.v;
+        await setBalance(DAI_ADDR, senderAcc.address, amount);
+
+        const pullTokenAddr = await getAddrFromRegistry('PermitPullToken');
+
+        const proxyBalanceBefore = await balanceOf(DAI_ADDR, proxy.address);
+        const pullTokenAction = new dfs.actions.basic.PermitPullTokenAction(
+            DAI_ADDR, senderAcc.address, uintMax, deadline, v, r, s,
+        );
+        const pullTokenData = pullTokenAction.encodeForDsProxyCall()[1];
+
+        await proxy['execute(address,bytes)'](pullTokenAddr, pullTokenData);
+        const proxyBalanceAfter = await balanceOf(DAI_ADDR, proxy.address);
+
+        const approvedAmount = await daiContract.allowance(senderAcc.address, proxy.address);
+        console.log(`approved amount: ${approvedAmount.toString()}`);
+
+        console.log(proxyBalanceBefore.toString());
+        console.log(proxyBalanceAfter.toString());
+
+        expect(proxyBalanceAfter.sub(proxyBalanceBefore)).to.be.eq(amount);
     });
 });
