9544b37af8c82efe43ee6163b71dad5afe83fb82
Nenad Palinkasevic <nesa993@gmail.com>
2022-02-07 12:20:07 +0100

    fix: switch back addresses to internal

--- contracts/actions/liquity/helpers/MainnetLiquityAddresses.sol
+++ contracts/actions/liquity/helpers/MainnetLiquityAddresses.sol
@@ -3,15 +3,15 @@
 pragma solidity =0.7.6;
 
 contract MainnetLiquityAddresses {
-    address public constant LUSDTokenAddr = 0x5f98805A4E8be255a32880FDeC7F6728C6568bA0;
-    address public constant LQTYTokenAddr = 0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D;
-    address public constant PriceFeedAddr = 0x4c517D4e2C851CA76d7eC94B805269Df0f2201De;
-    address public constant BorrowerOperationsAddr = 0x24179CD81c9e782A4096035f7eC97fB8B783e007;
-    address public constant TroveManagerAddr = 0xA39739EF8b0231DbFA0DcdA07d7e29faAbCf4bb2;
-    address public constant SortedTrovesAddr = 0x8FdD3fbFEb32b28fb73555518f8b361bCeA741A6;
-    address public constant HintHelpersAddr = 0xE84251b93D9524E0d2e621Ba7dc7cb3579F997C0;
-    address public constant CollSurplusPoolAddr = 0x3D32e8b97Ed5881324241Cf03b2DA5E2EBcE5521;
-    address public constant StabilityPoolAddr = 0x66017D22b0f8556afDd19FC67041899Eb65a21bb;
-    address public constant LQTYStakingAddr = 0x4f9Fbb3f1E99B56e0Fe2892e623Ed36A76Fc605d;
-    address public constant LQTYFrontEndAddr = 0x76720aC2574631530eC8163e4085d6F98513fb27;
+    address internal constant LUSDTokenAddr = 0x5f98805A4E8be255a32880FDeC7F6728C6568bA0;
+    address internal constant LQTYTokenAddr = 0x6DEA81C8171D0bA574754EF6F8b412F2Ed88c54D;
+    address internal constant PriceFeedAddr = 0x4c517D4e2C851CA76d7eC94B805269Df0f2201De;
+    address internal constant BorrowerOperationsAddr = 0x24179CD81c9e782A4096035f7eC97fB8B783e007;
+    address internal constant TroveManagerAddr = 0xA39739EF8b0231DbFA0DcdA07d7e29faAbCf4bb2;
+    address internal constant SortedTrovesAddr = 0x8FdD3fbFEb32b28fb73555518f8b361bCeA741A6;
+    address internal constant HintHelpersAddr = 0xE84251b93D9524E0d2e621Ba7dc7cb3579F997C0;
+    address internal constant CollSurplusPoolAddr = 0x3D32e8b97Ed5881324241Cf03b2DA5E2EBcE5521;
+    address internal constant StabilityPoolAddr = 0x66017D22b0f8556afDd19FC67041899Eb65a21bb;
+    address internal constant LQTYStakingAddr = 0x4f9Fbb3f1E99B56e0Fe2892e623Ed36A76Fc605d;
+    address internal constant LQTYFrontEndAddr = 0x76720aC2574631530eC8163e4085d6F98513fb27;
 }
