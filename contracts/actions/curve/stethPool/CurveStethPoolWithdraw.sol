a7cf22f6526a005b5e99ed2705ee3f688405f2c2
kjaredic <kjaredic@protonmail.com>
2022-03-11 18:12:28 +0100

    feat: add Curve stEth pool actions

---
+++ contracts/actions/curve/stethPool/CurveStethPoolWithdraw.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.7.6;
+pragma experimental ABIEncoderV2;
+
+import "../../../interfaces/curve/stethPool/ICurveStethPool.sol";
+import "../helpers/CurveHelper.sol";
+import "../../../utils/TokenUtils.sol";
+import "../../../utils/SafeMath.sol";
+import "../../ActionBase.sol";
+
+contract CurveStethPoolWithdraw is ActionBase {
+    using TokenUtils for address;
+    using SafeMath for uint256;
+
+    address constant internal CURVE_STETH_POOL_ADDR = 0xDC24316b9AE028F1497c275EB9192a3Ea0f67022;
+    address constant STE_CRV_ADDR = 0x06325440D014e39736583c165C2963BA99fAf14E;
+    address constant STETH_ADDR = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
+
+    struct Params {
+        address from;         // address where to pull lp tokens from
+        address to;       // address that will receive withdrawn tokens
+        uint256[2] amounts;     // amount of each token to withdraw
+        uint256 maxBurnAmount;  // max amount of LP tokens to burn
+    }
+
+    function executeAction(
+        bytes[] memory _callData,
+        bytes[] memory _subData,
+        uint8[] memory _paramMapping,
+        bytes32[] memory _returnValues
+    ) public payable virtual override returns (bytes32) {
+        Params memory params = parseInputs(_callData);
+        params.from = _parseParamAddr(params.from, _paramMapping[0], _subData, _returnValues);
+        params.to = _parseParamAddr(params.to, _paramMapping[1], _subData, _returnValues);
+        params.amounts[0] = _parseParamUint(params.amounts[0], _paramMapping[2], _subData, _returnValues);
+        params.amounts[1] = _parseParamUint(params.amounts[1], _paramMapping[3], _subData, _returnValues);
+        params.maxBurnAmount = _parseParamUint(params.maxBurnAmount, _paramMapping[4], _subData, _returnValues);
+
+        uint256 burnedLp = _curveWithdraw(params);
+        return bytes32(burnedLp);
+    }
+
+    /// @inheritdoc ActionBase
+    function executeActionDirect(bytes[] memory _callData) public payable virtual override {
+        Params memory params = parseInputs(_callData);
+        _curveWithdraw(params);
+    }
+
+    /// @inheritdoc ActionBase
+    function actionType() public pure virtual override returns (uint8) {
+        return uint8(ActionType.STANDARD_ACTION);
+    }
+
+    //////////////////////////// ACTION LOGIC ////////////////////////////
+
+    /// @notice Withdraws tokens from curve steth pool
+    function _curveWithdraw(Params memory _params) internal returns (uint256 burnedLp) {
+        require(_params.to != address(0), "to cant be 0x0");
+
+        STE_CRV_ADDR.pullTokensIfNeeded(_params.from, _params.maxBurnAmount);
+        STE_CRV_ADDR.approveToken(CURVE_STETH_POOL_ADDR, _params.maxBurnAmount);
+
+        burnedLp = ICurveStethPool(CURVE_STETH_POOL_ADDR).remove_liquidity_imbalance(
+            _params.amounts,
+            _params.maxBurnAmount
+        );
+
+        TokenUtils.depositWeth(_params.amounts[0]);
+        TokenUtils.WETH_ADDR.withdrawTokens(_params.to, _params.amounts[0]);
+        STETH_ADDR.withdrawTokens(_params.to, _params.amounts[1]);
+        // return unburned lp tokens to from
+        STE_CRV_ADDR.withdrawTokens(_params.from, _params.maxBurnAmount.sub(burnedLp));
+
+        logger.Log(
+            address(this),
+            msg.sender,
+            "CurveStethPoolWithdraw",
+            abi.encode(burnedLp)
+        );
+    }
+
+    function parseInputs(bytes[] memory _callData) internal pure returns (Params memory params) {
+        params = abi.decode(_callData[0], (Params));
+    }
+}
