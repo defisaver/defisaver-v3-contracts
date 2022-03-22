a5697439f891d8ff006ca7df32aa751a8211d820
Nenad Palinkasevic <nesa993@gmail.com>
2022-03-22 11:54:36 +0100

    feat: add Optimism related addresses for exchange

---
+++ contracts/exchangeV3/helpers/OptimismExchangeAddresses.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.10;
+
+contract OptimismExchangeAddresses {
+
+    address internal constant FEE_RECIPIENT_ADDRESS = 0x0923E2F6F11c397fE34A9F5A75e581c1Af0Ad435;
+    address internal constant DISCOUNT_ADDRESS = 0x67167dC29afC0ded53C01a279d5313943A25e5cD;
+    address internal constant SAVER_EXCHANGE_REGISTRY = 0x36Bf2251E9797df071A9b8bc4dE58F7cAcc16F44;
+    address internal constant ZRX_ALLOWLIST_ADDR = 0x68f2F8c2a8aD0AdC760989Bd7D29e127a1f00510;
+    address internal constant ETH_ADDRESS = 0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000;
+}
