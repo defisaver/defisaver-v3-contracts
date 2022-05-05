59eb6b821678471896333978bf01d9b772845334
majkic99 <vrnebojsamajkic@gmail.com>
2022-05-05 16:23:05 +0200

    fix: small changes in addresses on opti

--- contracts/exchangeV3/wrappersV3/helpers/OptimismWrapperAddresses.sol
+++ contracts/exchangeV3/wrappersV3/helpers/OptimismWrapperAddresses.sol
@@ -8,4 +8,8 @@ contract OptimismWrapperAddresses {
     address payable internal constant WALLET_ID = payable(0x322d58b9E75a6918f7e7849AEe0fF09369977e08);
     address internal constant UNI_V3_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
     address internal constant UNI_V3_QUOTER = 0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6;
+
+    //TODO set this?
+    address internal constant KYBER_INTERFACE = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address internal constant UNI_V2_ROUTER = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 }
