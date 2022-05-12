57529e800ceb7cc4d12b3b8d33068168d4bb792a
kjaredic <kjaredic@protonmail.com>
2022-05-12 18:01:55 +0200

    feat: add dfs exchange wrapper for Curve

---
+++ contracts/exchangeV3/wrappersV3/CurveWrapperV3.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.10;
+
+import "../../interfaces/exchange/IExchangeV3.sol";
+import "../../interfaces/curve/ISwaps.sol";
+import "../../interfaces/IERC20.sol";
+import "../../auth/AdminAuth.sol";
+import "../../utils/SafeERC20.sol";
+import "./helpers/WrapperHelper.sol";
+
+/// @title DFS exchange wrapper for Curve
+contract CurveWrapperV3 is IExchangeV3, AdminAuth, WrapperHelper {
+    using SafeERC20 for IERC20;
+
+    ISwaps public constant exchangeContract = ISwaps(CURVE_EXCHANGE_CONTRACT);
+
+    /// @notice Sells _srcAmount of tokens on Curve
+    /// @param _srcAddr From token
+    /// @param _srcAmount From amount
+    /// @param _additionalData Route and swap params
+    /// @return uint256 amount of tokens received from selling
+    function sell(address _srcAddr, address, uint256 _srcAmount, bytes calldata _additionalData) external override returns (uint) {
+        IERC20(_srcAddr).safeApprove(address(exchangeContract), _srcAmount);
+
+        (
+            address[9] memory _route, uint256[3][4] memory _swap_params
+        ) = abi.decode(_additionalData, (address[9], uint256[3][4]));
+
+        address[4] memory pools;
+        uint256 amountOut = exchangeContract.exchange_multiple(
+            _route,
+            _swap_params,
+            _srcAmount,
+            1,   // _expected
+            pools,
+            msg.sender
+        );
+
+        return amountOut;
+    }
+
+    /// @dev depricated function
+    function buy(address, address, uint, bytes calldata) external override returns(uint) {
+        return 0;
+    }
+
+    /// @notice Return a rate for which we can sell an amount of tokens
+    /// @param _srcAmount From amount
+    /// @param _additionalData Route and swap params
+    /// @return uint256 Rate (price)
+    function getSellRate(address, address, uint256 _srcAmount, bytes memory _additionalData) public override returns (uint) {
+        (
+            address[9] memory _route, uint256[3][4] memory _swap_params
+        ) = abi.decode(_additionalData, (address[9], uint256[3][4]));
+
+        uint256 amountOut = exchangeContract.get_exchange_multiple_amount(
+            _route,
+            _swap_params,
+            _srcAmount
+        );
+        return amountOut;
+    }
+
+    /// @dev depricated function
+    function getBuyRate(address, address, uint, bytes memory) public override returns (uint) {
+        return 0;
+    }
+
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable {}
+}
--- contracts/interfaces/curve/ISwaps.sol
+++ contracts/interfaces/curve/ISwaps.sol
@@ -110,4 +110,46 @@ interface ISwaps {
     ///@param _pool Pool address
     ///@return `CurveCalc` address
     function get_calculator(address _pool) external view returns (address);
+
+
+    /// @notice Perform up to four swaps in a single transaction
+    /// @dev Routing and swap params must be determined off-chain. This
+    ///     functionality is designed for gas efficiency over ease-of-use.
+    /// @param _route Array of [initial token, pool, token, pool, token, ...]
+    ///     The array is iterated until a pool address of 0x00, then the last
+    ///     given token is transferred to `_receiver`
+    /// @param _swap_params Multidimensional array of [i, j, swap type] where i and j are the correct
+    ///     values for the n'th pool in `_route`. The swap type should be 1 for
+    ///     a stableswap `exchange`, 2 for stableswap `exchange_underlying`, 3
+    ///     for a cryptoswap `exchange`, 4 for a cryptoswap `exchange_underlying`,
+    ///     5 for Polygon factory metapools `exchange_underlying`, 6-8 for
+    ///     underlying coin -> LP token "exchange" (actually `add_liquidity`), 9 and 10
+    ///     for LP token -> underlying coin "exchange" (actually `remove_liquidity_one_coin`)
+    /// @param _amount The amount of `_route[0]` token being sent.
+    /// @param _expected The minimum amount received after the final swap.
+    /// @param _pools Array of pools for swaps via zap contracts. This parameter is only needed for
+    ///     Polygon meta-factories underlying swaps.
+    /// @param _receiver Address to transfer the final output token to.
+    /// @return Received amount of the final output token
+    function exchange_multiple(
+        address[9] memory _route,
+        uint256[3][4] memory _swap_params,
+        uint256 _amount,
+        uint256 _expected,
+        address[4] memory _pools,
+        address _receiver
+    ) external payable returns (uint256);
+
+    function exchange_multiple(
+        address[9] memory _route,
+        uint256[3][4] memory _swap_params,
+        uint256 _amount,
+        uint256 _expected
+    ) external payable returns (uint256);
+
+    function get_exchange_multiple_amount(
+        address[9] memory _route,
+        uint256[3][4] memory _swap_params,
+        uint256 _amount
+    ) external view returns (uint256);
 }
