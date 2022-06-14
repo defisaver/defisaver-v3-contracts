9649c706f7646143b41cf2a68b0d7fa474197813
Nenad Palinkasevic <nesa993@gmail.com>
2022-06-14 15:32:14 +0200

    fix: change with new mcd FL addr

--- contracts/actions/flashloan/helpers/MainnetFLAddresses.sol
+++ contracts/actions/flashloan/helpers/MainnetFLAddresses.sol
@@ -9,7 +9,7 @@ contract MainnetFLAddresses {
     address internal constant DYDX_FL_FEE_FAUCET = 0x47f159C90850D5cE09E21F931d504536840f34b4;
     address internal constant AAVE_LENDING_POOL = 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9;
     address internal constant AAVE_LENDING_POOL_ADDRESS_PROVIDER = 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5;
-    address internal constant DSS_FLASH_ADDR = 0x1EB4CF3A948E7D72A198fe073cCb8C7a948cD853;
+    address internal constant DSS_FLASH_ADDR = 0x60744434d6339a6B27d73d9Eda62b6F66a0a04FA;
     address internal constant DAI_ADDR = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address internal constant ST_ETH_ADDR = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
 }
