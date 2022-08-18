d254b5cb9d73bb7090da1928f207fd0910b2151c
Nenad Palinkasevic <nesa993@gmail.com>
2022-08-18 14:06:02 +0200

    feat: change mock feed addr

--- cmd/forkooor.js
+++ cmd/forkooor.js
@@ -109,7 +109,7 @@ try {
     console.log('No forked registry set yet, please run deploy');
 }
 
-const MOCK_CHAINLINK_ORACLE = '0x9C3744c5F72Bb18a9a8a5Cfd1336121EB0d3AdCa';
+const MOCK_CHAINLINK_ORACLE = '0x0CA7cE9EeD38c82BE579fc21494a93cadAF87B01';
 const REGISTRY_ADDR = '0x287778F121F134C66212FB16c9b53eC991D32f5b'; // forkedAddresses.DFSRegistry;
 const abiCoder = new hre.ethers.utils.AbiCoder();
 
