a3b1d9a782f05a7586bfeea49c8b6f115f823e37
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2022-08-26 15:36:16 +0200

    Add CompV3Allow Contract

---
+++ contracts/actions/compoundV3/CompV3Allow.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.10;
+
+import "../../interfaces/compoundV3/IComet.sol";
+import "../../interfaces/compoundV3/ICometExt.sol";
+import "../../interfaces/IWETH.sol";
+import "../../utils/TokenUtils.sol";
+import "../ActionBase.sol";
+import "./helpers/CompV3Helper.sol";
+
+/// @title Allow or disallow manager
+contract CompV3Allow is ActionBase, CompV3Helper {
+
+     using TokenUtils for address;
+
+    struct Params {
+        address manager;
+        bool isAllowed;
+    }
+    error CompV3AllowError();
+
+    /// @inheritdoc ActionBase
+    function executeAction(
+        bytes memory _callData,
+        bytes32[] memory _subData,
+        uint8[] memory _paramMapping,
+        bytes32[] memory _returnValues
+    ) public payable virtual override returns (bytes32) {
+        Params memory params = parseInputs(_callData);
+
+        params.manager = _parseParamAddr(params.manager, _paramMapping[0], _subData, _returnValues);
+        params.isAllowed = _parseParamUint(params.isAllowed ? 1 : 0, _paramMapping[1], _subData, _returnValues) == 1;
+
+        (bool isAllowed, bytes memory logData) = _allow(params.manager, params.isAllowed);
+        emit ActionEvent("CompV3Allow", logData);
+        return bytes32(uint256(isAllowed ? 1 : 0));
+    }
+
+    /// @inheritdoc ActionBase
+    function executeActionDirect(bytes memory _callData) public payable override {
+        Params memory params = parseInputs(_callData);
+        (, bytes memory logData) = _allow(params.manager, params.isAllowed);
+        logger.logActionDirectEvent("CompV3Allow", logData);
+    }
+
+    /// @inheritdoc ActionBase
+    function actionType() public pure virtual override returns (uint8) {
+        return uint8(ActionType.STANDARD_ACTION);
+    }
+
+    //////////////////////////// ACTION LOGIC ////////////////////////////
+
+    /// @notice User allowes or disallowes manager
+    /// @param _manager Address of manager
+    /// @param _isAllowed True for allow, false for disallow
+    function _allow(
+        address _manager,
+        bool _isAllowed
+    ) internal returns (bool, bytes memory) {
+        ICometExt(COMET_EXT_ADDR).allow(_manager,_isAllowed);
+
+        bytes memory logData = abi.encode(_manager, _isAllowed);
+        return (_isAllowed, logData);
+    }
+
+    function parseInputs(bytes memory _callData) public pure returns (Params memory params) {
+        params = abi.decode(_callData, (Params));
+    }
+
+}
