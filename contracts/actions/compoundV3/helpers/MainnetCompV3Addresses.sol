e15951150d20c63625babecc7ba592bd5707655c
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2022-08-26 11:25:53 +0200

    Add USDC address

--- contracts/actions/compoundV3/helpers/MainnetCompV3Addresses.sol
+++ contracts/actions/compoundV3/helpers/MainnetCompV3Addresses.sol
@@ -5,6 +5,7 @@ pragma solidity =0.8.10;
 contract MainnetCompV3Addresses {
     address internal constant COMET_ADDR = 0xc3d688B66703497DAA19211EEdff47f25384cdc3;
     address internal constant COMET_EXT_ADDR = 0x285617313887d43256F852cAE0Ee4de4b68D45B0;
+    address internal constant USDC_ADDR = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
 }
 
