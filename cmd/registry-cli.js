08eee54feffced8c98e57a062156d40e039ecd6e
Nenad Palinkasevic <nesa993@gmail.com>
2022-08-29 15:54:59 +0200

    feat: finish registry-cli tool

--- cmd/registry-cli.js
+++ cmd/registry-cli.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-param-reassign */
 /* eslint-disable import/no-extraneous-dependencies */
 require('dotenv-safe').config();
@@ -6,12 +7,22 @@ const ethers = require('ethers');
 const { program } = require('commander');
 
 const { addrs } = require('../test/utils');
+const { getAllFiles } = require('../scripts/hardhat-tasks-functions');
+const { getNameId } = require('../test/utils');
 
 const registryAbi = require('../artifacts/contracts/core/DFSRegistry.sol/DFSRegistry.json').abi;
 
-const { getAllFiles } = require('../scripts/hardhat-tasks-functions');
+const setProviderAndRegistry = async (options) => {
+    const network = options.network.length === 0 ? 'mainnet' : options.network;
 
-const { getNameId } = require('../test/utils');
+    const nodeName = network !== 'mainnet' ? `${network.toUpperCase()}_NODE` : 'ETHEREUM_NODE';
+
+    const provider = new ethers.providers.WebSocketProvider(process.env[nodeName]);
+
+    const registry = new ethers.Contract(addrs[network].REGISTRY_ADDR, registryAbi, provider);
+
+    return { provider, registry };
+};
 
 const generateIds = () => {
     const idsMap = {};
@@ -33,22 +44,63 @@ const parseAddNewContractEvent = (event) => ({
     waitTime: parseFloat(event.args[3].toString()),
 });
 
+const parseApproveContractChangeEvent = (event) => ({
+    id: event.args[1],
+    oldAddr: event.args[2],
+    newAddr: event.args[3],
+});
+
 const getEntry = async (registry, id) => {
     const entry = await registry.entries(id);
 
     return {
         id,
         addr: entry.contractAddr,
-        changeTime: entry.changeStartTime.toString(),
+        changeTime: entry.waitPeriod.toString(),
     };
 };
 
-const fetchAllContractsInRegistry = async (network) => {
-    const nodeName = network !== 'mainnet' ? `${network.toUpperCase()}_NODE` : 'ETHEREUM_NODE';
+const getEntryHistory = async (idOrName, options) => {
+    const { provider, registry } = await setProviderAndRegistry(options);
 
-    const provider = new ethers.providers.WebSocketProvider(process.env[nodeName]);
+    const id = idOrName.startsWith('0x') ? idOrName : getNameId(idOrName);
 
-    const registry = new ethers.Contract(addrs[network].REGISTRY_ADDR, registryAbi, provider);
+    let filter = registry.filters.AddNewContract();
+    let events = await registry.queryFilter(filter);
+
+    const addNewContractEvent = events.find((e) => parseAddNewContractEvent(e).id === id);
+
+    console.log(parseAddNewContractEvent(addNewContractEvent));
+
+    filter = registry.filters.ApproveContractChange();
+    events = await registry.queryFilter(filter);
+
+    const updateEvents = events.filter((e) => e.args[1] === id);
+
+    updateEvents.forEach((e) => {
+        console.log(parseApproveContractChangeEvent(e));
+    });
+};
+
+const getFullEntryData = async (idOrName, options) => {
+    const { provider, registry } = await setProviderAndRegistry(options);
+
+    const id = idOrName.startsWith('0x') ? idOrName : getNameId(idOrName);
+
+    const entry = await registry.entries(id);
+
+    return {
+        isRegistered: entry.exists,
+        id,
+        addr: entry.contractAddr,
+        changeTime: entry.waitPeriod.toString(),
+        inContractChange: entry.inContractChange,
+        inWaitPeriodChange: entry.inWaitPeriodChange,
+    };
+};
+
+const fetchAllContractsInRegistry = async (options) => {
+    const { provider, registry } = await setProviderAndRegistry(options);
 
     // fetch newContract events
     const filter = registry.filters.AddNewContract();
@@ -79,14 +131,28 @@ const fetchAllContractsInRegistry = async (network) => {
 (async () => {
     program
         .command('dump')
-        .option('-n, --network <network>', 'network', [])
+        .option('-n, --network <network>', 'Specify network we are calling (defaults to L1)', [])
         .description('Returns all the correctly registered contracts in the registry')
         .action(async (options) => {
-            console.log(options);
+            await fetchAllContractsInRegistry(options);
+            process.exit(0);
+        });
 
-            const network = options.network.length === 0 ? 'mainnet' : options.network;
+    program
+        .command('get-entry <nameOrId>')
+        .option('-n, --network <network>', 'Specify network we are calling (defaults to L1)', [])
+        .description('Return current state for the entry')
+        .action(async (nameOrId, options) => {
+            console.log((await getFullEntryData(nameOrId, options)));
+            process.exit(0);
+        });
 
-            await fetchAllContractsInRegistry(network);
+    program
+        .command('get-entry-history <nameOrId>')
+        .option('-n, --network <network>', 'Specify network we are calling (defaults to L1)', [])
+        .description('Returns history of changes for the entry')
+        .action(async (nameOrId, options) => {
+            await getEntryHistory(nameOrId, options);
             process.exit(0);
         });
 
