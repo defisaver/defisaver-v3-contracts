544793f98fdced80420b9fe0342ebab4e706547c
Hajduk Dušan <hajdukdusan99@gmail.com>
2022-08-30 14:35:55 +0200

    Update compV3-claim Test

--- test/compoundV3/compV3-claim.js
+++ test/compoundV3/compV3-claim.js
@@ -8,6 +8,7 @@ const {
     getProxy,
     WETH_ADDRESS,
     send,
+    setBalance,
 } = require('../utils');
 const { COMP_ADDR } = require('../utils-comp');
 
@@ -20,30 +21,43 @@ describe('CompV3-Claim', function () {
     before(async () => {
         await redeploy('CompV3Supply');
         await redeploy('CompV3Claim');
-
+        await redeploy('CompV3Borrow');
         senderAcc = (await hre.ethers.getSigners())[0];
         proxy = await getProxy(senderAcc.address);
     });
 
     it('... claim usdc tokens for proxy account', async () => {
-
         // base min for rewards is 1000000000000
-        const amount = hre.ethers.utils.parseUnits('10', 18);
+        const amount = hre.ethers.utils.parseUnits('1000', 6);
 
-        await supplyCompV3(proxy, WETH_ADDRESS, amount, senderAcc.address);
+        const COMET_REWARDS_ADDR = '0x1B0e765F6224C21223AeA2af16c1C46E38885a40';
+        const COMET_ADDR = '0xc3d688B66703497DAA19211EEdff47f25384cdc3';
+
+        const abi = [
+            'function getRewardOwed(address, address) public view returns(address, uint256)',
+        ];
+
+        const CometRewardsContract = new ethers.Contract(COMET_REWARDS_ADDR, abi, senderAcc)
+
+        await setBalance(COMP_ADDR, COMET_REWARDS_ADDR, hre.ethers.utils.parseUnits('1000000000', 18));
+
+        await supplyCompV3(proxy, USDC_ADDR, amount, senderAcc.address);
+
+        await network.provider.send("evm_increaseTime", [36000]);
+        await network.provider.send("evm_mine");
+
+        const tx = await CometRewardsContract.callStatic.getRewardOwed(COMET_ADDR, proxy.address)
+        console.log(tx.toString())
 
         const BalanceBefore = await balanceOf(COMP_ADDR, senderAcc.address);
         const BalanceProxyBefore = await balanceOf(COMP_ADDR, proxy.address);
 
-        // const COMET_REWARDS_ADDR = '0x1B0e765F6224C21223AeA2af16c1C46E38885a40';
-        // await send(COMP_ADDR, COMET_REWARDS_ADDR, hre.ethers.utils.parseUnits('10', 18));
-
-        await claimCompV3(proxy, proxy.address, senderAcc.address, false);
+        await claimCompV3(proxy, proxy.address, senderAcc.address, true);
 
         const BalanceAfter = await balanceOf(COMP_ADDR, senderAcc.address);
         const BalanceProxyAfter = await balanceOf(COMP_ADDR, proxy.address);
 
         expect(BalanceProxyAfter).to.be.eq(BalanceProxyBefore);
-        expect(BalanceAfter).to.be.gte(BalanceBefore);
+        expect(BalanceAfter).to.be.gt(BalanceBefore);
     });
 });
