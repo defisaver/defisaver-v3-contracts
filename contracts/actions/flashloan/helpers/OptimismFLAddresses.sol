e642f7488354b38648559ad319acd4a70f83e810
Nenad Palinkasevic <nesa993@gmail.com>
2022-09-22 12:35:17 +0200

    feat: update new addresses FL and AaveSubProxy

--- contracts/actions/flashloan/helpers/OptimismFLAddresses.sol
+++ contracts/actions/flashloan/helpers/OptimismFLAddresses.sol
@@ -8,6 +8,8 @@ contract OptimismFLAddresses {
     address internal constant DYDX_FL_FEE_FAUCET = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     address internal constant DSS_FLASH_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     address internal constant ST_ETH_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address internal constant EULER_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address internal constant EULER_MARKET_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
     address public constant WETH_ADDR = 0x4200000000000000000000000000000000000006;
     address internal constant DAI_ADDR = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1;
