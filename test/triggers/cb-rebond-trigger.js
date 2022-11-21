a1ac2f648ad2959aa6d712a1ad46edc360a554b0
majkic99 <vrnebojsamajkic@gmail.com>
2022-11-21 14:24:51 +0700

    style: stylise tests in better order

--- test/triggers/cb-rebond-trigger.js
+++ test/triggers/cb-rebond-trigger.js
@@ -29,7 +29,7 @@ describe('CB-rebond-trigger', function () {
     });
 
     for (let i = 0; i < amounts.length; ++i) {
-        it('... should get optimal rebond time when using BLUSDCap while checking TPI', async () => {
+        it('... should get optimal rebond time when using BLUSDCap', async () => {
             const lusdAmount = amounts[i];
             const lusdAmountWei = hre.ethers.utils.parseUnits(lusdAmount, 18);
             const cbManagerAddress = await chickenBondsView.CBManager();
@@ -58,49 +58,12 @@ describe('CB-rebond-trigger', function () {
             );
 
             const rebondAmount = calcAccruedAmountForMs(systemInfo, blusdCap, rebondMs);
-            console.log('rebondAmount calc from js using blusdcap: ', rebondAmount.toString());
+            console.log('rebondAmount calc from js using BLUSDCap: ', rebondAmount.toString());
 
             const optimalLusdAmount = await cbRebondTrigger.getOptimalLusdAmount(blusdCap, blusdCap);
-            console.log('rebondAmount calc from sol using blusdcap: ', optimalLusdAmount[0] / 1e18);
+            console.log('rebondAmount calc from sol using BLUSDCap: ', optimalLusdAmount[0] / 1e18);
         });
         it('... should get optimal rebond time while using accrued BLUSD', async () => {
-            const lusdAmount = amounts[i];
-            const lusdAmountWei = hre.ethers.utils.parseUnits(lusdAmount, 18);
-            const cbManagerAddress = await chickenBondsView.CBManager();
-            const cbManager = await hre.ethers.getContractAt('IChickenBondManager', cbManagerAddress);
-            const backingRatio = await cbManager.calcSystemBackingRatio();
-            const chickenInFee = await cbManager.CHICKEN_IN_AMM_FEE();
-
-            const chickenInFeeAmount = (lusdAmountWei.mul(chickenInFee.toString()).div(1e18.toString())).toString();
-            let bondAmountMinusChickenInFee = (lusdAmountWei.sub(chickenInFeeAmount.toString())).toString();
-            bondAmountMinusChickenInFee = hre.ethers.utils.parseUnits(bondAmountMinusChickenInFee, 0);
-
-            const blusdCap = (bondAmountMinusChickenInFee.mul(1e18.toString()).div(backingRatio.toString())).toString();
-
-            const systemInfo = await getSystemInfo(chickenBondsView);
-
-            const floorPrice = new Dec(systemInfo.totalReserveLUSD).div(systemInfo.bLUSDSupply).toString();
-
-            const testMarketNoTPI = (hre.ethers.utils.parseUnits('1', 18)).toString();
-
-            const marketPrice = (await cbRebondTrigger.getBLusdPriceFromCurve(testMarketNoTPI)) / 1e18;
-            systemInfo.marketPrice = marketPrice;
-
-            const marketPricePremium = calcCBondsBLUSDMarketPremium(floorPrice, marketPrice);
-
-            const rebondMs = calcRebondMs(
-                systemInfo.accrualParameter,
-                marketPricePremium,
-                systemInfo.chickenInAMMFee,
-            );
-
-            const rebondAmount = calcAccruedAmountForMs(systemInfo, blusdCap, rebondMs);
-            console.log('rebondAmount calc from js when not counting TPI: ', rebondAmount.toString());
-
-            const optimalLusdAmount = await cbRebondTrigger.getOptimalLusdAmount(blusdCap, testMarketNoTPI);
-            console.log('rebondAmount calc from sol when not counting TPI: ', optimalLusdAmount[0] / 1e18);
-        });
-        it('... should get optimal rebond time while not counting TPI', async () => {
             const lusdAmount = amounts[i];
             const lusdAmountWei = hre.ethers.utils.parseUnits(lusdAmount, 18);
             const cbManagerAddress = await chickenBondsView.CBManager();
@@ -141,6 +104,44 @@ describe('CB-rebond-trigger', function () {
 
             const optimalLusdAmount = await cbRebondTrigger.getOptimalLusdAmount(blusdCap, breakEvenBLusdAmount);
             console.log('rebondAmount calc from sol at moment of breakeven: ', optimalLusdAmount[0] / 1e18);
+        });
+
+        it('... should get optimal rebond time not counting TPI', async () => {
+            const lusdAmount = amounts[i];
+            const lusdAmountWei = hre.ethers.utils.parseUnits(lusdAmount, 18);
+            const cbManagerAddress = await chickenBondsView.CBManager();
+            const cbManager = await hre.ethers.getContractAt('IChickenBondManager', cbManagerAddress);
+            const backingRatio = await cbManager.calcSystemBackingRatio();
+            const chickenInFee = await cbManager.CHICKEN_IN_AMM_FEE();
+
+            const chickenInFeeAmount = (lusdAmountWei.mul(chickenInFee.toString()).div(1e18.toString())).toString();
+            let bondAmountMinusChickenInFee = (lusdAmountWei.sub(chickenInFeeAmount.toString())).toString();
+            bondAmountMinusChickenInFee = hre.ethers.utils.parseUnits(bondAmountMinusChickenInFee, 0);
+
+            const blusdCap = (bondAmountMinusChickenInFee.mul(1e18.toString()).div(backingRatio.toString())).toString();
+
+            const systemInfo = await getSystemInfo(chickenBondsView);
+
+            const floorPrice = new Dec(systemInfo.totalReserveLUSD).div(systemInfo.bLUSDSupply).toString();
+
+            const testMarketNoTPI = (hre.ethers.utils.parseUnits('1', 18)).toString();
+
+            const marketPrice = (await cbRebondTrigger.getBLusdPriceFromCurve(testMarketNoTPI)) / 1e18;
+            systemInfo.marketPrice = marketPrice;
+
+            const marketPricePremium = calcCBondsBLUSDMarketPremium(floorPrice, marketPrice);
+
+            const rebondMs = calcRebondMs(
+                systemInfo.accrualParameter,
+                marketPricePremium,
+                systemInfo.chickenInAMMFee,
+            );
+
+            const rebondAmount = calcAccruedAmountForMs(systemInfo, blusdCap, rebondMs);
+            console.log('rebondAmount calc from js when not counting TPI: ', rebondAmount.toString());
+
+            const optimalLusdAmount = await cbRebondTrigger.getOptimalLusdAmount(blusdCap, testMarketNoTPI);
+            console.log('rebondAmount calc from sol when not counting TPI: ', optimalLusdAmount[0] / 1e18);
         });
     }
 });
