f354c1dba070c9590aac73e4d40e53f8132bac2a
majkic99 <vrnebojsamajkic@gmail.com>
2022-11-25 16:03:09 +0700

    feat: add impact check on rebond update action

--- contracts/actions/liquity/bonds/CBUpdateRebondSub.sol
+++ contracts/actions/liquity/bonds/CBUpdateRebondSub.sol
@@ -10,11 +10,16 @@ import "../../../actions/liquity/helpers/CBHelper.sol";
 /// @title Special action to update rebond strategy data (Only use in that context)
 contract CBUpdateRebondSub is ActionBase, CBHelper {
 
+    error SubDatHashMismatch(uint256, bytes32, bytes32);
+    error ImpactTooHigh();
+
     /// @param subId Id of the sub we are changing (user must be owner)
     /// @param bondId Id of the chicken bond NFT we just created
+    /// @param previousBondId Id of the chicken bond NFT that was chickened In
     struct Params {
         uint256 subId;
-        uint256 bondId;
+        uint256 newBondId;
+        uint256 previousBondId;
     }
 
     /// @inheritdoc ActionBase
@@ -33,8 +38,8 @@ contract CBUpdateRebondSub is ActionBase, CBHelper {
             _returnValues
         );
 
-        params.bondId = _parseParamUint(
-            params.bondId,
+        params.newBondId = _parseParamUint(
+            params.newBondId,
             _paramMapping[1],
             _subData,
             _returnValues
@@ -59,9 +64,26 @@ contract CBUpdateRebondSub is ActionBase, CBHelper {
     //////////////////////////// ACTION LOGIC ////////////////////////////
 
     function updateRebondSub(Params memory _params) internal {
-        StrategyModel.StrategySub memory rebondSub = formatRebondSub(_params.subId, _params.bondId);
+        if (_params.previousBondId != 0) {
+            StrategyModel.StoredSubData memory storedCBSubData = SubStorage(SUB_STORAGE_ADDR).getSub(_params.subId);
+            StrategyModel.StrategySub memory previousRebondSub = formatRebondSub(_params.subId, _params.previousBondId);
+            bytes32 cbSubDataHash = keccak256(abi.encode(previousRebondSub));
 
-        SubStorage(SUB_STORAGE_ADDR).updateSubData(_params.subId, rebondSub);
+            // data sent from the caller must match the stored hash of the data
+            if (cbSubDataHash != storedCBSubData.strategySubHash) {
+                revert SubDatHashMismatch(_params.subId, cbSubDataHash, storedCBSubData.strategySubHash);
+            }
+
+            uint256 previousBondLUSDDeposited = CBManager.getBondData(_params.previousBondId).lusdAmount;
+            uint256 newBondLUSDDeposited = CBManager.getBondData(_params.newBondId).lusdAmount;
+
+            if (newBondLUSDDeposited <= previousBondLUSDDeposited) {
+                revert ImpactTooHigh();
+            }
+        }
+        StrategyModel.StrategySub memory newRebondSub = formatRebondSub(_params.subId, _params.newBondId);
+
+        SubStorage(SUB_STORAGE_ADDR).updateSubData(_params.subId, newRebondSub);
     }
 
     function parseInputs(bytes memory _callData) public pure returns (Params memory params) {
