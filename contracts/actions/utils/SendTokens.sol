5c12e7f5492ce6273706e261643de0daffc3399b
majkic99 <vrnebojsamajkic@gmail.com>
2022-12-01 17:26:29 +0100

    feat: create send tokens action

---
+++ contracts/actions/utils/SendTokens.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.10;
+
+import "../../utils/TokenUtils.sol";
+import "../ActionBase.sol";
+
+/// @title Helper action to send a token to the specified address
+contract SendTokens is ActionBase {
+
+    using TokenUtils for address;
+
+    struct Params {
+        address[] tokens;
+        address[] receivers;
+        uint256[] amounts;
+    }
+
+    /// @inheritdoc ActionBase
+    function executeAction(
+        bytes memory _callData,
+        bytes32[] memory _subData,
+        uint8[] memory _paramMapping,
+        bytes32[] memory _returnValues
+    ) public virtual payable override returns (bytes32) {
+        (Params memory inputData, uint256 arrayLength) = parseInputs(_callData);
+        
+        for (uint256 i = 0; i < arrayLength; i++){
+            inputData.tokens[i] = _parseParamAddr(inputData.tokens[i], _paramMapping[i], _subData, _returnValues);
+            inputData.receivers[i] = _parseParamAddr(inputData.receivers[i], _paramMapping[arrayLength + i], _subData, _returnValues);
+            inputData.amounts[i] = _parseParamUint(inputData.amounts[i], _paramMapping[2 * arrayLength + i], _subData, _returnValues);
+        }
+
+        _sendTokens(inputData, arrayLength);
+
+        return bytes32(0);
+    }
+
+    /// @inheritdoc ActionBase
+    function executeActionDirect(bytes memory _callData) public payable override {
+        (Params memory inputData, uint256 arrayLength) = parseInputs(_callData);
+
+        _sendTokens(inputData, arrayLength);
+    }
+
+    /// @inheritdoc ActionBase
+    function actionType() public virtual override pure returns (uint8) {
+        return uint8(ActionType.STANDARD_ACTION);
+    }
+
+
+    //////////////////////////// ACTION LOGIC ////////////////////////////
+    
+
+    /// @notice Sends tokens to the specified addresses, works with Eth also
+    function _sendTokens(Params memory _inputData, uint256 arrayLength) internal {
+        for (uint256 i = 0; i < arrayLength; i++){
+            _inputData.tokens[i].withdrawTokens(_inputData.receivers[i], _inputData.amounts[i]);
+        }
+
+    }
+
+    function parseInputs(bytes memory _callData) public pure returns (Params memory params, uint256 arrayLength) {
+        params = abi.decode(_callData, (Params));
+        require(params.tokens.length == params.receivers.length);
+        require(params.tokens.length == params.amounts.length);
+        arrayLength = params.tokens.length;
+    }
+}
---
+++ test/utils-actions/send-tokens.js
@@ -0,0 +1,19 @@
+const {
+    redeploy,
+} = require('../utils');
+const { sendTokensTest } = require('./utils-actions-tests');
+
+describe('Send-Token', function () {
+    this.timeout(80000);
+
+    before(async () => {
+        await redeploy('SendTokens');
+        await redeploy('WrapEth');
+        await redeploy('SendToken');
+        await redeploy('RecipeExecutor');
+    });
+
+    it('... should send tokens using SendTokens action and compare gas prices', async () => {
+        await sendTokensTest();
+    });
+});
