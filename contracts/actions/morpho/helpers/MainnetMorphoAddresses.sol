4dc11becc2e39812191ea212dca7bcf7632cb6a4
kjaredic <kjaredic@protonmail.com>
2023-01-16 14:50:13 +0100

    feat: add Morpho claim action

--- contracts/actions/morpho/helpers/MainnetMorphoAddresses.sol
+++ contracts/actions/morpho/helpers/MainnetMorphoAddresses.sol
@@ -2,7 +2,9 @@
 pragma solidity =0.8.10;
 
 contract MainnetMorphoAddresses {
+    address public constant MORPHO_TOKEN_ADDR = 0x9994E35Db50125E0DF82e4c2dde62496CE330999;
     address public constant MORPHO_AAVEV2_ADDR = 0x777777c9898D384F785Ee44Acfe945efDFf5f3E0;
     address public constant MORPHO_AAVEV2_LENS_ADDR = 0x507fA343d0A90786d86C7cd885f5C49263A91FF4;
+    address public constant REWARDS_DISTRIBUTOR_ADDR = 0x3B14E5C73e0A56D607A8688098326fD4b4292135;
     address public constant DEFAULT_MARKET_DATA_PROVIDER = 0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d;
 }
