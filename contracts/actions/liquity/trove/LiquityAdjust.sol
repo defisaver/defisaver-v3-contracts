82dc9c8b69ee3eed47096f3e4152c1e568db1ba9
Nenad Palinkasevic <nesa993@gmail.com>
2023-01-17 13:29:15 +0100

    feat: add prototype of LiquitAdjust action

---
+++ contracts/actions/liquity/trove/LiquityAdjust.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.10;
+
+import "../helpers/LiquityHelper.sol";
+import "../../../utils/TokenUtils.sol";
+import "../../ActionBase.sol";
+
+contract LiquityAdjust is ActionBase, LiquityHelper {
+    using TokenUtils for address;
+
+    enum CollChange { SUPPLY, WITHDRAW }
+    enum DebtChange { PAYBACK, BORROW }
+
+    struct Params {
+        uint256 maxFeePercentage;   // Highest borrowing fee to accept, ranges between 0.5 and 5%
+        uint256 collAmount;         // Amount of ETH to supply/withdraw
+        uint256 lusdAmount;         // Amount of LUSD tokens to borrow/payback
+        CollChange collChange;      // Whether to supply or withdraw collateral
+        DebtChange debtChange;      // Whether to borrow or payback debt
+        address from;               // Address where to pull the tokens from
+        address to;                 // Address that will receive the tokens
+        address upperHint;
+        address lowerHint;
+    }
+
+    /// @inheritdoc ActionBase
+    function executeAction(
+        bytes memory _callData,
+        bytes32[] memory _subData,
+        uint8[] memory _paramMapping,
+        bytes32[] memory _returnValues
+    ) public payable virtual override returns (bytes32) {
+        Params memory params = parseInputs(_callData);
+
+        params.maxFeePercentage = _parseParamUint(
+            params.maxFeePercentage,
+            _paramMapping[0],
+            _subData,
+            _returnValues
+        );
+        params.lusdAmount = _parseParamUint(
+            params.lusdAmount,
+            _paramMapping[1],
+            _subData,
+            _returnValues
+        );
+        params.to = _parseParamAddr(params.to, _paramMapping[2], _subData, _returnValues);
+
+        (uint256 borrowedAmount, bytes memory logData) = _liquityAdjust(params);
+        emit ActionEvent("LiquityAdjust", logData);
+        return bytes32(borrowedAmount);
+    }
+
+    /// @inheritdoc ActionBase
+    function executeActionDirect(bytes memory _callData) public payable virtual override {
+        Params memory params = parseInputs(_callData);
+        (, bytes memory logData) = _liquityAdjust(params);
+        logger.logActionDirectEvent("LiquityAdjust", logData);
+    }
+
+    /// @inheritdoc ActionBase
+    function actionType() public pure virtual override returns (uint8) {
+        return uint8(ActionType.STANDARD_ACTION);
+    }
+
+    //////////////////////////// ACTION LOGIC ////////////////////////////
+
+    function _liquityAdjust(Params memory _params) internal returns (uint256, bytes memory) {
+        uint collAmount = 0;
+
+        if (_params.collChange == CollChange.SUPPLY) {
+            if (_params.collAmount == type(uint256).max) {
+                _params.collAmount = TokenUtils.WETH_ADDR.getBalance(_params.from);
+            }
+
+            TokenUtils.WETH_ADDR.pullTokensIfNeeded(_params.from, _params.collAmount);
+            TokenUtils.withdrawWeth(_params.collAmount);
+
+            collAmount = _params.collAmount;
+        }
+
+        if (_params.debtChange == DebtChange.PAYBACK) {
+        //     if (_params.lusdAmount == type(uint256).max) {
+        //         _params.lusdAmount = lusdToken.balanceOf(_params.from);
+        //     }
+
+        //     lusdToken.pullTokensIfNeeded(_params.from, _params.lusdAmount);
+        }
+
+        BorrowerOperations.adjustTrove{value: collAmount}(
+            _params.maxFeePercentage,
+            _params.collAmount,
+            _params.lusdAmount,
+            _params.debtChange == DebtChange.BORROW,
+            _params.upperHint,
+            _params.lowerHint
+        );
+
+        if (_params.collChange == CollChange.WITHDRAW) {
+            TokenUtils.depositWeth(_params.collAmount);
+            TokenUtils.WETH_ADDR.withdrawTokens(_params.to, _params.collAmount);
+        }
+
+        if (_params.debtChange == DebtChange.BORROW) {
+            LUSD_TOKEN_ADDRESS.withdrawTokens(_params.to, _params.lusdAmount);
+        }
+
+        bytes memory logData = abi.encode(_params.maxFeePercentage, _params.lusdAmount, _params.to);
+        return (_params.lusdAmount, logData);
+    }
+
+    function parseInputs(bytes memory _callData) public pure returns (Params memory _params) {
+        _params = abi.decode(_callData, (Params));
+    }
+}
