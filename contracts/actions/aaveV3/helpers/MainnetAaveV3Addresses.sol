ca85a6dfa97f0a7ad5c7e2d99f74cddba254ecc2
Nenad Palinkasevic <nesa993@gmail.com>
2023-01-20 12:45:29 +0100

    feat: add mainnet aaveV3 addresses and deploy script

--- contracts/actions/aaveV3/helpers/MainnetAaveV3Addresses.sol
+++ contracts/actions/aaveV3/helpers/MainnetAaveV3Addresses.sol
@@ -3,9 +3,11 @@
 pragma solidity =0.8.10;
 
 contract MainnetAaveV3Addresses {
-    // TODO: add all addresses
-    address internal constant REWARDS_CONTROLLER_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address internal constant DEFAULT_AAVE_MARKET = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address internal constant REWARDS_CONTROLLER_ADDRESS = 0x8164Cc65827dcFe994AB23944CBC90e0aa80bFcb;
+    address internal constant DEFAULT_AAVE_MARKET = 0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e;
+    address internal constant AAVE_ORACLE_V3 = 0x54586bE62E3c3580375aE3723C145253060Ca0C2;
+
+    // Not used in the current implementation
     address internal constant TRANSIENT_STORAGE = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address internal constant AAVE_ORACLE_V3 = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
 }
---
+++ scripts/deploy-aaveV3.js
@@ -0,0 +1,39 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+const hre = require('hardhat');
+const { start } = require('./utils/starter');
+
+const { redeploy, addrs, network } = require('../test/utils');
+
+const { topUp } = require('./utils/fork');
+
+async function main() {
+    const senderAcc = (await hre.ethers.getSigners())[0];
+    await topUp(senderAcc.address);
+
+    const aaveV3ATokenPayback = await redeploy('AaveV3ATokenPayback', addrs[network].REGISTRY_ADDR, true, true);
+    const aaveV3Borrow = await redeploy('AaveV3Borrow', addrs[network].REGISTRY_ADDR, true, true);
+    const aaveV3ClaimRewards = await redeploy('AaveV3ClaimRewards', addrs[network].REGISTRY_ADDR, true, true);
+    const aaveV3CollateralSwitch = await redeploy('AaveV3CollateralSwitch', addrs[network].REGISTRY_ADDR, true, true);
+    const aaveV3Payback = await redeploy('AaveV3Payback', addrs[network].REGISTRY_ADDR, true, true);
+    const aaveV3SetEMode = await redeploy('AaveV3SetEMode', addrs[network].REGISTRY_ADDR, true, true);
+    const aaveV3Supply = await redeploy('AaveV3Supply', addrs[network].REGISTRY_ADDR, true, true);
+    const aaveV3SwapBorrowRateMode = await redeploy('AaveV3SwapBorrowRateMode', addrs[network].REGISTRY_ADDR, true, true);
+    const aaveV3Withdraw = await redeploy('AaveV3Withdraw', addrs[network].REGISTRY_ADDR, true, true);
+    const aaveV3View = await redeploy('AaveV3View', addrs[network].REGISTRY_ADDR, true, true);
+
+    console.log(`AaveV3ATokenPayback: ${aaveV3ATokenPayback.address}`);
+    console.log(`AaveV3Borrow: ${aaveV3Borrow.address}`);
+    console.log(`AaveV3ClaimRewards: ${aaveV3ClaimRewards.address}`);
+    console.log(`AaveV3CollateralSwitch: ${aaveV3CollateralSwitch.address}`);
+    console.log(`AaveV3Payback: ${aaveV3Payback.address}`);
+    console.log(`AaveV3SetEMode: ${aaveV3SetEMode.address}`);
+    console.log(`AaveV3Supply: ${aaveV3Supply.address}`);
+    console.log(`AaveV3SwapBorrowRateMode: ${aaveV3SwapBorrowRateMode.address}`);
+    console.log(`AaveV3Withdraw: ${aaveV3Withdraw.address}`);
+    console.log(`AaveV3View: ${aaveV3View.address}`);
+
+    process.exit(0);
+}
+
+start(main);
