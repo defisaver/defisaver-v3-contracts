5ba58d1379cbc9aac52859af0756d7a37dfa2f53
kjaredic <kjaredic@protonmail.com>
2023-03-07 14:24:32 +0100

    feat: add LiquityRatioCheck action and update LiquityRatioTrigger

--- contracts/triggers/LiquityRatioTrigger.sol
+++ contracts/triggers/LiquityRatioTrigger.sol
@@ -5,9 +5,12 @@ pragma solidity =0.8.10;
 import "../auth/AdminAuth.sol";
 import "../actions/liquity/helpers/LiquityRatioHelper.sol";
 import "../interfaces/ITrigger.sol";
+import "../utils/TransientStorage.sol";
 
 /// @title Trigger contract that verifies if current Liquity position ratio went over/under the subbed ratio
 contract LiquityRatioTrigger is ITrigger, AdminAuth, LiquityRatioHelper {
+
+    TransientStorage public constant tempStorage = TransientStorage(TRANSIENT_STORAGE);
 
     enum RatioState { OVER, UNDER }
 
@@ -30,6 +33,8 @@ contract LiquityRatioTrigger is ITrigger, AdminAuth, LiquityRatioHelper {
         (uint256 currRatio, bool isActive) = getRatio(triggerSubData.troveOwner);
         
         if (isActive == false) return false;
+
+        tempStorage.setBytes32("LIQUITY_RATIO", bytes32(currRatio));
         
         if (RatioState(triggerSubData.state) == RatioState.OVER) {
             if (currRatio > triggerSubData.ratio) return true;
