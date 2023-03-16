b055fd6e80cc2507ee4c0ac8eddfbfc401d73191
Nenad Palinkasevic <nesa993@gmail.com>
2023-03-16 10:25:30 +0100

    refactor: move Morpho Market mapping from view contract and add deploy script

--- contracts/views/MorphoAaveV3View.sol
+++ contracts/views/MorphoAaveV3View.sol
@@ -2,22 +2,7 @@
 pragma solidity =0.8.10;
 
 import "../actions/morpho/helpers/MorphoAaveV3Helper.sol";
-import "../auth/AdminAuth.sol";
 
-contract MorphoAaveV2View is MorphoAaveV3Helper, AdminAuth {
+contract MorphoAaveV2View is MorphoAaveV3Helper {
 
-    mapping (uint256 => address) public morphoAddresses;
-
-    constructor(address _morphoGeneralAddress, address _morphoEthAddress) {
-        morphoAddresses[0] = _morphoGeneralAddress;
-        morphoAddresses[1] = _morphoEthAddress;
-    }
-
-    function getMorphoAddress(uint256 _emodeId) public view returns (address) {
-        return morphoAddresses[_emodeId];
-    }
-
-    function addNewMorphoAddress(uint256 _emodeId, address _morphoAddress) public onlyOwner {
-        morphoAddresses[_emodeId] = _morphoAddress;
-    }
 }
