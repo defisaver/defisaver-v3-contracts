d299d94cbce4169a0ff468204a817fdf61bf43dc
majkic99 <vrnebojsamajkic@gmail.com>
2023-05-24 14:05:46 +0200

    feat: add view function for detailed trigger view (multicall use)

--- contracts/views/StrategyTriggerView.sol
+++ contracts/views/StrategyTriggerView.sol
@@ -6,9 +6,7 @@ import "../core/strategy/BundleStorage.sol";
 import "../core/strategy/StrategyStorage.sol";
 import "../interfaces/ITrigger.sol";
 
-
 contract StrategyTriggerView is StrategyModel, CoreHelper {
-
     DFSRegistry public constant registry = DFSRegistry(REGISTRY_ADDR);
 
     function checkTriggers(
@@ -17,11 +15,15 @@ contract StrategyTriggerView is StrategyModel, CoreHelper {
     ) public returns (bool) {
         Strategy memory strategy;
 
-        { // to handle stack too deep
+        {
+            // to handle stack too deep
             uint256 strategyId = _sub.strategyOrBundleId;
 
             if (_sub.isBundle) {
-                strategyId = BundleStorage(BUNDLE_STORAGE_ADDR).getStrategyId(_sub.strategyOrBundleId, 0);
+                strategyId = BundleStorage(BUNDLE_STORAGE_ADDR).getStrategyId(
+                    _sub.strategyOrBundleId,
+                    0
+                );
             }
 
             strategy = StrategyStorage(STRATEGY_STORAGE_ADDR).getStrategy(strategyId);
@@ -41,5 +43,55 @@ contract StrategyTriggerView is StrategyModel, CoreHelper {
             if (!isTriggered) return false;
         }
         return true;
+    }
+
+    enum TriggerStatus {
+        TRUE,
+        FALSE,
+        REVERT
+    }
+
+    function checkTriggersDetailed(
+        StrategySub memory _sub,
+        bytes[] calldata _triggerCallData
+    ) public returns (TriggerStatus[] memory) {
+        Strategy memory strategy;
+
+        {
+            // to handle stack too deep
+            uint256 strategyId = _sub.strategyOrBundleId;
+
+            if (_sub.isBundle) {
+                strategyId = BundleStorage(BUNDLE_STORAGE_ADDR).getStrategyId(
+                    _sub.strategyOrBundleId,
+                    0
+                );
+            }
+
+            strategy = StrategyStorage(STRATEGY_STORAGE_ADDR).getStrategy(strategyId);
+        }
+
+        bytes4[] memory triggerIds = strategy.triggerIds;
+
+        address triggerAddr;
+
+        TriggerStatus[] memory triggerStatuses = new TriggerStatus[](strategy.triggerIds.length);
+
+        for (uint256 i = 0; i < triggerIds.length; i++) {
+            triggerAddr = registry.getAddr(triggerIds[i]);
+            try
+                ITrigger(triggerAddr).isTriggered(_triggerCallData[i], _sub.triggerData[i])
+            returns (bool isTriggered) {
+                if (isTriggered) {
+                    triggerStatuses[i] = TriggerStatus.TRUE;
+                } else {
+                    triggerStatuses[i] = TriggerStatus.FALSE;
+                }
+            } catch {
+                triggerStatuses[i] = TriggerStatus.REVERT;
+            }
+        }
+
+        return triggerStatuses;
     }
 }
