0c545c1590b2a5919831c45a8c22549375d78d0f
kjaredic <kjaredic@protonmail.com>
2023-07-26 13:16:45 +0200

    test: update curveusd borrow rate trigger test

--- test-sol/triggers/TestCurveUsdBorrowRateTrigger.t.sol
+++ test-sol/triggers/TestCurveUsdBorrowRateTrigger.t.sol
@@ -2,15 +2,10 @@
 pragma solidity =0.8.10;
 
 import "ds-test/test.sol";
-import "../CheatCodes.sol";
-
 import "../../contracts/triggers/CurveUsdBorrowRateTrigger.sol";
-import "../utils/Tokens.sol";
-import "../utils/CompUser.sol";
-import "../TokenAddresses.sol";
 
 
-contract TestCurveUsdBorrowRateTrigger is DSTest, DSMath, Tokens {
+contract TestCurveUsdBorrowRateTrigger is DSTest {
     CurveUsdBorrowRateTrigger trigger;
 
     address WSTETH_MARKET = 0x100dAa78fC509Db39Ef7D04DE0c1ABD299f4C6CE;
@@ -19,12 +14,50 @@ contract TestCurveUsdBorrowRateTrigger is DSTest, DSMath, Tokens {
         trigger = new CurveUsdBorrowRateTrigger();
     }
 
-    function testGetSafetyRatio() public {
-        console.log("OKEEEE");
-
+    function testWstethRateSub() public {
         uint256 rate = trigger._calcBorrowRate(WSTETH_MARKET);
 
-        console.log(rate);
-    }
+        CurveUsdBorrowRateTrigger.SubParams memory subEqualOverFail = CurveUsdBorrowRateTrigger.SubParams(
+            WSTETH_MARKET,
+            rate,
+            uint8(CurveUsdBorrowRateTrigger.TargetRateState.OVER)
+        );
 
+        CurveUsdBorrowRateTrigger.SubParams memory subEqualUnderFail = CurveUsdBorrowRateTrigger.SubParams(
+            WSTETH_MARKET,
+            rate,
+            uint8(CurveUsdBorrowRateTrigger.TargetRateState.UNDER)
+        );
+
+        CurveUsdBorrowRateTrigger.SubParams memory subOver = CurveUsdBorrowRateTrigger.SubParams(
+            WSTETH_MARKET,
+            rate - 1,
+            uint8(CurveUsdBorrowRateTrigger.TargetRateState.OVER)
+        );
+
+        CurveUsdBorrowRateTrigger.SubParams memory subOverFail = CurveUsdBorrowRateTrigger.SubParams(
+            WSTETH_MARKET,
+            rate + 1,
+            uint8(CurveUsdBorrowRateTrigger.TargetRateState.OVER)
+        );
+
+        CurveUsdBorrowRateTrigger.SubParams memory subUnder = CurveUsdBorrowRateTrigger.SubParams(
+            WSTETH_MARKET,
+            rate + 1,
+            uint8(CurveUsdBorrowRateTrigger.TargetRateState.UNDER)
+        );
+
+        CurveUsdBorrowRateTrigger.SubParams memory subUnderFail = CurveUsdBorrowRateTrigger.SubParams(
+            WSTETH_MARKET,
+            rate - 1,
+            uint8(CurveUsdBorrowRateTrigger.TargetRateState.UNDER)
+        );
+
+        assertTrue(trigger.isTriggered(abi.encode(), abi.encode(subEqualOverFail)) == false);
+        assertTrue(trigger.isTriggered(abi.encode(), abi.encode(subEqualUnderFail)) == false);
+        assertTrue(trigger.isTriggered(abi.encode(), abi.encode(subOver)) == true);
+        assertTrue(trigger.isTriggered(abi.encode(), abi.encode(subOverFail)) == false);
+        assertTrue(trigger.isTriggered(abi.encode(), abi.encode(subUnder)) == true);
+        assertTrue(trigger.isTriggered(abi.encode(), abi.encode(subUnderFail)) == false);
+    }
 }
