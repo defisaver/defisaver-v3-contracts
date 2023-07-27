ce20509dcd034a65157b6fff662d1ba24b3c1196
kjaredic <kjaredic@protonmail.com>
2023-07-27 17:29:24 +0200

    fix: reimburse aave v2 position holders with up to 2 wei in AaveWithdraw
    Aave has a bug in supply logic such that the user balance is sometimes off by one wei
    This commit fixes that so that the migration recipe doesnt revert at withdraw

--- contracts/actions/aave/helpers/MainnetAaveAddresses.sol
+++ contracts/actions/aave/helpers/MainnetAaveAddresses.sol
@@ -5,4 +5,5 @@ pragma solidity =0.8.10;
 contract MainnetAaveAddresses {
     address internal constant STAKED_CONTROLLER_ADDR = 0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5;
     address internal constant STAKED_TOKEN_ADDR = 0x4da27a545c0c5B758a6BA100e3a049001de870f5;
+    address internal constant DYDX_FL_FEE_FAUCET = 0x47f159C90850D5cE09E21F931d504536840f34b4;
 }
