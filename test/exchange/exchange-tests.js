cf8579c18ed04609a44e0b04f0e409eb4bb2327b
majkic99 <vrnebojsamajkic@gmail.com>
2023-08-14 15:42:12 +0200

    test: test 1inch further

--- test/exchange/exchange-tests.js
+++ test/exchange/exchange-tests.js
@@ -638,24 +638,32 @@ const oneInchTest = async () => {
             buyAssetInfo = getAssetInfo('USDC');
 
             buyBalanceBefore = await balanceOf(buyAssetInfo.address, senderAcc.address);
-            amount = hre.ethers.utils.parseUnits('1', 18);
-            await depositToWeth(amount);
+            amount = hre.ethers.utils.parseUnits('10', 18);
+            await setBalance(sellAssetInfo.address, senderAcc.address, amount);
             await approve(sellAssetInfo.address, proxy.address);
+
+            const protocols = 'UNISWAP_V1,UNISWAP_V3,UNISWAP_V2,SUSHI,MOONISWAP,BALANCER,COMPOUND,CURVE,CURVE_V2_SPELL_2_ASSET,CURVE_V2_SGT_2_ASSET,CURVE_V2_THRESHOLDNETWORK_2_ASSET,CHAI,OASIS,KYBER,AAVE,IEARN,BANCOR,PMM1,SWERVE,BLACKHOLESWAP,DODO,DODO_V2,VALUELIQUID,SHELL,DEFISWAP,SAKESWAP,LUASWAP,MINISWAP,MSTABLE,PMM2,SYNTHETIX,AAVE_V2,ST_ETH,ONE_INCH_LP,ONE_INCH_LP_1_1,LINKSWAP,S_FINANCE,PSM,POWERINDEX,PMM3,XSIGMA,SMOOTHY_FINANCE,SADDLE,PMM4,KYBER_DMM,BALANCER_V2,SETH_WRAPPER,CURVE_V2,CURVE_V2_EURS_2_ASSET,CURVE_V2_ETH_CRV,CURVE_V2_ETH_CVX,CONVERGENCE_X,ONE_INCH_LIMIT_ORDER,ONE_INCH_LIMIT_ORDER_V2,ONE_INCH_LIMIT_ORDER_V3,DFX_FINANCE,FIXED_FEE_SWAP,DXSWAP,SHIBASWAP,UNIFI,PMMX,PMM5,PSM_PAX,PMM2MM1,WSTETH,DEFI_PLAZA,FIXED_FEE_SWAP_V3,SYNTHETIX_WRAPPER,SYNAPSE,CURVE_V2_YFI_2_ASSET,CURVE_V2_ETH_PAL,POOLTOGETHER,ETH_BANCOR_V3,PMM6,ELASTICSWAP,BALANCER_V2_WRAPPER,FRAXSWAP,RADIOSHACK,KYBERSWAP_ELASTIC,CURVE_V2_TWO_CRYPTO,PMM9,STABLE_PLAZA,PMM8,ZEROX_LIMIT_ORDER,CURVE_3CRV,KYBER_DMM_STATIC,ANGLE,ROCKET_POOL,ETHEREUM_ELK,ETHEREUM_PANCAKESWAP_V2,SYNTHETIX_ATOMIC_SIP288,PSM_GUSD,INTEGRAL,MAINNET_SOLIDLY,NOMISWAP_STABLE,CURVE_V2_TWOCRYPTO_META,MAVERICK_V1,VERSE,DFX_FINANCE_V2,ZK_BOB,PANCAKESWAP_V3,NOMISWAPEPCS,XFAI,PMM11,CURVE_V2_LLAMMA,CURVE_V2_TRICRYPTO_NG,PMM8_2,SUSHISWAP_V3,SFRX_ETH';
 
             const options = {
                 method: 'GET',
                 baseURL: 'https://api.1inch.dev/swap',
                 url: `/v5.2/1/swap?src=${sellAssetInfo.address}&dst=${buyAssetInfo.address}&amount=${amount}&from=${oneInchWrapper.address}`
-                + '&slippage=10'
+                + '&slippage=0.3'
                 + '&usePatching=true'
-                + '&disableEstimate=true',
+                + '&disableEstimate=true'
+                + `&protocols=${protocols}`
+                + '&allowPartialFill=false'
+                + '&includeProtocols=true',
                 headers: {
                     Authorization: 'Bearer 4trHOrTtM6UWnztd8m68QSXBtuARRZoF',
                 },
             };
+            // set slippage to user slippage + fee%
+            // add all protocols and remove one by one
             console.log(options.baseURL + options.url);
             const priceObject = await axios(options).then((response) => response.data);
             console.log(priceObject);
+            console.log(priceObject.protocols[0]);
             // THIS IS CHANGEABLE WITH API INFORMATION
             const allowanceTarget = priceObject.tx.to;
             const price = 1; // just for testing, anything bigger than 0 triggers offchain if
@@ -683,7 +691,7 @@ const oneInchTest = async () => {
             exchangeObject = formatExchangeObjForOffchain(
                 sellAssetInfo.address,
                 buyAssetInfo.address,
-                hre.ethers.utils.parseUnits('1', 18),
+                amount,
                 oneInchWrapper.address,
                 allowanceTarget,
                 allowanceTarget,
