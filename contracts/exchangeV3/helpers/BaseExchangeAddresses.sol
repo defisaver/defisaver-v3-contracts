4fee5860d9fb1b6caa3906bab456c579491a0477
Nenad Palinkasevic <nesa993@gmail.com>
2023-09-29 14:29:25 +0200

    refactor: update based on reviews

--- contracts/exchangeV3/helpers/BaseExchangeAddresses.sol
+++ contracts/exchangeV3/helpers/BaseExchangeAddresses.sol
@@ -8,5 +8,5 @@ contract BaseExchangeAddresses {
     address internal constant DISCOUNT_ADDRESS = 0x4839d021A24820e57C31D386d430e2e82694F73B;
     address internal constant SAVER_EXCHANGE_REGISTRY = 0x2A588cBCBd5e6c6ba7ED0E260B8107F599017DDE;
     address internal constant ZRX_ALLOWLIST_ADDR = 0x169D6E128238ebabF86032Ae9da65938eaD7F69e;
-    address internal constant GROUP_REGISTRY = 0xa898078f369A78CE6b8023715e8f6d2Ad7d2719f;
+    address internal constant TOKEN_GROUP_REGISTRY = 0xa898078f369A78CE6b8023715e8f6d2Ad7d2719f;
 }
