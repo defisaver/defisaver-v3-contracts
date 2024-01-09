53f7a911524316efad8ece60e14ea76069682060
Nenad Palinkasevic <nesa993@gmail.com>
2024-01-09 14:06:58 +0100

    refactor: remove unused pragma

--- contracts/actions/curve/gauge/CurveMintCrv.sol
+++ contracts/actions/curve/gauge/CurveMintCrv.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.10;
-pragma experimental ABIEncoderV2;
 
 import "../../ActionBase.sol";
 import "../helpers/CurveHelper.sol";
--- contracts/actions/utils/TokenBalance.sol
+++ contracts/actions/utils/TokenBalance.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity =0.8.10;
-pragma experimental ABIEncoderV2;
+
 import "../ActionBase.sol";
 import "../../utils/TokenUtils.sol";
 
