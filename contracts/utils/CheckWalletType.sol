32394fb17dc50017636b43e405cb7e37d4daee09
Rajko Zagorac <rajkozagorac00@gmail.com>
2024-01-17 10:15:55 +0100

    fix: remove unnecessary isContract check in CheckWalletType to keep things simple

--- contracts/utils/CheckWalletType.sol
+++ contracts/utils/CheckWalletType.sol
@@ -8,13 +8,6 @@ import "./helpers/UtilHelper.sol";
 /// @title CheckWalletType - Helper contract to check if address belongs to DSProxy or not
 contract CheckWalletType is UtilHelper {
     function isDSProxy(address _proxy) public view returns (bool) {
-        return
-            isAddressContract(_proxy) &&
-            IDSProxyFactory(PROXY_FACTORY_ADDR).isProxy(_proxy);
-    }
-
-    function isAddressContract(address addr) private view returns (bool) {
-        bytes memory code = addr.code;
-        return code.length > 0;
+        return IDSProxyFactory(PROXY_FACTORY_ADDR).isProxy(_proxy);
     }
 }
