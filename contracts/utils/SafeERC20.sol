37f5b6c51bbc0cc9f108a5b5893d029e73532f10
Nenad Palinkasevic <nesa993@gmail.com>
2024-01-25 08:49:18 +0100

    refactor: add deployed addresses so we dont inject them on the fly

--- contracts/utils/SafeERC20.sol
+++ contracts/utils/SafeERC20.sol
@@ -8,15 +8,7 @@ pragma solidity =0.8.10;
 import "../interfaces/IERC20.sol";
 import "./Address.sol";
 
-/**
- * @title SafeERC20
- * @dev Wrappers around ERC20 operations that throw on failure (when the token
- * contract returns false). Tokens that return no value (and instead revert or
- * throw on failure) are also supported, non-reverting calls are assumed to be
- * successful.
- * To use this library you can add a `using SafeERC20 for IERC20;` statement to your contract,
- * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
- */
+
 library SafeERC20 {
     using Address for address;
 
