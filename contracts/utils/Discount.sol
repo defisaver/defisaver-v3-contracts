667e39875fbf4eae4161009ef22035bc21d14cce
majkic99 <vrnebojsamajkic@gmail.com>
2024-01-31 15:55:05 +0100

    feat: rename discount function name and add natspec

--- contracts/utils/Discount.sol
+++ contracts/utils/Discount.sol
@@ -3,10 +3,11 @@ pragma solidity =0.8.10;
 
 import "../auth/AdminAuth.sol";
 
+/// @title Contract used to check if a DFS user is exempt from paying a service fee
 contract Discount is AdminAuth{
     mapping(address => bool) public serviceFeesDisabled;
 
-    function enableServiceFee(address _wallet) public onlyOwner{
+    function reenableServiceFee(address _wallet) public onlyOwner{
         serviceFeesDisabled[_wallet] = false;
     }
 
