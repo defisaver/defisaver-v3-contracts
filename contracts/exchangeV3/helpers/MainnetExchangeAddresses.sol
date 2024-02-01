7059240e554fb60e0735ebc7bd132cac5ab81daa
Nenad Palinkasevic <nesa993@gmail.com>
2024-02-01 08:42:36 +0100

    refactor: add new discount addr

--- contracts/exchangeV3/helpers/MainnetExchangeAddresses.sol
+++ contracts/exchangeV3/helpers/MainnetExchangeAddresses.sol
@@ -5,7 +5,7 @@ pragma solidity =0.8.10;
 contract MainnetExchangeAddresses {
 
     address internal constant FEE_RECIPIENT_ADDRESS = 0x39C4a92Dc506300c3Ea4c67ca4CA611102ee6F2A;
-    address internal constant DISCOUNT_ADDRESS = 0x1b14E8D511c9A4395425314f849bD737BAF8208F;
+    address internal constant DISCOUNT_ADDRESS = 0x84fE6D4aaD0CA1ce3af7153eecd11729fa7a74f0;
     address internal constant WRAPPER_EXCHANGE_REGISTRY = 0x653893375dD1D942D2C429caB51641F2bf14d426;
     address internal constant EXCHANGE_AGGREGATOR_REGISTRY_ADDR = 0x7b67D9D7993A258C4b2C31CDD9E6cbD5Fb674985;
     address internal constant ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
