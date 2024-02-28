8ba9529a7703a16782398d88e654fd2b5be4dc7c
Rajko Zagorac <rajkozagorac00@gmail.com>
2024-02-28 11:07:30 +0100

    fix: ignore enable as collateral field in SparkSupply action

--- contracts/actions/spark/SparkSupply.sol
+++ contracts/actions/spark/SparkSupply.sol
@@ -10,6 +10,7 @@ import "./helpers/SparkHelper.sol";
 contract SparkSupply is ActionBase, SparkHelper {
     using TokenUtils for address;
 
+    /// @dev enableAsColl - left for backwards compatibility, it's not used in this action
     struct Params {
         uint256 amount;
         address from;
@@ -33,7 +34,6 @@ contract SparkSupply is ActionBase, SparkHelper {
         params.amount = _parseParamUint(params.amount, _paramMapping[0], _subData, _returnValues);
         params.from = _parseParamAddr(params.from, _paramMapping[1], _subData, _returnValues);
         params.assetId = uint16(_parseParamUint(params.assetId, _paramMapping[2], _subData, _returnValues));
-        params.enableAsColl = _parseParamUint(params.enableAsColl ? 1 : 0, _paramMapping[3], _subData, _returnValues) == 1;
         params.useDefaultMarket = _parseParamUint(params.useDefaultMarket ? 1 : 0, _paramMapping[4], _subData, _returnValues) == 1;
         params.useOnBehalf = _parseParamUint(params.useOnBehalf ? 1 : 0, _paramMapping[5], _subData, _returnValues) == 1;
         params.market = _parseParamAddr(params.market, _paramMapping[6], _subData, _returnValues);
@@ -56,7 +56,6 @@ contract SparkSupply is ActionBase, SparkHelper {
             params.amount,
             params.from,
             params.assetId,
-            params.enableAsColl,
             params.onBehalf
         );
         emit ActionEvent("SparkSupply", logData);
@@ -71,7 +70,6 @@ contract SparkSupply is ActionBase, SparkHelper {
             params.amount,
             params.from,
             params.assetId,
-            params.enableAsColl,
             params.onBehalf
         );
         logger.logActionDirectEvent("SparkSupply", logData);
@@ -84,7 +82,6 @@ contract SparkSupply is ActionBase, SparkHelper {
             params.amount,
             params.from,
             params.assetId,
-            params.enableAsColl,
             params.onBehalf
         );
         logger.logActionDirectEvent("SparkSupply", logData);
@@ -103,14 +100,12 @@ contract SparkSupply is ActionBase, SparkHelper {
     /// @param _amount Amount of tokens to be deposited
     /// @param _from Where are we pulling the supply tokens amount from
     /// @param _assetId The id of the token to be deposited
-    /// @param _enableAsColl If the supply asset should be collateral
     /// @param _onBehalf For what user we are supplying the tokens, defaults to user's wallet
     function _supply(
         address _market,
         uint256 _amount,
         address _from,
         uint16 _assetId,
-        bool _enableAsColl,
         address _onBehalf
     ) internal returns (uint256, bytes memory) {
         IPoolV3 lendingPool = getLendingPool(_market);
@@ -134,19 +129,12 @@ contract SparkSupply is ActionBase, SparkHelper {
 
         lendingPool.supply(tokenAddr, _amount, _onBehalf, SPARK_REFERRAL_CODE);
 
-        if (_enableAsColl) {
-            lendingPool.setUserUseReserveAsCollateral(tokenAddr, true);
-        } else {
-            lendingPool.setUserUseReserveAsCollateral(tokenAddr, false);
-        }
-
         bytes memory logData = abi.encode(
             _market,
             tokenAddr,
             _amount,
             _from,
-            _onBehalf,
-            _enableAsColl
+            _onBehalf
         );
         return (_amount, logData);
     }
