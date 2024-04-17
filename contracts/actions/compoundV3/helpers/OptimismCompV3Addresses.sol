530434bb06944b634a74ef6fcf18a0259566b0d6
majkic99 <vrnebojsamajkic@gmail.com>
2024-04-18 00:12:42 +0200

    fix: put good compv3 comet address in optimism file

--- contracts/actions/compoundV3/helpers/OptimismCompV3Addresses.sol
+++ contracts/actions/compoundV3/helpers/OptimismCompV3Addresses.sol
@@ -3,7 +3,7 @@
 pragma solidity =0.8.10;
 
 contract OptimismCompV3Addresses {
-    address internal constant COMET_REWARDS_ADDR = 0x88730d254A2f7e6AC8388c3198aFd694bA9f7fae;
+    address internal constant COMET_REWARDS_ADDR = 0x443EA0340cb75a160F31A440722dec7b5bc3C2E9;
     // not yet available
     address internal constant COMP_ETH_COMET = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 }
