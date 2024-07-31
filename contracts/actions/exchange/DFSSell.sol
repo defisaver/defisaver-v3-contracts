76423bc11c2642962aa16b8dd5d4f41cb5072be8
majkic99 <vrnebojsamajkic@gmail.com>
2024-07-31 11:16:43 +0200

    refactor: refactor logic for isDirect dfs sell in recipe

--- contracts/actions/exchange/DFSSell.sol
+++ contracts/actions/exchange/DFSSell.sol
@@ -52,13 +52,10 @@ contract DFSSell is ActionBase, DFSExchangeWithTxSaver {
         );
         params.from = _parseParamAddr(params.from, _paramMapping[3], _subData, _returnValues);
         params.to = _parseParamAddr(params.to, _paramMapping[4], _subData, _returnValues);
-        uint256 exchangedAmount;
-        bytes memory logData;
-        if (_returnValues.length == 1){
-            (exchangedAmount, logData) = _dfsSell(params.exchangeData, params.from, params.to, true);
-        } else {
-            (exchangedAmount, logData) = _dfsSell(params.exchangeData, params.from, params.to, false);
-        }
+        
+        bool isDirect = _returnValues.length == 1 ? true : false;
+
+        (uint256 exchangedAmount, bytes memory logData) = _dfsSell(params.exchangeData, params.from, params.to, isDirect);
 
         emit ActionEvent("DFSSell", logData);
         return bytes32(exchangedAmount);
