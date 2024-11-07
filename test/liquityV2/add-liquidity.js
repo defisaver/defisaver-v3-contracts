6eb390f71003594e28b467736e9fc5811f846dc3
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2024-11-07 08:52:13 +0100

    test: update add liquidity for new bold address on fork

--- test/liquityV2/add-liquidity.js
+++ test/liquityV2/add-liquidity.js
@@ -20,7 +20,7 @@ const addLiquidity = async () => {
             proxy = await getProxy(senderAcc.address, hre.config.isWalletSafe);
         });
         it('...test add liquidity', async () => {
-            const bold = '0x4f37d1f70b7ed0868baa367c82897006b5e1a6e4';
+            const bold = '0xbb57f8ad4baf3970270e78f55ebeeb1e0bef3ccb';
             const dai = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
             const boldAmount = hre.ethers.utils.parseUnits('1000000000', 18);
             const daiAmount = hre.ethers.utils.parseUnits('1000000000', 18);
@@ -28,13 +28,13 @@ const addLiquidity = async () => {
             await setBalance(dai, senderAcc.address, daiAmount);
             await uniV3CreatePool(
                 proxy,
-                bold,
                 dai,
+                bold,
                 '100',
                 -101,
                 99,
-                boldAmount,
                 daiAmount,
+                boldAmount,
                 senderAcc.address,
                 senderAcc.address,
                 '79228162514264337593543950336',
