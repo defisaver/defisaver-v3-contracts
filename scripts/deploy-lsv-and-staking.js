f013d7a2499d080028f218e8d6b6f0cadd82adb8
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2024-11-22 12:06:20 +0100

    feat: add staking actions deployment script

---
+++ scripts/deploy-lsv-and-staking.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const hre = require('hardhat');
+const { start } = require('./utils/starter');
+const {
+    redeploy, addrs, network, getOwnerAddr,
+} = require('../test/utils');
+const { topUp } = require('./utils/fork');
+
+async function main() {
+    const senderAcc = (await hre.ethers.getSigners())[0];
+    await topUp(senderAcc.address);
+    await topUp(getOwnerAddr());
+
+    const lsvSell = await redeploy('LSVSell', addrs[network].REGISTRY_ADDR, false, true);
+    const etherFiStake = await redeploy('EtherFiStake', addrs[network].REGISTRY_ADDR, false, true);
+    const etherFiWrap = await redeploy('EtherFiWrap', addrs[network].REGISTRY_ADDR, false, true);
+    const etherFiUnwrap = await redeploy('EtherFiUnwrap', addrs[network].REGISTRY_ADDR, false, true);
+    const renzoStake = await redeploy('RenzoStake', addrs[network].REGISTRY_ADDR, false, true);
+    console.log(`LSVSell: ${lsvSell.address}`);
+    console.log(`EtherFiStake: ${etherFiStake.address}`);
+    console.log(`EtherFiWrap: ${etherFiWrap.address}`);
+    console.log(`EtherFiUnwrap: ${etherFiUnwrap.address}`);
+    console.log(`RenzoStake: ${renzoStake.address}`);
+
+    process.exit(0);
+}
+
+start(main);
