449d47484ad8f0ced87a069c2c1c9a21a30c0d33
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2024-12-06 11:29:36 +0100

    test: add morpho blue boost on price deployment script

---
+++ scripts/deploy-morpho-boost-on-price.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable import/no-extraneous-dependencies */
+
+const hre = require('hardhat');
+const { configure } = require('@defisaver/sdk');
+const { start } = require('./utils/starter');
+const {
+    addrs,
+    getOwnerAddr,
+    openStrategyAndBundleStorage,
+    network,
+    getNetwork,
+    redeploy,
+} = require('../test/utils');
+const { topUp } = require('./utils/fork');
+const { addBotCaller, createStrategy, createBundle } = require('../test/utils-strategies');
+const {
+    createMorphoBlueBoostOnTargetPriceL2Strategy,
+} = require('../test/l2-strategies');
+const { createMorphoBlueBoostOnTargetPriceStrategy } = require('../test/strategies');
+
+const deployBoostOnPriceBundle = async (isFork) => {
+    await openStrategyAndBundleStorage(isFork);
+    const boostOnPriceStrategy = getNetwork() === 'mainnet'
+        ? createMorphoBlueBoostOnTargetPriceStrategy()
+        : createMorphoBlueBoostOnTargetPriceL2Strategy();
+    const flBoostOnPriceStrategy = getNetwork() === 'mainnet'
+        ? createMorphoBlueBoostOnTargetPriceStrategy()
+        : createMorphoBlueBoostOnTargetPriceL2Strategy();
+    const boostOnPriceStrategyId = await createStrategy(undefined, ...boostOnPriceStrategy, false);
+    const flBoostOnPriceStrategyId = await createStrategy(
+        undefined, ...flBoostOnPriceStrategy, false,
+    );
+    const boostOnPriceBundleId = await createBundle(
+        undefined, [boostOnPriceStrategyId, flBoostOnPriceStrategyId],
+    );
+    return boostOnPriceBundleId;
+};
+
+async function main() {
+    // configure({
+    //     chainId: 8453,
+    //     testMode: true,
+    // });
+    const isFork = true;
+    const senderAcc = (await hre.ethers.getSigners())[0];
+    await topUp(senderAcc.address);
+    await topUp(getOwnerAddr());
+
+    const l2BotAccounts = [
+        '0x61fe1bdcd91E8612a916f86bA50a3EDF3E5654c4',
+        '0xC561281982c3042376eB8242d6A78Ab18062674F',
+    ];
+    const l1BotAccounts = [
+        '0x1b6c1a0e20af6f8b7f7b9e3e2f3b3d3d5f1b0e5a',
+        '0x1b6c1a0e20af6f8b7f7b9e3e2f3b3d3d5f1b0e5a',
+    ];
+    const bots = getNetwork() === 'mainnet' ? l1BotAccounts : l2BotAccounts;
+    for (let i = 0; i < bots.length; ++i) {
+        await topUp(bots[i]);
+        await addBotCaller(bots[i], addrs[network].REGISTRY_ADDR, isFork);
+    }
+
+    const morphoBluePriceTrigger = await redeploy('MorphoBluePriceTrigger', addrs[network].REGISTRY_ADDR, false, isFork);
+    const morphoBlueTargetRatioCheck = await redeploy('MorphoBlueTargetRatioCheck', addrs[network].REGISTRY_ADDR, false, isFork);
+    console.log('MorphoBluePriceTrigger:', morphoBluePriceTrigger.address);
+    console.log('MorphoBlueTargetRatioCheck:', morphoBlueTargetRatioCheck.address);
+
+    const boostOnPriceBundleId = await deployBoostOnPriceBundle(isFork);
+    console.log('BoostOnPriceBundle:', boostOnPriceBundleId);
+
+    process.exit(0);
+}
+
+start(main);
