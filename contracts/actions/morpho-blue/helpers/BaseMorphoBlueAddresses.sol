5975c7c8cebe015cbbe1ea1e2fb9e6c45f912e4d
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2024-12-12 20:12:48 +0100

    feat: add MorphoBlueReallocateLiquidity contract with test

--- contracts/actions/morpho-blue/helpers/BaseMorphoBlueAddresses.sol
+++ contracts/actions/morpho-blue/helpers/BaseMorphoBlueAddresses.sol
@@ -5,4 +5,5 @@ contract BaseMorphoBlueAddresses {
     address public constant MORPHO_BLUE_ADDRESS = 0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb;
     address internal constant MORPHO_TOKEN_WRAPPER = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     address internal constant LEGACY_MORPHO_TOKEN = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address public constant PUBLIC_ALLOCATOR = 0xA090dD1a701408Df1d4d0B85b716c87565f90467;
 }
