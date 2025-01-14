7dd1711b61166d1c1e88d1e95087aefc8e3477dc
majkic99 <vrnebojsamajkic@gmail.com>
2025-01-14 15:02:37 +0100

    feat: create liquity v2 boost and repay on price strategies

---
+++ contracts/actions/checkers/LiquityV2TargetRatioCheck.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.24;
+
+import { ActionBase } from "../ActionBase.sol";
+import { LiquityV2RatioHelper } from "../liquityV2/helpers/LiquityV2RatioHelper.sol";
+
+/// @title LiquityV2RatioCheck - Checks liquity target ratio for trove and reverts if faulty
+contract LiquityV2RatioCheck is ActionBase, LiquityV2RatioHelper {
+
+    /// @dev 5% offset acceptable
+    uint256 internal constant RATIO_OFFSET = 50000000000000000;
+
+    error BadAfterRatio(uint256 currentRatio, uint256 targetRatio);
+
+    struct Params {
+        address market;
+        uint256 troveId;
+        uint256 targetRatio;
+    }
+
+    /// @inheritdoc ActionBase
+    function executeAction(
+        bytes memory _callData,
+        bytes32[] memory _subData,
+        uint8[] memory _paramMapping,
+        bytes32[] memory _returnValues
+    ) public payable virtual override returns (bytes32) {
+        Params memory params = parseInputs(_callData);
+
+        params.market = _parseParamAddr(params.market, _paramMapping[0], _subData, _returnValues);
+        params.troveId = _parseParamUint(params.troveId, _paramMapping[1], _subData, _returnValues);
+        params.targetRatio = _parseParamUint(params.targetRatio, _paramMapping[2], _subData, _returnValues);
+
+        (uint256 currRatio,) = getRatio(params.market, params.troveId);
+        
+        if (
+            currRatio > (params.targetRatio + RATIO_OFFSET) ||
+            currRatio < (params.targetRatio - RATIO_OFFSET)
+        ) {
+            revert BadAfterRatio(currRatio, params.targetRatio);
+        }
+
+        emit ActionEvent("LiquityV2TargetRatioCheck", abi.encode(currRatio));
+        return bytes32(currRatio);
+    }
+
+    /// @inheritdoc ActionBase
+    // solhint-disable-next-line no-empty-blocks
+    function executeActionDirect(bytes memory _callData) public payable override {}
+
+    /// @inheritdoc ActionBase
+    function actionType() public pure virtual override returns (uint8) {
+        return uint8(ActionType.CHECK_ACTION);
+    }
+
+    function parseInputs(bytes memory _callData) public pure returns (Params memory inputData) {
+        inputData = abi.decode(_callData, (Params));
+    }
+
+}
---
+++ contracts/triggers/LiquityV2QuotePriceTrigger.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.24;
+
+import { AdminAuth } from "../auth/AdminAuth.sol";
+import { ITrigger } from "../interfaces/ITrigger.sol";
+import { TriggerHelper } from "./helpers/TriggerHelper.sol";
+import { ITroveManager } from "../interfaces/liquityV2/ITroveManager.sol";
+import { IAddressesRegistry } from "../interfaces/liquityV2/IAddressesRegistry.sol";
+import { IPriceFeed } from "../interfaces/liquityV2/IPriceFeed.sol";
+
+/// @title Trigger contract that verifies if price of collateral on a chosen LiquityV2 market went over or under a certain threshold
+contract LiquityV2RatioTrigger is 
+    ITrigger,
+    AdminAuth,
+    TriggerHelper
+{
+    enum PriceState {
+        OVER,
+        UNDER
+    }
+
+    /// @param market address of the market where the trove is
+    /// @param price threshold price that represents the triggerable point
+    /// @param state represents if we want the current state to be higher or lower than ratio param
+    struct SubParams {
+        address market;
+        uint256 price;
+        uint8 state;
+    }
+
+    /// @dev checks current price of collateral in a LiquityV2 market and triggers if it's in a correct state
+    function isTriggered(bytes memory, bytes memory _subData)
+        public
+        override
+        returns (bool)
+    {   
+        SubParams memory triggerSubData = parseSubInputs(_subData);
+        IAddressesRegistry market = IAddressesRegistry(triggerSubData.market);
+
+        IPriceFeed priceFeed = IPriceFeed(market.priceFeed());
+        (uint256 collPrice, ) = priceFeed.fetchPrice();
+        uint256 currPrice = collPrice;
+
+        if (PriceState(triggerSubData.state) == PriceState.OVER) {
+            if (currPrice > triggerSubData.price) return true;
+        }
+
+        if (PriceState(triggerSubData.state) == PriceState.UNDER) {
+            if (currPrice < triggerSubData.price) return true;
+        }
+
+        return false;
+    }
+
+    function parseSubInputs(bytes memory _subData) public pure returns (SubParams memory params) {
+        params = abi.decode(_subData, (SubParams));
+    }
+
+    function changedSubData(bytes memory _subData) public pure override  returns (bytes memory) {}
+    
+    function isChangeable() public pure override returns (bool){
+        return false;
+    }
+}
