cfc055829c0420f5330045625a99a885e78eda76
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-01-20 12:46:09 +0100

    test: update Bold address

--- contracts/actions/liquityV2/helpers/MainnetLiquityV2Addresses.sol
+++ contracts/actions/liquityV2/helpers/MainnetLiquityV2Addresses.sol
@@ -3,5 +3,5 @@ pragma solidity =0.8.24;
 
 contract MainnetLiquityV2Addresses {
     // TODO: change after deployment
-    address internal constant BOLD_ADDR = 0x9DD82A1437509c2644862d73858EF602B2b5604D;
+    address internal constant BOLD_ADDR = 0x28514D2e99ADD34aF36DD779126195FE053E4f97;
 }
