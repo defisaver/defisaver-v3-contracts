aaa8cec7d6efb5fbaf269a1064c558e4bb1081d4
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-01-22 10:43:00 +0100

    feat: adapt checksum BOLD address

--- contracts/actions/liquityV2/helpers/MainnetLiquityV2Addresses.sol
+++ contracts/actions/liquityV2/helpers/MainnetLiquityV2Addresses.sol
@@ -2,5 +2,5 @@
 pragma solidity =0.8.24;
 
 contract MainnetLiquityV2Addresses {
-    address internal constant BOLD_ADDR = 0xb01dd87b29d187f3e3a4bf6cdaebfb97f3d9ab98;
+    address internal constant BOLD_ADDR = 0xb01dd87B29d187F3E3a4Bf6cdAebfb97F3D9aB98;
 }
