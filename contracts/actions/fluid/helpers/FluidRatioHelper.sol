9162188d081fc684caaaeab5d1dbd6a73142a682
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-01-28 15:42:52 +0100

    feat: add Fluid Ratio Trigger action

---
+++ contracts/actions/fluid/helpers/FluidRatioHelper.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.24;
+
+import { IERC20 } from "../../../interfaces/IERC20.sol";
+import { IFluidVaultResolver } from "../../../interfaces/fluid/IFluidVaultResolver.sol";
+import { FluidHelper } from "./FluidHelper.sol";
+import { DSMath } from "../../../DS/DSMath.sol";
+
+/// @title Helper methods for Fluid ratio calc.
+contract FluidRatioHelper is DSMath, FluidHelper {
+
+    uint256 internal constant T1_VAULT_ID = 1000;
+    uint256 internal constant T2_VAULT_ID = 2000;
+    uint256 internal constant T3_VAULT_ID = 3000;
+    uint256 internal constant T4_VAULT_ID = 4000;
+
+    uint256 internal constant ORACLE_PRICE_DECIMALS = 27;
+
+    /// @notice Gets ratio for a fluid position
+    /// @param _nftId nft id of the fluid position
+    function getRatio(uint256 _nftId) public view returns (uint256 ratio) {
+        (
+            IFluidVaultResolver.UserPosition memory userPosition,
+            IFluidVaultResolver.VaultEntireData memory vaultData
+        ) = IFluidVaultResolver(FLUID_VAULT_RESOLVER).positionByNftId(_nftId);
+
+        // TODO: For now, only handle the case for T1 Vaults
+        if (vaultData.constantVariables.vaultId == T1_VAULT_ID) {
+            uint256 collAmount = userPosition.supply;
+            uint256 debtAmount = userPosition.borrow;
+
+            if (debtAmount == 0) return uint256(0);
+
+            uint256 collDec = IERC20(vaultData.constantVariables.supplyToken.token0).decimals();
+            uint256 debtDec = IERC20(vaultData.constantVariables.borrowToken.token0).decimals();
+
+            /// @dev Examples:
+            // price = Price of collateral token in debt token scaled by priceScaler
+            // priceScaler = 10 ** (ORACLE_PRICE_DECIMALS - collDec + debtDec)
+            //
+            // 1. (2.5 WBTC / 50k USDC)
+            // price = 1028534478997854690000000000000 / 10 ** (27 - 8 + 6) = 102853.44789978547
+            // collAmount = 2.5 * 1e8
+            // debtAmount = 50000 * 1e6
+            // ratio = 2.5 * 1e8 * 1028534478997854690000000000000 * 1e6 * 1e18 / 10 ** (27 - 8 + 6) / 1e8 / (50000 * 1e6)
+            //       = 5.142672394989273e18
+            //
+            // 2. (3.2 weETH / 1.5 wstETH)
+            // price = 888143936867381715436793889 / 10 ** (27 - 18 + 18) = 0.8881439368673817
+            // collAmount = 3.2 * 1e18
+            // debtAmount = 1.5 * 1e18
+            // ratio = 3.2 * 1e18 * 888143936867381715436793889 * 1e18 * 1e18 / 10 ** (27 - 18 + 18) / 1e18 / (1.5 * 1e18)
+            //       = 1.894707065317081e18
+            //
+            // 3. (2 WBTC / 30 ETH)
+            // price =  321857633689348920539866335783307690682 / 10 ** (27 - 8 + 18) = 32.18576336893489
+            // collAmount = 2 * 1e8
+            // debtAmount = 30 * 1e18
+            // ratio = 2 * 1e8 * 321857633689348920539866335783307690682 * 1e18 * 1e18 / 10 ** (27 - 8 + 18) / 1e8 / (30 * 1e18)
+            //       = 2.1457175579289923e18
+            //
+            uint256 price = vaultData.configs.oraclePriceOperate;
+            uint256 priceScaler = 10 ** (ORACLE_PRICE_DECIMALS - collDec + debtDec);
+
+            ratio = collAmount * price * (10 ** debtDec) * WAD / priceScaler / (10 ** collDec) / debtAmount;
+        }
+
+        return 0;
+    }
+}
---
+++ contracts/triggers/FluidRatioTrigger.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.24;
+
+import { AdminAuth } from "../auth/AdminAuth.sol";
+import { ITrigger } from "../interfaces/ITrigger.sol";
+import { TriggerHelper } from "./helpers/TriggerHelper.sol";
+import { FluidRatioHelper } from "../actions/fluid/helpers/FluidRatioHelper.sol";
+import { TransientStorage } from "../utils/TransientStorage.sol";
+
+/// @title Trigger contract that verifies if current fluid position ratio went over/under the subbed ratio
+contract FluidRatioTrigger is 
+    ITrigger,
+    AdminAuth,
+    FluidRatioHelper,
+    TriggerHelper
+{
+    TransientStorage public constant tempStorage = TransientStorage(TRANSIENT_STORAGE);
+
+    enum RatioState { OVER, UNDER }
+
+    /// @param nftId nft id of the fluid position
+    /// @param ratio ratio that represents the triggerable point
+    /// @param state represents if we want the current state to be higher or lower than ratio param
+    struct SubParams {
+        uint256 nftId;
+        uint256 ratio;
+        uint8 state;
+    }
+    /// @dev checks current ratio of a LiquityV2 trove and triggers if it's in a correct state
+    function isTriggered(bytes memory, bytes memory _subData)
+        public
+        override
+        returns (bool)
+    {   
+        SubParams memory triggerSubData = parseSubInputs(_subData);
+
+        uint256 currRatio = getRatio(triggerSubData.nftId);
+        
+        if (currRatio == 0) return false;
+
+        tempStorage.setBytes32("FLUID_RATIO", bytes32(currRatio));
+        
+        if (RatioState(triggerSubData.state) == RatioState.OVER) {
+            if (currRatio > triggerSubData.ratio) return true;
+        }
+
+        if (RatioState(triggerSubData.state) == RatioState.UNDER) {
+            if (currRatio < triggerSubData.ratio) return true;
+        }
+
+        return false;
+    }
+
+    function parseSubInputs(bytes memory _subData) public pure returns (SubParams memory params) {
+        params = abi.decode(_subData, (SubParams));
+    }
+
+    function changedSubData(bytes memory _subData) public pure override  returns (bytes memory) {}
+    
+    function isChangeable() public pure override returns (bool) {
+        return false;
+    }
+}
