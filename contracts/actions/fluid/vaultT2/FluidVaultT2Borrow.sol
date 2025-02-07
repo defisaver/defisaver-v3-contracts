e8805698457f7fcd58b667fea7b3c6a6b908c062
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-02-07 19:57:30 +0100

    feat: add FluidVaultT2Borrow action

---
+++ contracts/actions/fluid/vaultT2/FluidVaultT2Borrow.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.24;
+
+import { IFluidVaultT2 } from "../../../interfaces/fluid/IFluidVaultT2.sol";
+import { FluidHelper } from "../helpers/FluidHelper.sol";
+import { TokenUtils } from "../../../utils/TokenUtils.sol";
+import { ActionBase } from "../../ActionBase.sol";
+
+/// @title Borrow assets from Fluid Vault T2 (2_col:1_debt)
+contract FluidVaultT2Borrow is ActionBase, FluidHelper {
+    using TokenUtils for address;
+
+    /// @param vault The address of the Fluid Vault T2
+    /// @param nftId ID of the NFT representing the position
+    /// @param amount Amount to borrow
+    /// @param to Address to send the borrowed assets to
+    /// @param wrapBorrowedEth Whether to wrap the borrowed ETH into WETH if the borrowed asset is ETH.
+    struct Params {
+        address vault;
+        uint256 nftId;
+        uint256 amount;
+        address to;
+        bool wrapBorrowedEth;
+    }
+
+    /// @inheritdoc ActionBase
+    function executeAction(
+        bytes memory _callData,
+        bytes32[] memory _subData,
+        uint8[] memory _paramMapping,
+        bytes32[] memory _returnValues
+    ) public payable virtual override returns (bytes32) {
+        Params memory params = parseInputs(_callData);
+
+        params.vault = _parseParamAddr(params.vault, _paramMapping[0], _subData, _returnValues);
+        params.nftId = _parseParamUint(params.nftId, _paramMapping[1], _subData, _returnValues);
+        params.amount = _parseParamUint(params.amount, _paramMapping[2], _subData, _returnValues);
+        params.to = _parseParamAddr(params.to, _paramMapping[3], _subData, _returnValues);
+        params.wrapBorrowedEth = _parseParamUint(
+            params.wrapBorrowedEth ? 1 : 0,
+            _paramMapping[4],
+            _subData,
+            _returnValues
+        ) == 1;
+
+        (uint256 amount, bytes memory logData) = _borrow(params);
+        emit ActionEvent("FluidVaultT2Borrow", logData);
+        return bytes32(amount);
+    }
+
+    /// @inheritdoc ActionBase
+    function executeActionDirect(bytes memory _callData) public payable override {
+        Params memory params = parseInputs(_callData);
+        (, bytes memory logData) = _borrow(params);
+        logger.logActionDirectEvent("FluidVaultT2Borrow", logData);
+    }
+
+    /// @inheritdoc ActionBase
+    function actionType() public pure virtual override returns (uint8) {
+        return uint8(ActionType.STANDARD_ACTION);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            ACTION LOGIC
+    //////////////////////////////////////////////////////////////*/
+    function _borrow(Params memory _params) internal returns (uint256, bytes memory) {
+        IFluidVaultT2.ConstantViews memory constants = IFluidVaultT2(_params.vault).constantsView();
+
+        bool shouldWrapBorrowedEth = _params.wrapBorrowedEth && constants.borrowToken.token0 == TokenUtils.ETH_ADDR;
+
+        IFluidVaultT2(_params.vault).operate(
+            _params.nftId,
+            0, /* newColToken0_ */
+            0, /* newColToken1_ */
+            0, /* colSharesMinMax_ */
+            int256(_params.amount),
+            shouldWrapBorrowedEth ? address(this) : _params.to
+        );
+
+        if (shouldWrapBorrowedEth) {
+            TokenUtils.depositWeth(_params.amount);
+            TokenUtils.WETH_ADDR.withdrawTokens(_params.to, _params.amount);
+        }
+
+        return (_params.amount, abi.encode(_params));
+    }
+
+    function parseInputs(bytes memory _callData) public pure returns (Params memory params) {
+        params = abi.decode(_callData, (Params));
+    }
+}
