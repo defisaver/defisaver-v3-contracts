597a3e44017fc747d49d7293703bf3fe3e28a962
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-02-18 18:26:36 +0100

    feat: add fluid vault t3 and t4 interfaces

--- contracts/interfaces/fluid/IFluidVaultT3.sol
+++ contracts/interfaces/fluid/IFluidVaultT3.sol
@@ -4,25 +4,27 @@ pragma solidity =0.8.24;
 
 import { IFluidVault } from "./IFluidVault.sol";
 
-interface IFluidVaultT2 is IFluidVault {
+interface IFluidVaultT3 is IFluidVault {
 
     /// @notice Performs operations on a vault position
     /// @dev This function allows users to modify their vault position by adjusting collateral and debt
     /// @param nftId_ The ID of the NFT representing the vault position
-    /// @param newColToken0_ The change in collateral amount of token0 (positive for deposit, negative for withdrawal)
-    /// @param newColToken1_ The change in collateral amount of token1 (positive for deposit, negative for withdrawal)
-    /// @param colSharesMinMax_ min or max collateral shares to mint or burn (positive for deposit, negative for withdrawal)
-    /// @param newDebt_ The change in debt amount (positive for borrowing, negative for repayment)
+    /// @param newCol_ The change in collateral amount (positive for deposit, negative for withdrawal)
+    /// @param newDebtToken0_ The change in debt amount for token0 (positive for borrowing, negative for repayment)
+    /// @param newDebtToken1_ The change in debt amount for token1 (positive for borrowing, negative for repayment)
+    /// @param debtSharesMinMax_ Min or max debt shares to burn or mint (positive for borrowing, negative for repayment)
     /// @param to_ The address to receive withdrawn collateral or borrowed tokens (if address(0), defaults to msg.sender)
     /// @return nftId_ The ID of the NFT representing the updated vault position
     /// @return supplyAmt_ Final supply amount (negative if withdrawal occurred)
     /// @return borrowAmt_ Final borrow amount (negative if repayment occurred)
+    /// @custom:security Re-entrancy protection is implemented
+    /// @custom:security ETH balance is validated before and after operation
     function operate(
         uint nftId_,
-        int newColToken0_,
-        int newColToken1_,
-        int colSharesMinMax_,
-        int newDebt_,
+        int newCol_,
+        int newDebtToken0_,
+        int newDebtToken1_,
+        int debtSharesMinMax_,
         address to_
     )
         external
@@ -33,26 +35,27 @@ interface IFluidVaultT2 is IFluidVault {
             int256 // final borrow amount. if - then payback
         );
 
+
     /// @notice Performs operations on a vault position with perfect collateral shares
     /// @dev This function allows users to modify their vault position by adjusting collateral and debt
     /// @param nftId_ The ID of the NFT representing the vault position
-    /// @param perfectColShares_ The change in collateral shares (positive for deposit, negative for withdrawal)
-    /// @param colToken0MinMax_ min or max collateral amount of token0 to withdraw or deposit (positive for deposit, negative for withdrawal)
-    /// @param colToken1MinMax_ min or max collateral amount of token1 to withdraw or deposit (positive for deposit, negative for withdrawal)
-    /// @param newDebt_ The change in debt amount (positive for borrowing, negative for repayment)
+    /// @param newCol_ The change in collateral amount (positive for deposit, negative for withdrawal)
+    /// @param perfectDebtShares_ The change in debt shares (positive for borrowing, negative for repayment)
+    /// @param debtToken0MinMax_ Min or max debt amount for token0 to payback or borrow (positive for borrowing, negative for repayment)
+    /// @param debtToken1MinMax_ Min or max debt amount for token1 to payback or borrow (positive for borrowing, negative for repayment)
     /// @param to_ The address to receive withdrawn collateral or borrowed tokens (if address(0), defaults to msg.sender)
     /// @return nftId_ The ID of the NFT representing the updated vault position
     /// @return r_ int256 array of return values:
-    ///              0 - final col shares amount (can only change on max withdrawal)
-    ///              1 - token0 deposit or withdraw amount
-    ///              2 - token1 deposit or withdraw amount
-    ///              3 - newDebt_ will only change if user sent type(int).min
+    ///              0 - col amount, will only change if user sends type(int).min
+    ///              1 - final debt shares amount (can only change on max payback)
+    ///              2 - token0 borrow or payback amount
+    ///              3 - token1 borrow or payback amount
     function operatePerfect(
         uint nftId_,
-        int perfectColShares_,
-        int colToken0MinMax_,
-        int colToken1MinMax_,
-        int newDebt_,
+        int newCol_,
+        int perfectDebtShares_,
+        int debtToken0MinMax_,
+        int debtToken1MinMax_,
         address to_
     )
         external
