6d2f0d02bab772275e79189b66cd2983f4a76f4a
Pavle Prica <pavle.prica5@gmail.com>
2025-04-17 14:12:52 +0200

    chore: polish per review comments

--- test-sol/triggers/StrategyTriggerViewNoRevert.t.sol
+++ test-sol/triggers/StrategyTriggerViewNoRevert.t.sol
@@ -4,19 +4,13 @@ pragma solidity =0.8.24;
 
 import { BaseTest } from "../utils/BaseTest.sol";
 import { Tokens } from "../utils/Tokens.sol";
+import { Addresses } from "../utils/Addresses.sol";
 import { StrategyTriggerViewNoRevert } from "../../contracts/views/StrategyTriggerViewNoRevert.sol";
 
 contract TestStrategyTriggerViewNoRevert is BaseTest, StrategyTriggerViewNoRevert {
 
-    address walletAddr;
-    address sender;
-
-    Tokens tokens;
-
     function setUp() public override {
         forkMainnetLatest();
-        tokens = new Tokens();
-        tokens.initTokenNamesIfNeeded();
     }
 
     function test_isLimitOrderStrategy() public {
@@ -24,15 +18,15 @@ contract TestStrategyTriggerViewNoRevert is BaseTest, StrategyTriggerViewNoRever
     }
 
     function test_verifyRequiredAmount() public {
-        address testAddr = 0xBbbBBBf0c3925013e5DD6C26620678825d7AfEe7;
+        address testAddr = bob;
 
         bytes32[] memory _subData = new bytes32[](4);
-        _subData[0] = bytes32(uint256(uint160(tokens.getTokenAddressFromName("USDC"))));
+        _subData[0] = bytes32(uint256(uint160(Addresses.USDC_ADDR)));
         _subData[2] = bytes32(uint256(5000));
 
         assertEq(uint(verifyRequiredAmount(testAddr, _subData)), uint(TriggerStatus.FALSE), "trigger status should be false");
 
-        gibTokens(testAddr, tokens.getTokenAddressFromName("USDC"), uint256(5001));
+        gibTokens(testAddr, Addresses.USDC_ADDR, uint256(5001));
 
         assertEq(uint(verifyRequiredAmount(testAddr, _subData)), uint(TriggerStatus.TRUE), "trigger status should be false");
     }
