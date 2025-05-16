98c170c8a0b628e1d90ee03f3d5cafab6a6e9437
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-05-16 12:12:10 +0200

    fix: multitrove getter interface

--- contracts/interfaces/liquityV2/IMultiTroveGetter.sol
+++ contracts/interfaces/liquityV2/IMultiTroveGetter.sol
@@ -5,21 +5,6 @@ pragma solidity =0.8.24;
 interface IMultiTroveGetter {
     struct CombinedTroveData {
         uint256 id;
-        uint256 debt;
-        uint256 coll;
-        uint256 stake;
-        uint256 annualInterestRate;
-        uint256 lastDebtUpdateTime;
-        uint256 lastInterestRateAdjTime;
-        address interestBatchManager;
-        uint256 batchDebtShares;
-        uint256 batchCollShares;
-        uint256 snapshotETH;
-        uint256 snapshotBoldDebt;
-    }
-
-    struct CombinedTroveData {
-        uint256 id;
         uint256 entireDebt;
         uint256 entireColl;
         uint256 redistBoldDebtGain;
