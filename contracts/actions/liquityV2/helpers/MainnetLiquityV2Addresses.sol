13151d1d70b42b3bc1332ab482d09250255e9a16
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-05-16 10:29:11 +0200

    feat: change liquityV2 addresses

--- contracts/actions/liquityV2/helpers/MainnetLiquityV2Addresses.sol
+++ contracts/actions/liquityV2/helpers/MainnetLiquityV2Addresses.sol
@@ -2,9 +2,9 @@
 pragma solidity =0.8.24;
 
 contract MainnetLiquityV2Addresses {
-    address internal constant BOLD_ADDR = 0xbFE297DACb7a2b16DF0C9d2b942D127DFb26fD59;
-    address internal constant MULTI_TROVE_GETTER_ADDR = 0xa4585b2e367034565f513dfA0e5088A062e0A5E1;
-    address internal constant WETH_MARKET_ADDR = 0xc3fe668b43439525F70FE860F89882F0BE312504;
-    address internal constant WSTETH_MARKET_ADDR = 0x9B27787Ff66Aa3CeA8dBC47772328459A1FA05aC;
-    address internal constant RETH_MARKET_ADDR = 0xDe524be191dE806011e98c8d36d50D7a88391a3E;
+    address internal constant BOLD_ADDR = 0x6440f144b7e50D6a8439336510312d2F54beB01D;
+    address internal constant MULTI_TROVE_GETTER_ADDR = 0xFA61dB085510C64B83056Db3A7Acf3b6f631D235;
+    address internal constant WETH_MARKET_ADDR = 0x20F7C9ad66983F6523a0881d0f82406541417526;
+    address internal constant WSTETH_MARKET_ADDR = 0x8d733F7ea7c23Cbea7C613B6eBd845d46d3aAc54;
+    address internal constant RETH_MARKET_ADDR = 0x6106046F031a22713697e04C08B330dDaf3e8789;
 }
