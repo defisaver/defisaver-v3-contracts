9ad60151d8248b05b2d366d821cfbb06cfd715fd
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-06-05 14:42:39 +0200

    fix: checksum address

--- contracts/actions/summerfi/helpers/ArbitrumSFAddresses.sol
+++ contracts/actions/summerfi/helpers/ArbitrumSFAddresses.sol
@@ -3,6 +3,6 @@
 pragma solidity =0.8.24;
 
 contract ArbitrumSFAddresses {
-    address constant SF_OPERATION_EXECUTOR = 0x90feaf7727a6ce75f518728d296877830fd39a49;
+    address constant SF_OPERATION_EXECUTOR = 0x90feAF7727A6CE75F518728d296877830FD39a49;
     address constant SF_OPERATIONS_REGISTRY = 0x392ACeBea829373A3eFDc0dA80a16003106d8f6E;
 }
