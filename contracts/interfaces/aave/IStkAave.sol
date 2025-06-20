01c64f7307e34eaf566d71bac3f016e346bbb5ff
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-06-20 15:30:58 +0200

    feat: make IStkAave to interface type

--- contracts/interfaces/aave/IStkAave.sol
+++ contracts/interfaces/aave/IStkAave.sol
@@ -2,11 +2,11 @@
 
 pragma solidity =0.8.24;
 
-abstract contract IStkAave {
-  function cooldown() external virtual;
-  function redeem(address to, uint256 amount) external virtual;
-  function REWARD_TOKEN() external virtual view returns (address);
-  function stake(address onBehalfOf, uint256 amount) external virtual;
-  function claimRewards(address to, uint256 amount) external virtual;
-  function STAKED_TOKEN() external virtual view returns (address);
-} 
+interface IStkAave {
+    function cooldown() external virtual;
+    function redeem(address to, uint256 amount) external virtual;
+    function REWARD_TOKEN() external virtual view returns (address);
+    function stake(address onBehalfOf, uint256 amount) external virtual;
+    function claimRewards(address to, uint256 amount) external virtual;
+    function STAKED_TOKEN() external virtual view returns (address);
+}
