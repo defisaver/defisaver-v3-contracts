3f3e5d7d4e057b86ea75ac3d9fa7c72bc68e4c39
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-07-15 19:53:20 +0200

    feat: update compV3 strategies deployment script

--- scripts/deploy-compV3-strategies.js
+++ scripts/deploy-compV3-strategies.js
@@ -18,7 +18,7 @@ const {
     createCompV3EOAFLCloseToCollStrategy,
     createCompV3FLCloseToCollStrategy,
 } = require('../strategies-spec/mainnet');
-const { createBundle } = require('../test/strategies/utils/utils-strategies');
+const { createBundle, createStrategy } = require('../test/strategies/utils/utils-strategies');
 const {
     createCompV3EOABoostOnPriceL2Strategy,
     createCompV3EOAFLBoostOnPriceL2Strategy,
@@ -36,34 +36,40 @@ const {
 
 const deployCompV3BoostOnPriceBundle = async (isEOA, isL2) => {
     await openStrategyAndBundleStorage(true);
-    const boostOnPriceStrategyId = isEOA
+    const boostOnPriceStrategy = isEOA
         ? (isL2 ? createCompV3EOABoostOnPriceL2Strategy() : createCompV3EOABoostOnPriceStrategy())
         : (isL2 ? createCompV3BoostOnPriceL2Strategy() : createCompV3BoostOnPriceStrategy());
-    const flBoostOnPriceStrategyId = isEOA
+    const flBoostOnPriceStrategy = isEOA
         ? (isL2 ? createCompV3EOAFLBoostOnPriceL2Strategy() : createCompV3EOAFLBoostOnPriceStrategy())
         : (isL2 ? createCompV3FLBoostOnPriceL2Strategy() : createCompV3FLBoostOnPriceStrategy());
+    const boostOnPriceStrategyId = await createStrategy(...boostOnPriceStrategy, false);
+    const flBoostOnPriceStrategyId = await createStrategy(...flBoostOnPriceStrategy, false);
     const bundleId = await createBundle([boostOnPriceStrategyId, flBoostOnPriceStrategyId]);
     return bundleId;
 };
 const deployCompV3RepayOnPriceBundle = async (isEOA, isL2) => {
     await openStrategyAndBundleStorage(true);
-    const repayOnPriceStrategyId = isEOA
+    const repayOnPriceStrategy = isEOA
         ? (isL2 ? createCompV3EOARepayOnPriceL2Strategy() : createCompV3EOARepayOnPriceStrategy())
         : (isL2 ? createCompV3RepayOnPriceL2Strategy() : createCompV3RepayOnPriceStrategy());
-    const flRepayOnPriceStrategyId = isEOA
+    const flRepayOnPriceStrategy = isEOA
         ? (isL2 ? createCompV3EOAFLRepayOnPriceL2Strategy() : createCompV3EOAFLRepayOnPriceStrategy())
         : (isL2 ? createCompV3FLRepayOnPriceL2Strategy() : createCompV3FLRepayOnPriceStrategy());
+    const repayOnPriceStrategyId = await createStrategy(...repayOnPriceStrategy, false);
+    const flRepayOnPriceStrategyId = await createStrategy(...flRepayOnPriceStrategy, false);
     const bundleId = await createBundle([repayOnPriceStrategyId, flRepayOnPriceStrategyId]);
     return bundleId;
 };
 const deployCompV3CloseBundle = async (isEOA, isL2) => {
     await openStrategyAndBundleStorage(true);
-    const closeStrategyId = isEOA
+    const closeStrategy = isEOA
         ? (isL2 ? createCompV3EOAFLCloseToDebtL2Strategy() : createCompV3EOAFLCloseToDebtStrategy())
         : (isL2 ? createCompV3FLCloseToDebtL2Strategy() : createCompV3FLCloseToDebtStrategy());
-    const flCloseStrategyId = isEOA
+    const flCloseStrategy = isEOA
         ? (isL2 ? createCompV3EOAFLCloseToCollL2Strategy() : createCompV3EOAFLCloseToCollStrategy())
         : (isL2 ? createCompV3FLCloseToCollL2Strategy() : createCompV3FLCloseToCollStrategy());
+    const closeStrategyId = await createStrategy(...closeStrategy, false);
+    const flCloseStrategyId = await createStrategy(...flCloseStrategy, false);
     const bundleId = await createBundle([closeStrategyId, flCloseStrategyId]);
     return bundleId;
 };
@@ -80,24 +86,24 @@ async function main() {
 
     const compV3PriceTrigger = await redeploy('CompV3PriceTrigger', true);
     const compV3PriceRangeTrigger = await redeploy('CompV3PriceRangeTrigger', true);
-    const compV3TargetRatioCheckAction = await redeploy('CompV3TargetRatioCheckAction', true);
+    const compV3TargetRatioCheckAction = await redeploy('CompV3TargetRatioCheck', true);
 
     console.log('CompV3PriceTrigger:', compV3PriceTrigger.address);
     console.log('CompV3PriceRangeTrigger:', compV3PriceRangeTrigger.address);
     console.log('CompV3TargetRatioCheckAction:', compV3TargetRatioCheckAction.address);
 
-    const boostOnPriceBundleId = await deployCompV3BoostOnPriceBundle(false, isL2);
     const repayOnPriceBundleId = await deployCompV3RepayOnPriceBundle(false, isL2);
+    const boostOnPriceBundleId = await deployCompV3BoostOnPriceBundle(false, isL2);
     const closeBundleId = await deployCompV3CloseBundle(false, isL2);
-    const eoaBoostOnPriceBundleId = await deployCompV3BoostOnPriceBundle(true, isL2);
     const eoaRepayOnPriceBundleId = await deployCompV3RepayOnPriceBundle(true, isL2);
+    const eoaBoostOnPriceBundleId = await deployCompV3BoostOnPriceBundle(true, isL2);
     const eoaCloseBundleId = await deployCompV3CloseBundle(true, isL2);
 
-    console.log('Boost on price bundle id:', boostOnPriceBundleId);
     console.log('Repay on price bundle id:', repayOnPriceBundleId);
+    console.log('Boost on price bundle id:', boostOnPriceBundleId);
     console.log('Close bundle id:', closeBundleId);
-    console.log('EOA Boost on price bundle id:', eoaBoostOnPriceBundleId);
     console.log('EOA Repay on price bundle id:', eoaRepayOnPriceBundleId);
+    console.log('EOA Boost on price bundle id:', eoaBoostOnPriceBundleId);
     console.log('EOA Close bundle id:', eoaCloseBundleId);
 }
 
