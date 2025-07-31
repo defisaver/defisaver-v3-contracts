4f14c49c25a7a109bf370dd53a6bc5276e0c04c4
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-07-31 16:39:05 +0200

    feat: adapt compV3 strategies deployment script

--- scripts/deploy-compV3-strategies.js
+++ scripts/deploy-compV3-strategies.js
@@ -3,7 +3,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const hre = require('hardhat');
 const { topUp } = require('./utils/fork');
-const { getOwnerAddr, redeploy, openStrategyAndBundleStorage, network } = require('../test/utils/utils');
+const {
+    getOwnerAddr,
+    redeploy,
+    openStrategyAndBundleStorage,
+    network,
+} = require('../test/utils/utils');
 const {
     createCompV3BoostOnPriceStrategy,
     createCompV3FLBoostOnPriceStrategy,
@@ -32,6 +37,10 @@ const {
     createCompV3FLCloseToDebtL2Strategy,
     createCompV3EOAFLCloseToCollL2Strategy,
     createCompV3FLCloseToCollL2Strategy,
+    createCompV3EOARepayL2Strategy,
+    createCompV3EOAFlRepayL2Strategy,
+    createCompV3EOABoostL2Strategy,
+    createCompV3EOAFlBoostL2Strategy,
 } = require('../strategies-spec/l2');
 
 const deployCompV3BoostOnPriceBundle = async (isEOA, isL2) => {
@@ -73,6 +82,24 @@ const deployCompV3CloseBundle = async (isEOA, isL2) => {
     const bundleId = await createBundle([flCloseToDebtStrategyId, flCloseToCollStrategyId]);
     return bundleId;
 };
+const deployCompV3EOARepayL2Bundle = async () => {
+    await openStrategyAndBundleStorage(true);
+    const repayStrategy = createCompV3EOARepayL2Strategy();
+    const flRepayStrategy = createCompV3EOAFlRepayL2Strategy();
+    const repayStrategyId = await createStrategy(...repayStrategy, true);
+    const flRepayStrategyId = await createStrategy(...flRepayStrategy, true);
+    const bundleId = await createBundle([repayStrategyId, flRepayStrategyId]);
+    return bundleId;
+};
+const deployCompV3EOABoostL2Bundle = async () => {
+    await openStrategyAndBundleStorage(true);
+    const boostStrategy = createCompV3EOABoostL2Strategy();
+    const flBoostStrategy = createCompV3EOAFlBoostL2Strategy();
+    const boostStrategyId = await createStrategy(...boostStrategy, true);
+    const flBoostStrategyId = await createStrategy(...flBoostStrategy, true);
+    const bundleId = await createBundle([boostStrategyId, flBoostStrategyId]);
+    return bundleId;
+};
 async function main() {
     /* //////////////////////////////////////////////////////////////
                                 SETUP
@@ -86,9 +113,19 @@ async function main() {
 
     const compV3PriceTrigger = await redeploy('CompV3PriceTrigger', true);
     const compV3PriceRangeTrigger = await redeploy('CompV3PriceRangeTrigger', true);
+    const compV3Borrow = await redeploy('CompV3Borrow', true);
+    const compV3Payback = await redeploy('CompV3Payback', true);
+    const compV3Supply = await redeploy('CompV3Supply', true);
+    const compV3Withdraw = await redeploy('CompV3Withdraw', true);
+    const compV3RatioCheck = await redeploy('CompV3RatioCheck', true);
 
     console.log('CompV3PriceTrigger:', compV3PriceTrigger.address);
     console.log('CompV3PriceRangeTrigger:', compV3PriceRangeTrigger.address);
+    console.log('CompV3Borrow:', compV3Borrow.address);
+    console.log('CompV3Payback:', compV3Payback.address);
+    console.log('CompV3Supply:', compV3Supply.address);
+    console.log('CompV3Withdraw:', compV3Withdraw.address);
+    console.log('CompV3RatioCheck:', compV3RatioCheck.address);
 
     const repayOnPriceBundleId = await deployCompV3RepayOnPriceBundle(false, isL2);
     const boostOnPriceBundleId = await deployCompV3BoostOnPriceBundle(false, isL2);
@@ -103,6 +140,28 @@ async function main() {
     console.log('EOA Repay on price bundle id:', eoaRepayOnPriceBundleId);
     console.log('EOA Boost on price bundle id:', eoaBoostOnPriceBundleId);
     console.log('EOA Close bundle id:', eoaCloseBundleId);
+
+    if (isL2) {
+        const eoaRepayBundleId = await deployCompV3EOARepayL2Bundle();
+        const eoaBoostBundleId = await deployCompV3EOABoostL2Bundle();
+
+        console.log('EOA Repay bundle id:', eoaRepayBundleId);
+        console.log('EOA Boost bundle id:', eoaBoostBundleId);
+
+        const existingRepayBundleOnArbitrumAndBase = 4;
+        const existingBoostBundleOnArbitrumAndBase = 5;
+
+        const compV3SubProxyL2 = await redeploy(
+            'CompV3SubProxyL2',
+            true,
+            existingRepayBundleOnArbitrumAndBase,
+            existingBoostBundleOnArbitrumAndBase,
+            eoaRepayBundleId,
+            eoaBoostBundleId,
+        );
+
+        console.log('CompV3SubProxyL2:', compV3SubProxyL2.address);
+    }
 }
 
 main().catch((error) => {
