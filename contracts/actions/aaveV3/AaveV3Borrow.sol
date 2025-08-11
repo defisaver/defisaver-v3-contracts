22aed2380c52fb277fc70d40c85a960c9eddc0ff
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-08-11 17:11:18 +0200

    fix: match AaveV3 actions with deployed code and fix default params piping

--- contracts/actions/aaveV3/AaveV3Borrow.sol
+++ contracts/actions/aaveV3/AaveV3Borrow.sol
@@ -53,6 +53,13 @@ contract AaveV3Borrow is ActionBase, AaveV3Helper {
             _returnValues
         );
 
+        if (params.useDefaultMarket) {
+            params.market = DEFAULT_AAVE_MARKET;
+        }
+        if (!params.useOnBehalf) {
+            params.onBehalf = address(0);
+        }
+
         (uint256 borrowAmount, bytes memory logData) = _borrow(
             params.market,
             params.assetId,
@@ -68,14 +75,6 @@ contract AaveV3Borrow is ActionBase, AaveV3Helper {
     /// @inheritdoc ActionBase
     function executeActionDirect(bytes memory _callData) public payable override {
         Params memory params = parseInputs(_callData);
-
-        if (params.useDefaultMarket) {
-            params.market = DEFAULT_AAVE_MARKET;
-        }
-        if (!params.useOnBehalf) {
-            params.onBehalf = address(0);
-        }
-
         (, bytes memory logData) = _borrow(
             params.market,
             params.assetId,
