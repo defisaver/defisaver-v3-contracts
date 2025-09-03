987a3be72c115eb8b875e2038903d8c053e5adf9
Milos Djurica <milosdjurica.work@gmail.com>
2025-09-03 15:27:08 +0200

    test: skeleton for aaveV3 EOA strats tests

---
+++ test/strategies/aaveV3/eoa/close.js
@@ -0,0 +1,12 @@
+// AaveV3 EOA Close strategies (CloseToColl/CloseToDebt with and without FL)
+
+const runEOACloseTests = () => {
+    describe('AaveV3 EOA Close Strategies Tests', () => {
+        it('... should execute AaveV3 EOA FL close-to-coll strategy for test pairs');
+        it('... should execute AaveV3 EOA FL close-to-debt strategy for test pairs');
+    });
+};
+
+module.exports = {
+    runEOACloseTests,
+};
