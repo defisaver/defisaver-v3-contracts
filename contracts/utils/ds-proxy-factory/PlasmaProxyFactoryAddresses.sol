b42581575c1376df1e1488ede9c542d18d20e980
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-09-25 18:40:38 +0200

    chore: match the right ds proxy address for plasma

--- contracts/utils/ds-proxy-factory/PlasmaProxyFactoryAddresses.sol
+++ contracts/utils/ds-proxy-factory/PlasmaProxyFactoryAddresses.sol
@@ -3,5 +3,5 @@
 pragma solidity =0.8.24;
 
 contract PlasmaProxyFactoryAddresses {
-    address internal constant PROXY_FACTORY_ADDR = 0x3Ca96CebC7779Ee86685c67c999d0f03158Ee9cA; // TODO:P
+    address internal constant PROXY_FACTORY_ADDR = 0x3Ca96CebC7779Ee86685c67c999d0f03158Ee9cA;
 }
