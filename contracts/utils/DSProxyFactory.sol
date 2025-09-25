aef003765a5bf11a114070c2df41c6968fe70f48
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-09-25 18:35:31 +0200

    feat: add mock DSProxyFactory implementation used for chains without dsproxy

---
+++ contracts/utils/DSProxyFactory.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.24;
+
+/// @title DSProxyFactory
+/// @notice Mock implementation without real build functionality.
+/// @dev This contract is used as a placeholder for chains without DSProxy support.
+contract DSProxyFactory {
+    /// @dev Will always return false.
+    mapping(address=>bool) public isProxy;
+
+    function build() public returns (address proxy) {
+        revert();
+    }
+
+    function build(address owner) public returns (address proxy) {
+        revert();
+    }
+}
