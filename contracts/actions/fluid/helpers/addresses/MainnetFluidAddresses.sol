de9b1e8e485e3eb196196c21619631990c15379d
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-10-16 19:05:08 +0200

    fix: rename to NATIVE_TOKEN_ADDR for all chains

--- contracts/actions/fluid/helpers/addresses/MainnetFluidAddresses.sol
+++ contracts/actions/fluid/helpers/addresses/MainnetFluidAddresses.sol
@@ -6,6 +6,6 @@ contract MainnetFluidAddresses {
     address internal constant FLUID_DEX_RESOLVER = 0x71783F64719899319B56BdA4F27E1219d9AF9a3d;
     address internal constant FLUID_LENDING_RESOLVER = 0xC215485C572365AE87f908ad35233EC2572A3BEC;
     address internal constant F_WRAPPED_NATIVE_TOKEN_ADDR = 0x90551c1795392094FE6D29B758EcCD233cFAa260;
-    address internal constant ETH_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address internal constant NATIVE_TOKEN_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     address internal constant FLUID_MERKLE_DISTRIBUTOR = 0x7060FE0Dd3E31be01EFAc6B28C8D38018fD163B0;
 }
