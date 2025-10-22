1de768df74a8a59ca8b55ef8afac40ef99552d3d
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-10-22 09:31:31 +0200

    chore: add test deployment for DSAProxy

--- contracts/core/helpers/MainnetCoreAddresses.sol
+++ contracts/core/helpers/MainnetCoreAddresses.sol
@@ -8,7 +8,7 @@ contract MainnetCoreAddresses {
 
     address internal constant PROXY_AUTH_ADDR = 0x149667b6FAe2c63D1B4317C716b0D0e4d3E2bD70;
     address internal constant MODULE_AUTH_ADDR = 0x7407974DDBF539e552F1d051e44573090912CC3D;
-    address internal constant DSA_AUTH_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address internal constant DSA_AUTH_ADDR = 0xfc6f5dDEEC0e6122a501d9Ba8a9b428c74c4640A;
 
     address internal constant SUB_STORAGE_ADDR = 0x1612fc28Ee0AB882eC99842Cde0Fc77ff0691e90;
     address internal constant BUNDLE_STORAGE_ADDR = 0x223c6aDE533851Df03219f6E3D8B763Bd47f84cf;
