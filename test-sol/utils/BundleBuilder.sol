2e2317bc1e6f8628a08a61d8d4ef45e25e3b5957
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-10-26 13:39:59 +0100

    refactor: use IAdminVault inside AdminAuth

--- test-sol/utils/BundleBuilder.sol
+++ test-sol/utils/BundleBuilder.sol
@@ -2,7 +2,7 @@
 pragma solidity =0.8.24;
 
 import { BundleStorage } from "../../contracts/core/strategy/BundleStorage.sol";
-import { AdminVault } from "../../contracts/auth/AdminAuth.sol";
+import { IAdminVault } from "../../contracts/interfaces/auth/IAdminVault.sol";
 import { Addresses } from "./Addresses.sol";
 import { CheatCodes } from "./CheatCodes.sol";
 
@@ -10,7 +10,7 @@ contract BundleBuilder is CheatCodes {
     function init(uint64[] memory _strategyIds) public returns (uint256 bundleId) {
         BundleStorage bundle = BundleStorage(Addresses.BUNDLE_ADDR);
 
-        cheats.startPrank(AdminVault(bundle.adminVault()).owner());
+        cheats.startPrank(IAdminVault(bundle.adminVault()).owner());
         bundleId = bundle.createBundle(_strategyIds);
         cheats.stopPrank();
     }
