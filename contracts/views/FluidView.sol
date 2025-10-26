ab218c1ecd5141f9347498af6cf16964e499f17f
Milos Djurica <milosdjurica.work@gmail.com>
2025-10-26 10:29:05 +0100

    fix: fix package.json and format on commit

--- contracts/views/FluidView.sol
+++ contracts/views/FluidView.sol
@@ -263,9 +263,13 @@ contract FluidView is FluidRatioHelper {
             borrowToken0: borrowToken0,
             borrowToken1: borrowToken1,
             supplyToken0Decimals: supplyToken0 != NATIVE_TOKEN_ADDR ? IERC20(supplyToken0).decimals() : 18,
-            supplyToken1Decimals: supplyToken1 != address(0) ? (supplyToken1 != NATIVE_TOKEN_ADDR ? IERC20(supplyToken1).decimals() : 18) : 0,
-            borrowToken0Decimals: borrowToken0 != NATIVE_TOKEN_ADDR ? IERC20(borrowToken0).decimals(): 18,
-            borrowToken1Decimals: borrowToken1 != address(0) ? (borrowToken1 != NATIVE_TOKEN_ADDR ? IERC20(borrowToken1).decimals() : 18) : 0,
+            supplyToken1Decimals: supplyToken1 != address(0)
+                ? (supplyToken1 != NATIVE_TOKEN_ADDR ? IERC20(supplyToken1).decimals() : 18)
+                : 0,
+            borrowToken0Decimals: borrowToken0 != NATIVE_TOKEN_ADDR ? IERC20(borrowToken0).decimals() : 18,
+            borrowToken1Decimals: borrowToken1 != address(0)
+                ? (borrowToken1 != NATIVE_TOKEN_ADDR ? IERC20(borrowToken1).decimals() : 18)
+                : 0,
             collateralFactor: data.configs.collateralFactor,
             liquidationThreshold: data.configs.liquidationThreshold,
             liquidationMaxLimit: data.configs.liquidationMaxLimit,
@@ -562,7 +566,7 @@ contract FluidView is FluidRatioHelper {
         // To lower the gas cost, we cap the gas limit at 9M, ensuring ~140k gas remains for fetching fWETH details
         // and enough gas is left for further operations within the same block.
         // For arbitrum, we don't need to cap as WETH will have EIP-2612 support.
-        if (_fToken == F_WRAPPED_NATIVE_TOKEN_ADDR && block.chainid != 42161) {
+        if (_fToken == F_WRAPPED_NATIVE_TOKEN_ADDR && block.chainid != 42_161) {
             details = IFluidLendingResolver(FLUID_LENDING_RESOLVER).getFTokenDetails{ gas: 9_000_000 }(_fToken);
         } else {
             details = IFluidLendingResolver(FLUID_LENDING_RESOLVER).getFTokenDetails(_fToken);
