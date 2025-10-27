83455b80f4ab8b4fa598524b97842e33cbf4b3a8
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-10-27 10:06:12 +0100

    feat: add fetchDSAProxyAccounts to GeneralView

--- contracts/interfaces/protocols/insta/IInstaList.sol
+++ contracts/interfaces/protocols/insta/IInstaList.sol
@@ -13,8 +13,21 @@ interface IInstaList {
         address next;
     }
 
+    struct UserLink {
+        uint64 first;
+        uint64 last;
+        uint64 count;
+    }
+
+    struct UserList {
+        uint64 prev;
+        uint64 next;
+    }
+
     function accountAddr(uint64 _id) external view returns (address);
     function accountID(address _addr) external view returns (uint64);
     function accountLink(uint64 _id) external view returns (AccountLink memory);
     function accountList(uint64 _id, address _user) external view returns (AccountList memory);
+    function userLink(address _user) external view returns (UserLink memory);
+    function userList(address _user, uint64 _id) external view returns (UserList memory);
 }
--- contracts/views/GeneralView.sol
+++ contracts/views/GeneralView.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.24;
 
+import { IInstaList } from "../interfaces/protocols/insta/IInstaList.sol";
 import { SmartWalletUtils } from "../utils/SmartWalletUtils.sol";
 import { WalletType } from "../utils/DFSTypes.sol";
 
@@ -16,5 +17,22 @@ contract GeneralView is SmartWalletUtils {
     {
         smartWalletType = _getWalletType(_smartWalletAddress);
         owner = _fetchOwnerOrWallet(_smartWalletAddress);
+    }
+
+    /// @notice Fetches the DSA Proxy Accounts for a user
+    /// @param _user Address of the user
+    /// @return accounts Array of DSA Proxy Accounts
+    function fetchDSAProxyAccounts(address _user) public view returns (address[] memory accounts) {
+        IInstaList list = IInstaList(DSA_LIST_ADDR);
+
+        IInstaList.UserLink memory userLink = list.userLink(_user);
+
+        accounts = new address[](userLink.count);
+
+        uint64 currentId = userLink.first;
+        for (uint64 i = 0; i < userLink.count; i++) {
+            accounts[i] = list.accountAddr(currentId);
+            currentId = list.userList(_user, currentId).next;
+        }
     }
 }
