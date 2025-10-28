be98bd813275e5f84c7c5bc7df88884839961f61
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-10-28 12:02:59 +0100

    feat: remove Linea & Insta connector addresses

--- contracts/actions/insta/helpers/PlasmaInstaConnectorAddresses.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity =0.8.24;
-
-contract PlasmaInstaConnectorAddresses {
-    address internal constant DEFISAVER_REGISTRY_ADDR = 0x44e98bB58d725F2eF93a195F518b335dCB784c78;
-    uint256 internal constant CONNECTOR_ID = 1;
-}
