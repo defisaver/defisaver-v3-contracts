956f2c983e5e43977b3fe674b0c408eb9d44743b
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2025-10-28 12:32:34 +0100

    feat: remove DSAProxy factory for linea and plasma

--- contracts/utils/addresses/dsaProxyFactory/PlasmaDSAProxyFactoryAddresses.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity =0.8.24;
-
-contract PlasmaDSAProxyFactoryAddresses {
-    address internal constant DSA_LIST_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-}
