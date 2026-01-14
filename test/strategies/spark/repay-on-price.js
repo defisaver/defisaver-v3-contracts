e3f4e1c8c5066e0ee94b8fb7e09b82efb681705f
marko-makaric <marko.makaric@decenter.com>
2026-01-14 15:38:07 +0100

    chore: added modular test structure for on price tests

--- test/strategies/spark/repay-on-price.js
+++ test/strategies/spark/repay-on-price.js
@@ -40,210 +40,226 @@ const TRIGGER_PRICE_OVER = 0;
 const PRICE_STATE_UNDER = automationSdk.enums.RatioState.UNDER;
 const PRICE_STATE_OVER = automationSdk.enums.RatioState.OVER;
 
-describe('Spark Repay On Price Strategy Tests', function () {
-    this.timeout(1200000);
+const runRepayOnPriceTests = () => {
+    describe('Spark Repay On Price Strategy Tests', function () {
+        this.timeout(1200000);
 
-    let snapshotId;
-    let senderAcc;
-    let proxy;
-    let botAcc;
-    let strategyExecutor;
-    let mockWrapper;
-    let bundleId;
-    let flAddr;
+        let snapshotId;
+        let senderAcc;
+        let proxy;
+        let botAcc;
+        let strategyExecutor;
+        let mockWrapper;
+        let bundleId;
+        let flAddr;
 
-    before(async () => {
-        const isFork = isNetworkFork();
-        await hre.network.provider.send('hardhat_setNextBlockBaseFeePerGas', ['0x0']);
+        before(async () => {
+            const isFork = isNetworkFork();
+            await hre.network.provider.send('hardhat_setNextBlockBaseFeePerGas', ['0x0']);
 
-        senderAcc = (await hre.ethers.getSigners())[0];
-        botAcc = (await hre.ethers.getSigners())[1];
-        await sendEther(senderAcc, addrs[network].OWNER_ACC, '10');
-        proxy = await getProxy(senderAcc.address);
-        await addBotCaller(botAcc.address, isFork);
+            senderAcc = (await hre.ethers.getSigners())[0];
+            botAcc = (await hre.ethers.getSigners())[1];
+            await sendEther(senderAcc, addrs[network].OWNER_ACC, '10');
+            proxy = await getProxy(senderAcc.address);
+            await addBotCaller(botAcc.address, isFork);
 
-        strategyExecutor = (await getStrategyExecutorContract()).connect(botAcc);
-        mockWrapper = await getAndSetMockExchangeWrapper(senderAcc);
+            strategyExecutor = (await getStrategyExecutorContract()).connect(botAcc);
+            mockWrapper = await getAndSetMockExchangeWrapper(senderAcc);
 
-        const flContract = await getContractFromRegistry('FLAction', isFork);
-        flAddr = flContract.address;
+            const flContract = await getContractFromRegistry('FLAction', isFork);
+            flAddr = flContract.address;
 
-        await redeploy('SparkTargetRatioCheck', isFork);
+            await redeploy('SparkTargetRatioCheck', isFork);
 
-        bundleId = await deploySparkRepayOnPriceBundle();
-    });
+            bundleId = await deploySparkRepayOnPriceBundle();
+        });
 
-    beforeEach(async () => {
-        snapshotId = await takeSnapshot();
-    });
+        beforeEach(async () => {
+            snapshotId = await takeSnapshot();
+        });
 
-    afterEach(async () => {
-        await revertToSnapshot(snapshotId);
-    });
+        afterEach(async () => {
+            await revertToSnapshot(snapshotId);
+        });
 
-    const baseTest = async (
-        collAsset,
-        debtAsset,
-        targetRatio,
-        collAmountInUSD,
-        debtAmountInUSD,
-        repayAmountInUSD,
-        isFLStrategy,
-        triggerPrice,
-        priceState,
-        marketAddress,
-    ) => {
-        const positionOwner = proxy.address;
-
-        await openSparkProxyPosition(
-            senderAcc.address,
-            proxy,
-            collAsset.symbol,
-            debtAsset.symbol,
-            collAmountInUSD,
-            debtAmountInUSD,
-            marketAddress,
-        );
-
-        // Check ratioBefore
-        const ratioBefore = await getSparkPositionRatio(positionOwner, null, marketAddress);
-        console.log('ratioBefore', ratioBefore);
-
-        // Get asset IDs
-        const collAssetId = (await getSparkReserveDataFromPool(collAsset.address, marketAddress))
-            .id;
-        const debtAssetId = (await getSparkReserveDataFromPool(debtAsset.address, marketAddress))
-            .id;
-
-        // Create subscription
-        const result = await subSparkRepayOnPriceBundle(
-            proxy,
-            bundleId,
-            collAsset.address,
-            collAssetId,
-            debtAsset.address,
-            debtAssetId,
-            marketAddress,
-            triggerPrice,
-            priceState,
-            targetRatio,
-        );
-        const repaySubId = result.subId;
-        const strategySub = result.strategySub;
-
-        const repayAmount = await fetchAmountInUSDPrice(collAsset.symbol, repayAmountInUSD);
-        const exchangeObject = await formatMockExchangeObjUsdFeed(
+        const baseTest = async (
             collAsset,
             debtAsset,
-            repayAmount,
-            mockWrapper,
-        );
+            targetRatio,
+            collAmountInUSD,
+            debtAmountInUSD,
+            repayAmountInUSD,
+            isFLStrategy,
+            triggerPrice,
+            priceState,
+            marketAddress,
+        ) => {
+            const positionOwner = proxy.address;
 
-        // Execute strategy
-        if (isFLStrategy) {
-            await callSparkFLRepayOnPriceStrategy(
-                strategyExecutor,
-                1,
-                repaySubId,
-                strategySub,
-                exchangeObject,
-                repayAmount,
+            await openSparkProxyPosition(
+                senderAcc.address,
+                proxy,
+                collAsset.symbol,
+                debtAsset.symbol,
+                collAmountInUSD,
+                debtAmountInUSD,
+                marketAddress,
+            );
+
+            // Check ratioBefore
+            const ratioBefore = await getSparkPositionRatio(positionOwner, null, marketAddress);
+            console.log('ratioBefore', ratioBefore);
+
+            // Get asset IDs
+            const collAssetId = (
+                await getSparkReserveDataFromPool(collAsset.address, marketAddress)
+            ).id;
+            const debtAssetId = (
+                await getSparkReserveDataFromPool(debtAsset.address, marketAddress)
+            ).id;
+
+            // Create subscription
+            const result = await subSparkRepayOnPriceBundle(
+                proxy,
+                bundleId,
                 collAsset.address,
-                flAddr,
+                collAssetId,
+                debtAsset.address,
+                debtAssetId,
+                marketAddress,
+                triggerPrice,
+                priceState,
+                targetRatio,
             );
-        } else {
-            await callSparkRepayOnPriceStrategy(
-                strategyExecutor,
-                0,
-                repaySubId,
-                strategySub,
-                exchangeObject,
+            const repaySubId = result.subId;
+            const strategySub = result.strategySub;
+
+            const repayAmount = await fetchAmountInUSDPrice(collAsset.symbol, repayAmountInUSD);
+            const exchangeObject = await formatMockExchangeObjUsdFeed(
+                collAsset,
+                debtAsset,
                 repayAmount,
+                mockWrapper,
             );
+
+            // Execute strategy
+            if (isFLStrategy) {
+                await callSparkFLRepayOnPriceStrategy(
+                    strategyExecutor,
+                    1,
+                    repaySubId,
+                    strategySub,
+                    exchangeObject,
+                    repayAmount,
+                    collAsset.address,
+                    flAddr,
+                );
+            } else {
+                await callSparkRepayOnPriceStrategy(
+                    strategyExecutor,
+                    0,
+                    repaySubId,
+                    strategySub,
+                    exchangeObject,
+                    repayAmount,
+                );
+            }
+
+            const ratioAfter = await getSparkPositionRatio(positionOwner, null, marketAddress);
+            console.log('ratioAfter', ratioAfter);
+            console.log('ratioBefore', ratioBefore);
+            expect(ratioAfter).to.be.gt(ratioBefore);
+        };
+
+        const testPairs = SPARK_AUTOMATION_TEST_PAIRS_REPAY || [];
+        for (let i = 0; i < testPairs.length; ++i) {
+            const pair = testPairs[i];
+            const collAsset = getAssetInfo(pair.collSymbol, chainIds[network]);
+            const debtAsset = getAssetInfo(pair.debtSymbol, chainIds[network]);
+
+            it(`... should execute Spark Repay on price strategy (UNDER) for ${pair.collSymbol} /
+            ${pair.debtSymbol} pair on Spark ${addrs[network].SPARK_MARKET}`, async () => {
+                const isFLStrategy = false;
+
+                await baseTest(
+                    collAsset,
+                    debtAsset,
+                    pair.targetRatioRepay,
+                    pair.collAmountInUSD,
+                    pair.debtAmountInUSD,
+                    pair.repayAmountInUSD,
+                    isFLStrategy,
+                    TRIGGER_PRICE_UNDER,
+                    PRICE_STATE_UNDER,
+                    pair.marketAddr,
+                );
+            });
+
+            it(`... should execute Spark Repay on price strategy (OVER) for ${pair.collSymbol} /
+            ${pair.debtSymbol} pair on Spark ${addrs[network].SPARK_MARKET}`, async () => {
+                const isFLStrategy = false;
+
+                await baseTest(
+                    collAsset,
+                    debtAsset,
+                    pair.targetRatioRepay,
+                    pair.collAmountInUSD,
+                    pair.debtAmountInUSD,
+                    pair.repayAmountInUSD,
+                    isFLStrategy,
+                    TRIGGER_PRICE_OVER,
+                    PRICE_STATE_OVER,
+                    pair.marketAddr,
+                );
+            });
+
+            it(`... should execute Spark FL Repay on price strategy (UNDER) for ${pair.collSymbol} /
+            ${pair.debtSymbol} pair on Spark ${addrs[network].SPARK_MARKET}`, async () => {
+                const isFLStrategy = true;
+
+                await baseTest(
+                    collAsset,
+                    debtAsset,
+                    pair.targetRatioRepay,
+                    pair.collAmountInUSD,
+                    pair.debtAmountInUSD,
+                    pair.repayAmountInUSD,
+                    isFLStrategy,
+                    TRIGGER_PRICE_UNDER,
+                    PRICE_STATE_UNDER,
+                    pair.marketAddr,
+                );
+            });
+
+            it(`... should execute Spark FL Repay on price strategy (OVER) for ${pair.collSymbol} /
+            ${pair.debtSymbol} pair on Spark ${addrs[network].SPARK_MARKET}`, async () => {
+                const isFLStrategy = true;
+
+                await baseTest(
+                    collAsset,
+                    debtAsset,
+                    pair.targetRatioRepay,
+                    pair.collAmountInUSD,
+                    pair.debtAmountInUSD,
+                    pair.repayAmountInUSD,
+                    isFLStrategy,
+                    TRIGGER_PRICE_OVER,
+                    PRICE_STATE_OVER,
+                    pair.marketAddr,
+                );
+            });
         }
+    });
+};
 
-        const ratioAfter = await getSparkPositionRatio(positionOwner, null, marketAddress);
-        console.log('ratioAfter', ratioAfter);
-        console.log('ratioBefore', ratioBefore);
-        expect(ratioAfter).to.be.gt(ratioBefore);
-    };
+describe('Spark repay on price strategies test', function () {
+    this.timeout(80000);
 
-    const testPairs = SPARK_AUTOMATION_TEST_PAIRS_REPAY || [];
-    for (let i = 0; i < testPairs.length; ++i) {
-        const pair = testPairs[i];
-        const collAsset = getAssetInfo(pair.collSymbol, chainIds[network]);
-        const debtAsset = getAssetInfo(pair.debtSymbol, chainIds[network]);
-
-        it(`... should execute Spark Repay on price strategy (UNDER) for ${pair.collSymbol} /
-            ${pair.debtSymbol} pair on Spark ${addrs[network].SPARK_MARKET}`, async () => {
-            const isFLStrategy = false;
-
-            await baseTest(
-                collAsset,
-                debtAsset,
-                pair.targetRatioRepay,
-                pair.collAmountInUSD,
-                pair.debtAmountInUSD,
-                pair.repayAmountInUSD,
-                isFLStrategy,
-                TRIGGER_PRICE_UNDER,
-                PRICE_STATE_UNDER,
-                pair.marketAddr,
-            );
-        });
-
-        it(`... should execute Spark Repay on price strategy (OVER) for ${pair.collSymbol} /
-            ${pair.debtSymbol} pair on Spark ${addrs[network].SPARK_MARKET}`, async () => {
-            const isFLStrategy = false;
-
-            await baseTest(
-                collAsset,
-                debtAsset,
-                pair.targetRatioRepay,
-                pair.collAmountInUSD,
-                pair.debtAmountInUSD,
-                pair.repayAmountInUSD,
-                isFLStrategy,
-                TRIGGER_PRICE_OVER,
-                PRICE_STATE_OVER,
-                pair.marketAddr,
-            );
-        });
-
-        it(`... should execute Spark FL Repay on price strategy (UNDER) for ${pair.collSymbol} /
-            ${pair.debtSymbol} pair on Spark ${addrs[network].SPARK_MARKET}`, async () => {
-            const isFLStrategy = true;
-
-            await baseTest(
-                collAsset,
-                debtAsset,
-                pair.targetRatioRepay,
-                pair.collAmountInUSD,
-                pair.debtAmountInUSD,
-                pair.repayAmountInUSD,
-                isFLStrategy,
-                TRIGGER_PRICE_UNDER,
-                PRICE_STATE_UNDER,
-                pair.marketAddr,
-            );
-        });
-
-        it(`... should execute Spark FL Repay on price strategy (OVER) for ${pair.collSymbol} /
-            ${pair.debtSymbol} pair on Spark ${addrs[network].SPARK_MARKET}`, async () => {
-            const isFLStrategy = true;
-
-            await baseTest(
-                collAsset,
-                debtAsset,
-                pair.targetRatioRepay,
-                pair.collAmountInUSD,
-                pair.debtAmountInUSD,
-                pair.repayAmountInUSD,
-                isFLStrategy,
-                TRIGGER_PRICE_OVER,
-                PRICE_STATE_OVER,
-                pair.marketAddr,
-            );
-        });
-    }
+    it('... test Spark repay on price strategies', async () => {
+        await runRepayOnPriceTests();
+    }).timeout(50000);
 });
+
+module.exports = {
+    runRepayOnPriceTests,
+};
