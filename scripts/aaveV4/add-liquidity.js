62739b9e1ca28322786f0a6a5f8b60a95e7abb5e
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2026-03-06 15:49:41 +0100

    feat: update add liquidity and caps scripts to work for all spokes and hubs

--- scripts/aaveV4/add-liquidity.js
+++ scripts/aaveV4/add-liquidity.js
@@ -1,31 +1,31 @@
 const hre = require('hardhat');
 const { topUp } = require('../utils/fork');
 const { network, approve, setBalance } = require('../../test/utils/utils');
+const { SPOKES } = require('../../test/utils/aaveV4');
 
 async function main() {
     const senderAcc = (await hre.ethers.getSigners())[0];
     await topUp(senderAcc.address, network);
 
-    const coreSpoke = '0x46539e9123A18c427e6b4DFF114c28CF405Cb023';
-    const spoke = await hre.ethers.getContractAt('ISpoke', coreSpoke);
-    const reserveCount = await spoke.getReserveCount();
-
-    for (let i = 0; i < reserveCount; i++) {
-        const reserve = await spoke.getReserve(i);
-        const underlying = reserve.underlying;
-        console.log('Underlying:', underlying);
-        const amount = hre.ethers.utils.parseUnits('1000000', reserve.decimals);
-
-        try {
-            await setBalance(underlying, senderAcc.address, amount);
-            await approve(underlying, spoke.address, senderAcc);
-            await spoke.supply(i, amount, senderAcc.address, { gasLimit: 3000000 });
-        } catch (error) {
-            console.error('Failed to add liquidity for reserve', underlying, i);
+    for (const spokeAddr of SPOKES) {
+        const spokeContract = await hre.ethers.getContractAt('ISpoke', spokeAddr);
+        const reserveCount = await spokeContract.getReserveCount();
+        for (let i = 0; i < reserveCount; i++) {
+            const reserve = await spokeContract.getReserve(i);
+            const underlying = reserve.underlying;
+            console.log('Underlying:', underlying);
+            const amount = hre.ethers.utils.parseUnits('1000000', reserve.decimals);
+            try {
+                await setBalance(underlying, senderAcc.address, amount);
+                await approve(underlying, spokeContract.address, senderAcc);
+                await spokeContract.supply(i, amount, senderAcc.address, { gasLimit: 3000000 });
+            } catch (error) {
+                console.error('Failed to add liquidity for reserve', underlying, i);
+            }
         }
+        console.log('Finished adding liquidity for spoke', spokeAddr);
     }
-
-    console.log('Finished adding liquidity');
+    console.log('Finished adding liquidity for all spokes');
 }
 
 main().catch((error) => {
--- scripts/aaveV4/set-aaveV4-caps.js
+++ scripts/aaveV4/set-aaveV4-caps.js
@@ -1,40 +1,42 @@
 const hre = require('hardhat');
 const { topUp } = require('../utils/fork');
 const { network } = require('../../test/utils/utils');
+const { SPOKES, HUBS } = require('../../test/utils/aaveV4');
 
 async function main() {
     const senderAcc = (await hre.ethers.getSigners())[0];
     await topUp(senderAcc.address, network);
 
-    const coreHub = '0x3Ed2C9829FBCab6015E331a0352F8ae148217D70';
-    const coreSpoke = '0x46539e9123A18c427e6b4DFF114c28CF405Cb023';
-    const authority = '0x56D22D619355ab10B80933c7F50569d95576D003';
-
-    const hub = await hre.ethers.getContractAt('IHub', coreHub);
-    const spoke = await hre.ethers.getContractAt('ISpoke', coreSpoke);
-
     const mockArtifact = await hre.artifacts.readArtifact('MockAuthorityAaveV4');
-    await hre.network.provider.send('hardhat_setCode', [authority, mockArtifact.deployedBytecode]);
-
     const spokeConfig = ['1099511627774', '1099511627774', '16777214', true, false];
 
-    const reserveCount = await spoke.getReserveCount();
-
-    for (let i = 0; i < reserveCount; i++) {
-        const reserve = await spoke.getReserve(i);
-        if (reserve.hub !== coreHub) {
-            const hubAlt = await hre.ethers.getContractAt('IHub', reserve.hub);
-            const authorityAlt = await hubAlt.authority();
-            await hre.network.provider.send('hardhat_setCode', [
-                authorityAlt,
-                mockArtifact.deployedBytecode,
-            ]);
-            await hubAlt.updateSpokeConfig(reserve.assetId, coreSpoke, spokeConfig);
-        } else {
-            await hub.updateSpokeConfig(reserve.assetId, coreSpoke, spokeConfig);
-        }
+    // Set mock authority for all hubs
+    for (const hubAddr of HUBS) {
+        const hubContract = await hre.ethers.getContractAt('IHub', hubAddr);
+        const authority = await hubContract.authority();
+        await hre.network.provider.send('hardhat_setCode', [
+            authority,
+            mockArtifact.deployedBytecode,
+        ]);
+        console.log(`Set mock authority for ${hubAddr}`);
     }
-    console.log('AaveV4 caps set successfully');
+    console.log('Finished setting mock authorities for all hubs');
+
+    // Set spoke config for all spokes
+    for (const spokeAddr of SPOKES) {
+        const spokeContract = await hre.ethers.getContractAt('ISpoke', spokeAddr);
+        const reserveCount = await spokeContract.getReserveCount();
+        for (let i = 0; i < reserveCount; i++) {
+            const reserve = await spokeContract.getReserve(i);
+            const hub = await hre.ethers.getContractAt('IHub', reserve.hub);
+            await hub.updateSpokeConfig(reserve.assetId, spokeAddr, spokeConfig);
+            console.log(
+                `Set spoke config for ${spokeAddr} on ${reserve.hub} for asset ${reserve.assetId}`,
+            );
+        }
+        console.log(`Finished setting spoke config for ${spokeAddr}`);
+    }
+    console.log('Finished setting spoke configs for all spokes');
 }
 
 main().catch((error) => {
