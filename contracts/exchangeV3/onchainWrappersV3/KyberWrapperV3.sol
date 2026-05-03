b8eefe2db5e6d9c8996833f424c282d56925ca06
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2026-05-03 17:08:32 +0200

    feat: improve natspec for on-chain and off-chain wrappers

--- contracts/exchangeV3/onchainWrappersV3/KyberWrapperV3.sol
+++ contracts/exchangeV3/onchainWrappersV3/KyberWrapperV3.sol
@@ -3,29 +3,39 @@
 pragma solidity =0.8.24;
 
 import { IKyberNetworkProxy } from "../../interfaces//exchange/IKyberNetworkProxy.sol";
+import { IERC20 } from "../../interfaces/token/IERC20.sol";
 import { IExchangeV3 } from "../../interfaces/exchange/IExchangeV3.sol";
+
 import { DSMath } from "../../_vendor/DS/DSMath.sol";
 import { AdminAuth } from "../../auth/AdminAuth.sol";
 import { WrapperHelper } from "./helpers/WrapperHelper.sol";
 import { TokenUtils } from "../../utils/token/TokenUtils.sol";
 import { DFSExchangeHelper } from "../DFSExchangeHelper.sol";
 import { SafeERC20 } from "../../_vendor/openzeppelin/SafeERC20.sol";
-import { IERC20 } from "../../interfaces/token/IERC20.sol";
 
+/// @title KyberWrapperV3
+/// @notice Wrapper contract used when the on-chain exchange is Kyber.
+/// @notice On-chain wrappers are primarily used for simulations and testing.
+/// @dev Important security assumptions:
+/// 1. Exchange wrapper contracts are not intended to be used standalone, but as part of a DFS sell action,
+///    which performs additional checks before forwarding the order and a final slippage check after the swap.
+///    See DFSExchangeCore for more details.
+/// 2. Wrapper contracts are designed to be stateless, meaning they do not hold funds,
+///    and any token balances can be cleared by anyone.
 contract KyberWrapperV3 is DSMath, IExchangeV3, AdminAuth, WrapperHelper, DFSExchangeHelper {
     error WrongDestAmountError(uint256, uint256);
 
     using SafeERC20 for IERC20;
 
     /// @notice Sells a _srcAmount of tokens at Kyber
-    /// @param _srcAddr From token
-    /// @param _destAddr To token
-    /// @param _srcAmount From amount
-    /// @return uint Destination amount
+    /// @param _srcAddr The token to sell
+    /// @param _destAddr The token to buy
+    /// @param _srcAmount The amount of tokens to sell
+    /// @return destAmount Amount of destination tokens received
     function sell(address _srcAddr, address _destAddr, uint256 _srcAmount, bytes memory)
         external
         override
-        returns (uint256)
+        returns (uint256 destAmount)
     {
         IERC20 srcToken = IERC20(_srcAddr);
         IERC20 destToken = IERC20(_destAddr);
@@ -34,9 +44,7 @@ contract KyberWrapperV3 is DSMath, IExchangeV3, AdminAuth, WrapperHelper, DFSExc
 
         srcToken.safeApprove(address(kyberNetworkProxy), _srcAmount);
 
-        /// @dev the amount of tokens received is checked in DFSExchangeCore
-        /// @dev Exchange wrapper contracts should not be used on their own
-        uint256 destAmount = kyberNetworkProxy.trade(
+        destAmount = kyberNetworkProxy.trade(
             srcToken,
             _srcAmount,
             destToken,
@@ -47,17 +55,16 @@ contract KyberWrapperV3 is DSMath, IExchangeV3, AdminAuth, WrapperHelper, DFSExc
             WALLET_ID
         );
 
+        // Sends leftover tokens including ETH to the caller.
         sendLeftover(_srcAddr, _destAddr, payable(msg.sender));
-
-        return destAmount;
     }
 
     /// @notice Return a rate for which we can sell an amount of tokens
     /// @dev Will fail if token is over 18 decimals
-    /// @param _srcAddr From token
-    /// @param _destAddr To token
-    /// @param _srcAmount From amount
-    /// @return rate Rate
+    /// @param _srcAddr The token to sell
+    /// @param _destAddr The token to buy
+    /// @param _srcAmount The amount of tokens to sell
+    /// @return rate The sell rate
     function getSellRate(address _srcAddr, address _destAddr, uint256 _srcAmount, bytes memory)
         public
         view
@@ -68,17 +75,17 @@ contract KyberWrapperV3 is DSMath, IExchangeV3, AdminAuth, WrapperHelper, DFSExc
             .getExpectedRate(IERC20(_srcAddr), IERC20(_destAddr), _srcAmount);
 
         // multiply with decimal difference in src token
-        rate = rate * (10 ** (18 - getDecimals(_srcAddr)));
+        rate = rate * (10 ** (18 - _getDecimals(_srcAddr)));
         // divide with decimal difference in dest token
-        rate = rate / (10 ** (18 - getDecimals(_destAddr)));
+        rate = rate / (10 ** (18 - _getDecimals(_destAddr)));
     }
 
-    // solhint-disable-next-line no-empty-blocks
-    receive() external payable { }
-
-    function getDecimals(address _token) internal view returns (uint256) {
+    function _getDecimals(address _token) internal view returns (uint256) {
         if (_token == TokenUtils.ETH_ADDR) return 18;
 
         return IERC20(_token).decimals();
     }
+
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable { }
 }
--- contracts/exchangeV3/onchainWrappersV3/UniswapWrapperV3.sol
+++ contracts/exchangeV3/onchainWrappersV3/UniswapWrapperV3.sol
@@ -3,29 +3,38 @@ pragma solidity =0.8.24;
 
 import { IExchangeV3 } from "../../interfaces/exchange/IExchangeV3.sol";
 import { IUniswapRouter } from "../../interfaces/exchange/IUniswapRouter.sol";
+import { IERC20 } from "../../interfaces/token/IERC20.sol";
+
 import { DSMath } from "../../_vendor/DS/DSMath.sol";
 import { AdminAuth } from "../../auth/AdminAuth.sol";
 import { WrapperHelper } from "./helpers/WrapperHelper.sol";
 import { TokenUtils } from "../../utils/token/TokenUtils.sol";
 import { SafeERC20 } from "../../_vendor/openzeppelin/SafeERC20.sol";
-import { IERC20 } from "../../interfaces/token/IERC20.sol";
 
-/// @title DFS exchange wrapper for UniswapV2
+/// @title UniswapWrapperV3
+/// @notice Wrapper contract used when the on-chain exchange is UniswapV2.
+/// @notice On-chain wrappers are primarily used for simulations and testing.
+/// @dev Important security assumptions:
+/// 1. Exchange wrapper contracts are not intended to be used standalone, but as part of a DFS sell action,
+///    which performs additional checks before forwarding the order and a final slippage check after the swap.
+///    See DFSExchangeCore for more details.
+/// 2. Wrapper contracts are designed to be stateless, meaning they do not hold funds,
+///    and any token balances can be cleared by anyone.
 contract UniswapWrapperV3 is DSMath, IExchangeV3, AdminAuth, WrapperHelper {
     using TokenUtils for address;
+    using SafeERC20 for IERC20;
 
     IUniswapRouter public constant router = IUniswapRouter(UNI_V2_ROUTER);
 
-    using SafeERC20 for IERC20;
-
     /// @notice Sells a _srcAmount of tokens at UniswapV2
-    /// @param _srcAddr From token
-    /// @param _srcAmount From amount
-    /// @return uint Destination amount
+    /// @param _srcAddr The token to sell
+    /// @param _srcAmount The amount of tokens to sell
+    /// @param _additionalData The UniswapV2 swap path
+    /// @return amountOut Amount of destination tokens received
     function sell(address _srcAddr, address, uint256 _srcAmount, bytes memory _additionalData)
         external
         override
-        returns (uint256)
+        returns (uint256 amountOut)
     {
         uint256[] memory amounts;
         address[] memory path = abi.decode(_additionalData, (address[]));
@@ -37,29 +46,30 @@ contract UniswapWrapperV3 is DSMath, IExchangeV3, AdminAuth, WrapperHelper {
         amounts =
             router.swapExactTokensForTokens(_srcAmount, 1, path, msg.sender, block.timestamp + 1);
 
-        // cleanup tokens if anything left after sell
+        // Sends leftover source tokens to the caller.
         uint256 amountLeft = IERC20(_srcAddr).balanceOf(address(this));
 
         if (amountLeft > 0) {
             IERC20(_srcAddr).safeTransfer(msg.sender, amountLeft);
         }
 
-        return amounts[amounts.length - 1];
+        amountOut = amounts[amounts.length - 1];
     }
 
     /// @notice Return a rate for which we can sell an amount of tokens
-    /// @param _srcAmount From amount
-    /// @return uint Rate
+    /// @param _srcAmount The amount of tokens to sell
+    /// @param _additionalData The UniswapV2 swap path
+    /// @return rate The sell rate
     function getSellRate(address, address, uint256 _srcAmount, bytes memory _additionalData)
         public
         view
         override
-        returns (uint256)
+        returns (uint256 rate)
     {
         address[] memory path = abi.decode(_additionalData, (address[]));
 
         uint256[] memory amounts = router.getAmountsOut(_srcAmount, path);
-        return wdiv(amounts[amounts.length - 1], _srcAmount);
+        rate = wdiv(amounts[amounts.length - 1], _srcAmount);
     }
 
     // solhint-disable-next-line no-empty-blocks
