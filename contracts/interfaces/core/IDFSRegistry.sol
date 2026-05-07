405a3a2884dc5bd2ca2834b23cf0f94b0c3dc69f
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2026-05-07 15:00:40 +0200

    fix: use correct bytes4 param in IDFSRegistry interface

--- contracts/interfaces/core/IDFSRegistry.sol
+++ contracts/interfaces/core/IDFSRegistry.sol
@@ -5,13 +5,13 @@ pragma solidity =0.8.24;
 interface IDFSRegistry {
     function getAddr(bytes4 _id) external view returns (address);
 
-    function addNewContract(bytes32 _id, address _contractAddr, uint256 _waitPeriod) external;
+    function addNewContract(bytes4 _id, address _contractAddr, uint256 _waitPeriod) external;
 
-    function startContractChange(bytes32 _id, address _newContractAddr) external;
+    function startContractChange(bytes4 _id, address _newContractAddr) external;
 
-    function approveContractChange(bytes32 _id) external;
+    function approveContractChange(bytes4 _id) external;
 
-    function cancelContractChange(bytes32 _id) external;
+    function cancelContractChange(bytes4 _id) external;
 
-    function changeWaitPeriod(bytes32 _id, uint256 _newWaitPeriod) external;
+    function changeWaitPeriod(bytes4 _id, uint256 _newWaitPeriod) external;
 }
