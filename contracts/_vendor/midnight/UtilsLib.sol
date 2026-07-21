f9d96b29430457f7ce5eb195240bef607e0f1b01
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2026-07-21 18:54:11 +0200

    feat: add initial midnight view

---
+++ contracts/_vendor/midnight/UtilsLib.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.24;
+
+/// @dev Taken from: https://github.com/morpho-org/midnight
+/// @dev Changes:
+/// - Removed all functions except for:
+/// - `clearBit`
+/// - `msb` implementation is changed to work with cancun and 0.8.24 compiler (can't use clz from Osaka)
+library UtilsLib {
+    /// @dev Assumes bitmap is not zero.
+    function msb(uint128 bitmap) internal pure returns (uint256 res) {
+        assembly {
+            bitmap := and(bitmap, 0xffffffffffffffffffffffffffffffff)
+            res := shl(6, gt(bitmap, 0xffffffffffffffff))
+            res := or(res, shl(5, gt(shr(res, bitmap), 0xffffffff)))
+            res := or(res, shl(4, gt(shr(res, bitmap), 0xffff)))
+            res := or(res, shl(3, gt(shr(res, bitmap), 0xff)))
+            res := or(res, shl(2, gt(shr(res, bitmap), 0xf)))
+            res := or(res, shl(1, gt(shr(res, bitmap), 0x3)))
+            res := or(res, gt(shr(res, bitmap), 0x1))
+        }
+    }
+
+    /// @dev Assumes bit is less than 128.
+    function clearBit(uint128 bitmap, uint256 bit) internal pure returns (uint128) {
+        // forge-lint: disable-next-item(unsafe-typecast)
+        return uint128(bitmap & ~(1 << bit));
+    }
+}
---
+++ contracts/actions/midnight/helpers/MidnightHelper.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity =0.8.24;
+
+import { Market, CollateralParams } from "../../../interfaces/protocols/midnight/IMidnight.sol";
+import { IOracle } from "../../../interfaces/protocols/midnight/IOracle.sol";
+import { IdLib } from "../../../_vendor/midnight/IdLib.sol";
+import { UtilsLib } from "../../../_vendor/midnight/UtilsLib.sol";
+import { BaseMidnightAddresses } from "./BaseMidnightAddresses.sol";
+
+contract MidnightHelper is BaseMidnightAddresses {
+    /// @dev Oracle price is scaled by 1e36 + debtDec - collDec
+    /// If we want to calculate collateral value in deb tokens:
+    /// X = collateral * price * 10^debtDec / 10^collDec / 10^(36 + debtDec - collDec)
+    /// X = collateral * price * 10^debtDec / 10^(collDec + 36 + debtDec - collDec)
+    /// X = collateral * price * 10^debtDec / 10^(36 + debtDec)
+    /// X = collateral * price / 10^36
+    /// X = collateral * price / ORACLE_PRICE_SCALE
+    uint256 internal constant ORACLE_PRICE_SCALE = 1e36;
+    uint256 internal constant WAD = 1e18;
+
+    function getRatio(bytes32 _id, address _user) public view returns (uint256 ratio) {
+        (,,,, uint128 debt, uint128 collateralBitmap) = MIDNIGHT.position(_id, _user);
+
+        if (debt == 0) return 0;
+
+        Market memory market = MIDNIGHT.toMarket(_id);
+
+        uint256 totalCollInDebtToken;
+        while (collateralBitmap != 0) {
+            uint256 i = UtilsLib.msb(collateralBitmap);
+            uint256 price = IOracle(market.collateralParams[i].oracle).price();
+            uint256 collateral = MIDNIGHT.collateral(_id, _user, i);
+            totalCollInDebtToken += collateral * price / ORACLE_PRICE_SCALE;
+            collateralBitmap = UtilsLib.clearBit(collateralBitmap, i);
+        }
+
+        ratio = totalCollInDebtToken * WAD / debt;
+    }
+}
