4cd6c36fbb41375985c0dc0d01032011e30ab902
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2026-07-28 16:25:56 +0200

    feat: bump hardhat version for osaka support

--- hardhat.config.js
+++ hardhat.config.js
@@ -121,23 +121,24 @@ module.exports = {
                 },
                 8453: {
                     hardforkHistory: {
-                        london: 1,
+                        osaka: 1,
                     },
                 },
             },
             forking: {
-                url: process.env.ETHEREUM_NODE,
+                url: process.env.BASE_NODE,
                 timeout: 1000000,
                 gasPrice: 50000000000,
                 // blockNumber: 12068716
             },
-            name: 'mainnet',
-            hardfork: 'cancun',
+            name: 'base',
+            hardfork: 'osaka',
+            blockGasLimit: 0x1fffffffffffff,
             accounts: {
                 balance: '10000000000000000000000000000',
                 privateKey: process.env.PRIV_KEY_MAINNET,
             },
-            chainId: 1,
+            chainId: 8453,
         },
         // NETWORKS FOR DEPLOYING
         mainnet: {
