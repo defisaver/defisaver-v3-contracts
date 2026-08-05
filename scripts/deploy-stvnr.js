796c1430fbcbfd8f2766671e89d43e4a5967017d
Milos Djurica <milosdjurica.work@gmail.com>
2026-08-05 18:47:28 +0200

    chore: add deployment addresses

--- scripts/deploy-stvnr.js
+++ scripts/deploy-stvnr.js
@@ -2,18 +2,60 @@ const hre = require('hardhat');
 const { topUp } = require('./utils/fork');
 const { getOwnerAddr, redeploy, network } = require('../test/utils/utils');
 
+const CONTRACTS_PER_NETWORK = {
+    mainnet: [
+        'TriggerView',
+        'AaveV3MinDebtTrigger',
+        'AaveV4MinDebtTrigger',
+        'CompV3MinDebtTrigger',
+        'FluidMinDebtTrigger',
+        'McdMinDebtTrigger',
+        'MorphoBlueMinDebtTrigger',
+        'SparkMinDebtTrigger',
+        'RequiredAmountAndAllowanceTrigger',
+    ],
+    optimism: ['TriggerView', 'AaveV3MinDebtTrigger', 'RequiredAmountAndAllowanceTrigger'],
+    arbitrum: [
+        'TriggerView',
+        'AaveV3MinDebtTrigger',
+        'CompV3MinDebtTrigger',
+        'FluidMinDebtTrigger',
+        'MorphoBlueMinDebtTrigger',
+        'RequiredAmountAndAllowanceTrigger',
+    ],
+    base: [
+        'TriggerView',
+        'AaveV3MinDebtTrigger',
+        'CompV3MinDebtTrigger',
+        'FluidMinDebtTrigger',
+        'MorphoBlueMinDebtTrigger',
+        'RequiredAmountAndAllowanceTrigger',
+    ],
+};
+
 async function main() {
+    const contracts = CONTRACTS_PER_NETWORK[network];
+
+    if (!contracts) {
+        throw new Error(`No TriggerView deployment list defined for network: ${network}`);
+    }
+
     const senderAcc = (await hre.ethers.getSigners())[0];
     await topUp(senderAcc.address, network);
     await topUp(getOwnerAddr(), network);
 
-    const stvnr = await redeploy('TriggerView', true);
-    const fluidMinDebtChecker = await redeploy('FluidMinDebtTrigger', true);
-    const aaveV3MinDebtChecker = await redeploy('AaveV3MinDebtTrigger', true);
+    console.log(`Deploying ${contracts.length} contracts on ${network}`);
 
-    console.log('TriggerView:', stvnr.address);
-    console.log('FluidMinDebtTrigger:', fluidMinDebtChecker.address);
-    console.log('AaveV3MinDebtTrigger:', aaveV3MinDebtChecker.address);
+    const deployed = {};
+
+    // sequential on purpose, deploys share the owner account nonce
+    for (const name of contracts) {
+        const c = await redeploy(name, true);
+        deployed[name] = c.address;
+    }
+
+    console.log('\nDeployed:');
+    console.table(deployed);
 }
 
 main().catch((error) => {
