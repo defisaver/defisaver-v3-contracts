cb6621784ba82ef6c63a3af9df4191a6eee0b36c
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2026-08-14 19:46:51 +0200

    refactor: reformat fl addresses

--- contracts/actions/flashloan/helpers/LineaFLAddresses.sol
+++ contracts/actions/flashloan/helpers/LineaFLAddresses.sol
@@ -3,36 +3,51 @@
 pragma solidity =0.8.24;
 
 contract LineaFLAddresses {
+    /**
+     * DFS Addresses
+     *
+     */
+    address internal constant DFS_REGISTRY_ADDR = 0x09fBeC68D216667C3262211D2E5609578951dCE0;
+    address internal constant FEE_FAUCET = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    /**
+     * Token Addresses
+     *
+     */
     address internal constant WETH_ADDR = 0xe5D7C2a44FfDDf6b295A15c148167daaAf5Cf34f;
     address internal constant DAI_ADDR = 0x4AF15ec2A0BD43Db75dd04E62FAA3B8EF36b00d5;
+    address internal constant ST_ETH_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address internal constant GHO_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    address internal constant CURVEUSD_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
-    // AAVE_LENDING_POOL == AAVE_V3_LENDING_POOL so current automation doesn't break
+    /**
+     * External Addresses
+     *
+     */
+    // Aave V2 -> Same as Aave V3 for backwards compatibility
     address internal constant AAVE_LENDING_POOL = 0xc47b8C00b0f69a36fa203Ffeac0334874574a8Ac;
     address internal constant AAVE_LENDING_POOL_ADDRESS_PROVIDER =
         0x89502c3731F69DDC95B65753708A07F8Cd0373F4;
+    // Aave V3
     address internal constant AAVE_V3_LENDING_POOL = 0xc47b8C00b0f69a36fa203Ffeac0334874574a8Ac;
     address internal constant AAVE_V3_LENDING_POOL_ADDRESS_PROVIDER =
         0x89502c3731F69DDC95B65753708A07F8Cd0373F4;
-
-    address internal constant DFS_REGISTRY_ADDR = 0x09fBeC68D216667C3262211D2E5609578951dCE0;
-    ////////// NOT USED
-    address internal constant VAULT_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address internal constant BALANCER_V3_VAULT_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address internal constant MORPHO_BLUE_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address internal constant UNI_V3_FACTORY = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-
-    address internal constant FEE_FAUCET = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    // this will stop MAKER flashloans on Optimism
-    address internal constant DSS_FLASH_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address internal constant ST_ETH_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-
+    // Spark
     address internal constant SPARK_LENDING_POOL_ADDRESS_PROVIDER =
         0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     address internal constant SPARK_LENDING_POOL = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-
-    address internal constant GHO_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    // Maker
+    address internal constant DSS_FLASH_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    // Balancer V2
+    address internal constant VAULT_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    // Balancer V3
+    address internal constant BALANCER_V3_VAULT_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    // GHO
     address internal constant GHO_FLASH_MINTER_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-
+    // Uniswap V3
+    address internal constant UNI_V3_FACTORY = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    // Morpho Blue
+    address internal constant MORPHO_BLUE_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    // Curve USD
     address internal constant CURVEUSD_FLASH_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-    address internal constant CURVEUSD_ADDR = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 }
