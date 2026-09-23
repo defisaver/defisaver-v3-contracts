ac4b7eba1c49137138607960c34b2ba7a5996f69
Rajko Zagorac <57317299+rajko-z@users.noreply.github.com>
2026-09-23 15:56:48 +0200

    chore: add new BytesTransientStorage address

--- contracts/actions/llamalend/helpers/MainnetLlamaLendAddresses.sol
+++ contracts/actions/llamalend/helpers/MainnetLlamaLendAddresses.sol
@@ -2,7 +2,7 @@
 pragma solidity =0.8.24;
 
 contract MainnetLlamaLendAddresses {
-    address internal constant BYTES_TRANSIENT_STORAGE = 0xd3FaFe60A64CFb4a136Cbb778e3D82e1725c4740;
+    address internal constant BYTES_TRANSIENT_STORAGE = 0x424e4819A1C33F2D8FDe70CD3c4801c03dB62C72;
     address internal constant LLAMALEND_FACTORY = 0xeA6876DDE9e3467564acBeE1Ed5bac88783205E0;
     /// @dev this is the only WETH controller which has use_eth param default to True in Controller.remove_collateral
     address internal constant OLD_WETH_CONTROLLER = 0xaade9230AA9161880E13a38C83400d3D1995267b;
